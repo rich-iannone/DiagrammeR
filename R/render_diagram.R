d175b1d3b332c6bb581b744bcfacdd4018ff7462
rich-iannone <riannone@me.com>
2014-12-28 01:03:37 -0800

    Modify indentation in HTML lines

--- R/render_diagram.R
+++ R/render_diagram.R
@@ -3,31 +3,30 @@
 #' @export render_diagram
 
 render_diagram <- function(diagram_code){
-
-# Create a blank 'index.html' file in a temporary directory
-tempDir <- tempfile()
-dir.create(tempDir)
-htmlFile <- file.path(tempDir, "index.html")
-
-# Add HTML to index.html file
-writeLines(paste0("
-<!DOCTYPE html>
-  <html>
-  <head>
-  <script src=\"https://cdn.rawgit.com/knsv/mermaid/master/dist/mermaid.full.min.js\"></script>
-    <meta charset=\"utf-8\">
-      <title></title>
-      
-      </head>
-      <body>
-      <div class=\"mermaid\">",
-diagram_code,
-"     </div>
-  </body>
-  </html>
-"), con = htmlFile)
-
-# Display diagram in viewer pane
-rstudio::viewer(htmlFile)
-
+  
+  # Create a blank 'index.html' file in a temporary directory
+  tempDir <- tempfile()
+  dir.create(tempDir)
+  htmlFile <- file.path(tempDir, "index.html")
+  
+  # Add HTML to index.html file
+  writeLines(paste0("
+  <!DOCTYPE html>
+    <html>
+    <head>
+    <script src=\"https://cdn.rawgit.com/knsv/mermaid/master/dist/mermaid.full.min.js\"></script>
+      <meta charset=\"utf-8\">
+        <title></title>
+        </head>
+        <body>
+        <div class=\"mermaid\">",
+  diagram_code,
+  "     </div>
+      </body>
+    </html>
+  "), con = htmlFile)
+  
+  # Display diagram in viewer pane
+  rstudio::viewer(htmlFile)
+  
 }
