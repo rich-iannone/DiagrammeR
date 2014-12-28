d5215c1ae8a4977275c82ae4f618de7eeb39402d
rich-iannone <riannone@me.com>
2014-12-28 01:28:49 -0800

    Add Example 4 to 'diagram_examples'

--- R/diagram_examples.R
+++ R/diagram_examples.R
@@ -5,7 +5,7 @@
 diagram_examples <- function(example_number){
   
   # Validate the value provided for 'example_number'
-  stopifnot(example_number %in% seq(1:2))
+  stopifnot(example_number %in% seq(1:4))
   
   # Example 1: Simple relationships running from left to right
   if (example_number == 1){
@@ -54,6 +54,20 @@ diagram_examples <- function(example_number){
     style E fill:#B6E6E6;
     "
   }
+ 
+  # Example 4: Include link text
+  if (example_number == 4){
+    
+    diagram_example <- "
+    graph LR;
+    A(Start)-->|Line Text|B(Keep Going)
+    B-->|More Line Text|C(Stop);
+    
+    style A fill:#A2EB86, stroke:#04C4AB, stroke-width:2px;
+    style B fill:#FFF289, stroke:#FCFCFF, stroke-width:2px, stroke-dasharray: 4, 4;
+    style C fill:#FFA070, stroke:#FF5E5E, stroke-width:2px;
+    "
+  }  
   
   render_diagram(diagram_example)
   
