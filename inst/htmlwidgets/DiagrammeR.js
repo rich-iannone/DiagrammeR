95f4a9c8fb7fb924b8f1c6db2246707a4b3d60ef
timelyportfolio <kent.russell@timelyportfolio.com>
2015-01-07 23:12:20 -0600

    check for svg in makeResponsive

--- inst/htmlwidgets/DiagrammeR.js
+++ inst/htmlwidgets/DiagrammeR.js
@@ -54,11 +54,13 @@ HTMLWidgets.widget({
     //  or at a minimum fit within the bounds set by htmlwidgets
     //  for the parent container
     function makeResponsive(el){
-       var svg = el.getElementsByTagName("svg")[0]
-       if(svg.width) {svg.removeAttribute("width")};
-       if(svg.height) {svg.removeAttribute("height")};
-       svg.style.width = "100%";
-       svg.style.height = "100%";
+       var svg = el.getElementsByTagName("svg")[0];
+       if(svg){
+        if(svg.width) {svg.removeAttribute("width")};
+        if(svg.height) {svg.removeAttribute("height")};
+        svg.style.width = "100%";
+        svg.style.height = "100%";
+       }
     };
 
     // set up a container for tasks to perform after completion
