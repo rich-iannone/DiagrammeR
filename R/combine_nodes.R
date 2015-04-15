474232e88a8d8cb2a09cb48e6917636b86b4d8b1
rich-iannone <riannone@me.com>
2015-04-14 23:45:05 -0700

    Determine which columns are not common to each df

--- R/combine_nodes.R
+++ R/combine_nodes.R
@@ -5,5 +5,20 @@ combine_nodes <- function(df1, df2){
   stopifnot(class(df1) == "data.frame")
   stopifnot(class(df2) == "data.frame")
 
+  # Examine the column names for each data frame and determine
+  # which columns are not common to each df
+  if (any(!(colnames(df2) %in% colnames(df1)))){
+    df_1_missing <- colnames(df2)[which(!(colnames(df2) %in% colnames(df1)))]
+  } else {
+    df_1_missing <- NA
+  }
+
+  if (any(!(colnames(df1) %in% colnames(df2)))){
+    df_2_missing <- colnames(df1)[which(!(colnames(df1) %in% colnames(df2)))]
+  } else {
+    df_2_missing <- NA
+  }
+
+
   return(df_new)
 }
