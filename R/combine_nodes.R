63bb8086f4e101e5f5a8e559324f69bea360d0a6
rich-iannone <riannone@me.com>
2015-04-14 23:43:59 -0700

    Determine whether data frames were supplied

--- R/combine_nodes.R
+++ R/combine_nodes.R
@@ -1,5 +1,9 @@
 
 combine_nodes <- function(df1, df2){
 
+  # Determine whether data frames were supplied
+  stopifnot(class(df1) == "data.frame")
+  stopifnot(class(df2) == "data.frame")
+
   return(df_new)
 }
