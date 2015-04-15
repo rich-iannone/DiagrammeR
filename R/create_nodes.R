aee78beec37d4d7c6d0b326888b4846c0aab4ca2
rich-iannone <riannone@me.com>
2015-04-15 01:00:57 -0700

    Expand or trim vectors to number of nodes

--- R/create_nodes.R
+++ R/create_nodes.R
@@ -4,4 +4,17 @@ create_nodes <- function(...){
 
   number_of_nodes <- length(nodes$nodes)
 
+  for (i in 1:length(nodes)){
+
+    # Expand vectors with single values to fill to number of nodes
+    if (length(nodes[[i]]) == 1){
+      nodes[[i]] <- rep(nodes[[i]], number_of_nodes)
+    }
+
+    # Trim vectors with number of values exceeding number of nodes
+    if (length(nodes[[i]]) > number_of_nodes){
+      nodes[[i]] <- nodes[[i]][1:number_of_nodes]
+    }
+  }
+
 }
