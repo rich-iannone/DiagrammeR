0762d9125f513a23a0f10c06b54e7c473ad327fc
rich-iannone <riannone@me.com>
2015-04-15 07:33:18 -0700

    Include loop to expand or trim vectors to desired length

--- R/create_edges.R
+++ R/create_edges.R
@@ -5,5 +5,23 @@ create_edges <- function(...){
 
   number_of_edges <- length(edges$edge_from)
 
+  for (i in 1:length(edges)){
+
+    # Expand vectors with single values to fill to number of edges
+    if (length(edges[[i]]) == 1){
+      edges[[i]] <- rep(edges[[i]], number_of_edges)
+    }
+
+    # Expand vectors with length > 1 and length < 'number_of_edges'
+    if (length(edges[[i]]) > 1 & length(edges[[i]]) < number_of_edges){
+      edges[[i]] <- c(edges[[i]], rep("", (number_of_edges - length(edges[[i]]))))
+    }
+
+    # Trim vectors with number of values exceeding number of edges
+    if (length(edges[[i]]) > number_of_edges){
+      edges[[i]] <- edges[[i]][1:number_of_edges]
+    }
+  }
+
   return(edges_df)
 }
