c6b247e22aef31b76e9754c27b82cc2fb07d7757
rich-iannone <riannone@me.com>
2015-04-17 22:31:35 -0700

    Create vector of node attributes that take numeric values

--- R/scale_nodes.R
+++ R/scale_nodes.R
@@ -4,5 +4,9 @@ scale_nodes <- function(nodes_df,
                         range,
                         scale_type = "linear"){
 
+  # Create vector of node attributes that take numeric values
+  numeric_node_attributes <-
+    c("fontsize", "height", "labelfontsize", "penwidth", "weight")
+
 
 }
