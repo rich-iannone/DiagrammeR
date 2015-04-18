de9e4bf8f604479aa97283235293431535eb001c
rich-iannone <riannone@me.com>
2015-04-17 22:33:49 -0700

    Create numeric scaled values and add to df

--- R/scale_nodes.R
+++ R/scale_nodes.R
@@ -19,5 +19,33 @@ scale_nodes <- function(nodes_df,
   # in the statement
   is_col_node_attribute <- node_attr %in% color_node_attributes
 
+  if (is_num_node_attribute){
+
+    # Stop function if the length of the numeric vector is not 2
+    stopifnot(length(range) == 2)
+
+    # Obtain the min and max values for the data to normalize
+    num_range_min_max <- c(min(to_scale), max(to_scale))
+
+    # Get normalized values for attribute if "linear" option chosen
+    if (scale_type == "linear"){
+
+      normalized <- range[1] +
+        ((to_scale - num_range_min_max[1]) *
+           (range[2] - range[1])) /
+        (num_range_min_max[2] - num_range_min_max[1])
+    }
+
+    # Create data frame for merging
+    normalized_df <- data.frame(normalized)
+
+    # Column bind both data frames together
+    nodes_df <- cbind(nodes_df, normalized_df)
+
+    # Change temporary name of column to that of attribute chosen
+    colnames(nodes_df)[which(names(nodes_df) %in% "normalized")] <- node_attr
+
+    return(nodes_df)
+  }
 
 }
