45fa95e5638951ee076fadcc582ac8ec102f8142
rich-iannone <riannone@me.com>
2015-04-18 13:32:39 -0700

    Add 'alpha' as a numeric node attribute

--- R/scale_nodes.R
+++ R/scale_nodes.R
@@ -17,7 +17,7 @@ scale_nodes <- function(nodes_df,
 
   # Create vector of node attributes that take numeric values
   numeric_node_attributes <-
-    c("fontsize", "height", "labelfontsize", "penwidth", "weight")
+    c("fontsize", "height", "labelfontsize", "penwidth", "weight", "alpha")
 
   # Create vector of node attributes that take color values
   color_node_attributes <- c("fillcolor", "fontcolor")
