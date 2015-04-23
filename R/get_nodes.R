3f324742131510ee0598968c363f653be6c5454d
rich-iannone <riannone@me.com>
2015-04-22 23:19:36 -0700

    Create 'get_nodes' function

---
+++ R/get_nodes.R
@@ -0,0 +1,11 @@
+#' Obtain a vector on node IDs from a node data frame
+#'
+#' Generates a vector of node IDs from a single node data frame
+#'
+#' @param nodes_df a data frame containing, at minimum, a column that contains node IDs for the graph. Optionally, additional columns (named as Graphviz node attributes) can be included with values for the named node attribute. These data frames can be conveniently generated using the 'create_nodes' function.
+#' @export get_nodes
+
+get_nodes <- function(nodes_df){
+
+
+}
