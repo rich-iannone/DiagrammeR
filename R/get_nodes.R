b67ed6eea320d23ca0a06e0e53a6aec370040e54
rich-iannone <riannone@me.com>
2015-04-23 00:06:33 -0700

    Modify roxygen statements

--- R/get_nodes.R
+++ R/get_nodes.R
@@ -1,6 +1,6 @@
-#' Obtain a vector on node IDs from a node data frame
+#' Get node ID information from node data frames
 #'
-#' Generates a vector of node IDs from a single node data frame
+#' Provides information on the node IDs from one or several node data frames.
 #'
 #' @param nodes_df a data frame containing, at minimum, a column that contains node IDs for the graph. Optionally, additional columns (named as Graphviz node attributes) can be included with values for the named node attribute. These data frames can be conveniently generated using the 'create_nodes' function.
 #' @export get_nodes
