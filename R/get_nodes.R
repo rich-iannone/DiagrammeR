b5031c03c0b68f8c6138479d1f2fd657ca1b0bb7
rich-iannone <riannone@me.com>
2015-04-26 14:12:14 -0700

    Get vector list of node IDs

--- R/get_nodes.R
+++ R/get_nodes.R
@@ -32,8 +32,59 @@ get_nodes <- function(...){
         object_type <- "edge_df"
       }
     }
+
+    if (object_type == "gv_graph"){
+
+      object <- object$nodes_df
+
+      if ("node" %in% colnames(object)){
+        nodes_column <- which("node" %in% colnames(object))
+      } else if ("nodes" %in% colnames(object)){
+        nodes_column <- which("nodes" %in% colnames(object))
+      } else if ("node_id" %in% colnames(object)){
+        nodes_column <- which("node_id" %in% colnames(object))
+      } else {
+        stop("There is no column with node ID information.")
+      }
+
+      node_ID <- c(node_ID, object[,nodes_column])
     }
 
+    if (object_type == "node_df"){
+
+      if ("node" %in% colnames(object)){
+        nodes_column <- which("node" %in% colnames(object))
+      } else if ("nodes" %in% colnames(object)){
+        nodes_column <- which("nodes" %in% colnames(object))
+      } else if ("node_id" %in% colnames(object)){
+        nodes_column <- which("node_id" %in% colnames(object))
+      } else {
+        stop("There is no column with node ID information.")
+      }
+
+      node_ID <- c(node_ID, object[,nodes_column])
+    }
+
+    if (object_type == "edge_df"){
+
+      both_from_to_columns <- all(c(any(c("edge_from", "from") %in%
+                                          colnames(object))),
+                                  any(c("edge_to", "to") %in%
+                                        colnames(object)))
+
+      if (exists("both_from_to_columns")){
+
+        if (both_from_to_columns == TRUE){
+
+          from_column <- which(colnames(object) %in% c("edge_from", "from"))[1]
+
+          to_column <- which(colnames(object) %in% c("edge_to", "to"))[1]
+        }
+      }
+
+      node_ID <- c(node_ID, unique(c(object[,from_column],
+                                     object[,to_column])))
+    }
   }
 
   all_ID_unique <- ifelse(anyDuplicated(node_ID) == 0, TRUE, FALSE)
