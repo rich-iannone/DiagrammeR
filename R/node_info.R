42871f00df509474ee7d661711f2e814de72eae4
rich-iannone <riannone@me.com>
2015-04-27 06:58:20 -0700

    Create function 'node_info'

---
+++ R/node_info.R
@@ -0,0 +1,11 @@
+#' Get detailed information on nodes
+#'
+#' Provides detailed information on nodes and their interrelationships within a graph.
+#'
+#' @param graph a graph object.
+#' @export node_info
+
+node_info <- function(graph){
+
+
+}
