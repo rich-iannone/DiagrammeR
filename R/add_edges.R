281cffcbf322f8ff0e48ac3446a42ef1ca15470b
rich-iannone <riannone@me.com>
2015-05-03 01:41:59 -0700

    Modify roxygen @param statements

--- R/add_edges.R
+++ R/add_edges.R
@@ -1,10 +1,10 @@
 #' Add edges to an existing graph object
 #' @description With a graph object of class 'gv_graph', add one or more edges of specified types to nodes within the graph.
 #' @param graph a graph object of class 'gv_graph' that is created using 'graphviz_graph'.
-#' @param edges an edge data frame that is created using 'create_edges'.
-#' @param edge_from the outgoing node from which the edge is connected.
-#' @param edge_to the incoming node to which the edge is connected.
-#' @param type a string specifying the relationship between the connected nodes.
+#' @param edges_df an edge data frame that is created using 'create_edges'.
+#' @param from a vector of the outgoing nodes from which each edge is connected.
+#' @param to a vector of the incoming nodes to which each edge is connected.
+#' @param relationship a string specifying the relationship between the connected nodes.
 #' @return a graph object of class 'gv_graph'.
 #' @export add_edges
 
