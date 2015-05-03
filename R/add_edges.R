ffe1d417064cff034ce390f5347daa8713fd8c34
rich-iannone <riannone@me.com>
2015-05-03 00:41:16 -0700

    Create the 'add_edges' function

---
+++ R/add_edges.R
@@ -0,0 +1,20 @@
+#' Add edges to an existing graph object
+#' @description With a graph object of class 'gv_graph', add one or more edges of specified types to nodes within the graph.
+#' @param graph a graph object of class 'gv_graph' that is created using 'graphviz_graph'.
+#' @param edges an edge data frame that is created using 'create_edges'.
+#' @param edge_from the outgoing node from which the edge is connected.
+#' @param edge_to the incoming node to which the edge is connected.
+#' @param type a string specifying the relationship between the connected nodes.
+#' @return a graph object of class 'gv_graph'.
+#' @export add_edges
+
+add_edges <- function(graph,
+                      edges = NULL,
+                      edge_from = NULL,
+                      edge_to = NULL,
+                      type = NULL){
+
+
+
+  return(gv_graph)
+}
