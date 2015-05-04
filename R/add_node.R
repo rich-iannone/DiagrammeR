f02888fac8bf2dbb244d0ac4f1b7092af385da88
rich-iannone <riannone@me.com>
2015-05-03 23:57:12 -0700

    Modify roxygen @param statements

--- R/add_node.R
+++ R/add_node.R
@@ -2,10 +2,10 @@
 #' @description With a graph object of class 'gv_graph', add a new node of a specified type to extant nodes within the graph.
 #' @param graph a graph object of class 'gv_graph' that is created using 'graphviz_graph'.
 #' @param node a node ID for the newly connected node.
-#' @param from a vector containing node IDs from which edges will be directed to the new node.
-#' @param to a vector containing node IDs to which edges will be directed from the new node.
-#' @param label an optional label to be ascribed to the node for visual display.
-#' @param type a vector of strings reppresenting the entity type for the nodes.
+#' @param from an optional vector containing node IDs from which edges will be directed to the new node.
+#' @param to an optional vector containing node IDs to which edges will be directed from the new node.
+#' @param label a character object for supplying an optional label to the node. Setting to TRUE ascribes the node ID to the label. Setting to FALSE yields a blank label.
+#' @param type an optional string that describes the entity type for the node to be added.
 #' @return a graph object of class 'gv_graph'.
 #' @export add_node
 
