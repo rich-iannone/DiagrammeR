a6d5de11e673ca198e74cc5dd94fb9f36957f078
rich-iannone <riannone@me.com>
2015-05-06 00:04:10 -0700

    Add function 'is_graph_empty'

---
+++ R/is_graph_empty.R
@@ -0,0 +1,10 @@
+#' Is the graph empty?
+#' @description Determines whether a graph contains any nodes and returns a logical value to reflect emptiness.
+#' @param graph a graph object of class 'gv_graph'.
+#' @return a logical value
+#' @export is_graph_empty
+
+is_graph_empty <- function(graph){
+
+
+}
