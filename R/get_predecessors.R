c682c3b95d19d3e014ad06be7d1c6bcf269fa90e
rich-iannone <riannone@me.com>
2015-05-05 19:24:53 -0700

    Add function 'get_predecessors'

---
+++ R/get_predecessors.R
@@ -0,0 +1,11 @@
+#' Get node IDs for predecessor nodes to the specified node
+#' @description Provides a vector of node IDs for all nodes that have a link to the given node.
+#' @param graph a graph object of class 'gv_graph'.
+#' @param node a node ID for the selected node.
+#' @return a vector of node ID values.
+#' @export get_predecessors
+
+get_predecessors <- function(graph,
+                             node){
+
+}
