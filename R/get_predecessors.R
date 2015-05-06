1466842721aba5b42119e9055cc8d91f30eb6f91
rich-iannone <riannone@me.com>
2015-05-06 00:31:10 -0700

    Obtain the node's predecessors

--- R/get_predecessors.R
+++ R/get_predecessors.R
@@ -8,4 +8,25 @@
 get_predecessors <- function(graph,
                              node){
 
+  # Determine whether graph has nodes
+  graph_is_not_empty <- !is_graph_empty(graph)
+
+  # Determine whether the node is in the graph
+  node_is_in_graph <- node_present(graph, node)
+
+  # Obtain the node's predecessors
+  if (graph_is_not_empty & node_is_in_graph & nrow(edge_info(graph)) > 0){
+
+    if (length(graph$edges_df[graph$edges_df$edge_to == node,]$edge_from) == 0){
+
+      predecessors <- NA
+
+    } else {
+
+      predecessors <- graph$edges_df[graph$edges_df$edge_to == node,]$edge_from
+
+    }
+
+    return(predecessors)
+  }
 }
