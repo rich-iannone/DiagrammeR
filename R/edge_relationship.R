008f3d0819156078d4d7276d845382bd7cdb6e43
rich-iannone <riannone@me.com>
2015-05-07 21:02:29 -0700

    Change name of possible 'action' value

--- R/edge_relationship.R
+++ R/edge_relationship.R
@@ -3,7 +3,7 @@
 #' @param graph a graph object of class 'gv_graph' that is created using 'graphviz_graph'.
 #' @param from a node ID from which the edge to be queried is outgoing.
 #' @param to a node ID to which the edge to be queried is incoming.
-#' @param action the type of operation to perform post-query. To remove a relationship from an edge, use either 'delete', 'remove', or 'drop'. To add a relationship to an edge with no set relationship, use 'add' or 'create'. To update an edge relationship, use 'update'. To return the value of an edge relationship, use 'read'. To determine whether there is a set relationship, use 'available'.
+#' @param action the type of operation to perform post-query. To remove a relationship from an edge, use either 'delete', 'remove', or 'drop'. To add a relationship to an edge with no set relationship, use 'add' or 'create'. To update an edge relationship, use 'update'. To return the value of an edge relationship, use 'read'. To determine whether there is a set relationship, use 'check'.
 #' @param value a string denoting the relationship, supplied only if 'action' was set to either 'add', 'create', or 'update'.
 #' @return a graph object of class 'gv_graph'.
 #' @export edge_relationship
@@ -95,7 +95,7 @@ edge_relationship <- function(graph,
     }
 
     # Determine whether a relationship has been set
-    if (action == "available"){
+    if (action == "check"){
 
       if (relationship_set == FALSE){
 
