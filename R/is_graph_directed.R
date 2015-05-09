d3ad7277b3c3ba0e250aa5293958cd3f7a4c05b6
rich-iannone <riannone@me.com>
2015-05-09 00:55:10 -0700

    Add function 'is_graph_directed'

---
+++ R/is_graph_directed.R
@@ -0,0 +1,13 @@
+#' Is the graph a directed graph?
+#' @description Determines whether a graph is set to be directed or not and returns a logical value to that effect.
+#' @param graph a graph object of class 'gv_graph'.
+#' @return a logical value
+#' @export is_graph_directed
+
+is_graph_directed <- function(graph){
+
+  # Determine if graph is directed by getting the value at 'graph$directed'
+  graph_is_directed <- graph$directed
+
+  return(graph_is_directed)
+}
