fcd6bf4b28a6b8237b9110fe91717a3dface12a9
rich-iannone <riannone@me.com>
2015-05-10 23:51:13 -0700

    Modify function name

--- R/graphviz_render.R
+++ R/graphviz_render.R
@@ -4,9 +4,9 @@
 #' @param output a string specifying the output type; \code{graph} (the default) renders the graph using the \code{grViz} function, \code{DOT} outputs DOT code for the graph, and \code{SVG} provides SVG code for the rendered graph.
 #' @param width an optional parameter for specifying the width of the resulting graphic in pixels.
 #' @param height an optional parameter for specifying the height of the resulting graphic in pixels.
-#' @export graphviz_render
+#' @export render_graph
 
-graphviz_render <- function(graph,
+render_graph <- function(graph,
                             output = "graph",
                             width = NULL,
                             height = NULL){
