c03802efd8fafc3e56fb7cb9a243c788f9d0fa96
rich-iannone <riannone@me.com>
2015-05-12 07:12:52 -0700

    Add function 'display_graph_object'

---
+++ R/display_graph_object.R
@@ -0,0 +1,9 @@
+#' Update and display graph object in Viewer
+#' @description Using a 'gv_graph' object, update values of counts for nodes, edges, attributes, directed state, and display the schematic in the RStudio Viewer.
+#' @param graph a 'gv_graph' object, created using the \code{create_graph} function
+#' @import stringr
+#' @export display_graph_object
+
+display_graph_object <- function(graph){
+
+}
