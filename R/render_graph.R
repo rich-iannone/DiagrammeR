655741e3c34510e567c0e17df1849e35473f5d96
rich-iannone <riannone@me.com>
2015-05-11 17:09:34 -0700

    Add roxygen @import statement

--- R/render_graph.R
+++ R/render_graph.R
@@ -4,6 +4,7 @@
 #' @param output a string specifying the output type; \code{graph} (the default) renders the graph using the \code{grViz} function, \code{DOT} outputs DOT code for the graph, and \code{SVG} provides SVG code for the rendered graph.
 #' @param width an optional parameter for specifying the width of the resulting graphic in pixels.
 #' @param height an optional parameter for specifying the height of the resulting graphic in pixels.
+#' @import stringr
 #' @export render_graph
 
 render_graph <- function(graph,
