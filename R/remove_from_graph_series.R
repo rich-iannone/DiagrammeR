c37271c5b48720e18885f9fb2444c2c6a2a2d17c
rich-iannone <riannone@me.com>
2015-05-19 00:32:49 -0700

    Add function "remove_from_graph_series"

---
+++ R/remove_from_graph_series.R
@@ -0,0 +1,13 @@
+#' Remove graph object from a graph series object
+#' Remove a single graph object from an array of graph objects in a graph series object.
+#' @param index the location of the graph object to be removed from the graph series object.
+#' @param graph_series a graph series object from which the graph object will be removed.
+#' @return a graph series object of type 'dgr_graph_1D'.
+#' @export remove_from_graph_series
+
+remove_from_graph_series <- function(index = "last",
+                                     graph_series){
+
+
+  return(graph_series)
+}
