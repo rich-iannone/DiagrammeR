375ea839ebbd3ea8acac4d4836abdc5d6d47b7ac
rich-iannone <riannone@me.com>
2015-05-23 08:37:00 -0700

    Add roxygen @param statement

--- R/subset_graph_series.R
+++ R/subset_graph_series.R
@@ -3,6 +3,7 @@
 #' @param graph_series a graph series object of type \code{dgr_graph_1D}.
 #' @param by either \code{number}, which allows for subsetting of the graph series by graph indices, or \code{time} which for graph series objects of type \code{temporal} allows for a subsetting of graphs by a date-time or time range.
 #' @param values where the subsetting of the graph series by to occur via graph indices (where \code{by = number}), provide a vector of those indices; when subsetting by time (where \code{by = time}), a range of times can be provided as a vector.
+#' @param tz the time zone (\code{tz}) corresponding to dates or date-time string provided in \code{values} (if \code{by = "date"}).
 #' @return a graph series object of type \code{dgr_graph_1D}.
 #' @export subset_graph_series
 
