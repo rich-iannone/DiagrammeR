e4cbb103d9e08b8f04e1bca95398046f057cb2e9
rich-iannone <riannone@me.com>
2015-05-24 16:39:08 -0700

    Add function 'series_info'

---
+++ R/series_info.R
@@ -0,0 +1,10 @@
+#' Get detailed information on a graph series
+#' Obtain a data frame with detailed information on the graphs within a graph series.
+#' @param graph_series a graph series object of type \code{dgr_graph_1D}.
+#' @return a data frame containing information on the graphs within the supplied graph series.
+#' @export series_info
+
+series_info <- function(graph_series){
+
+
+}
