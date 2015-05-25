45f92e249a62dd10f9a4fe5cdc77a61a22939943
rich-iannone <riannone@me.com>
2015-05-24 18:34:04 -0700

    Create data frame for each graph in graph series

--- R/series_info.R
+++ R/series_info.R
@@ -6,5 +6,30 @@
 
 series_info <- function(graph_series){
 
+  graphs_in_series <- graph_count(graph_series)
 
+  series_properties <-
+    as.data.frame(mat.or.vec(nr = graphs_in_series, nc = 7))
+  colnames(series_properties) <-
+    c("graph", "name", "date_time", "tz", "nodes", "edges", "directed")
+
+  for (i in 1:graphs_in_series){
+
+    series_properties[i, 1] <- i
+
+    series_properties[i, 2] <-
+      graph_series$graphs[[i]]$graph_name
+
+    series_properties[i, 3] <-
+      graph_series$graphs[[i]]$graph_time
+
+    series_properties[i, 4] <-
+      graph_series$graphs[[i]]$graph_tz
+
+    series_properties[i, 5] <- node_count(graph_series$graphs[[i]])
+
+    series_properties[i, 6] <- edge_count(graph_series$graphs[[i]])
+
+    series_properties[i, 7] <- is_graph_directed(graph_series$graphs[[i]])
+  }
 }
