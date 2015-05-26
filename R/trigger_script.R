dde7812b6ed40641611235d94f19f4482811cc36
rich-iannone <riannone@me.com>
2015-05-26 09:40:13 -0700

    Add function 'trigger_script'

---
+++ R/trigger_script.R
@@ -0,0 +1,19 @@
+#' Trigger a script embedded in a graph series object
+#' @description Run an R script located inside or referenced from the graph series object in order to migrate the state of one or more contained graphs.
+#' @param graph_series a graph series object of type \code{dgr_graph_1D}.
+#' @param script the index of the script character string or path reference held in in the graph series to be migrated.
+#' @return a graph series object of type \code{dgr_graph_1D}.
+#' @export trigger_script
+
+trigger_script <- function(graph_series,
+                           script = 1){
+
+  to_substitute <- deparse(substitute(graph_series))
+
+  substituted_script <- gsub("_SELF_", to_substitute,
+                             graph_series$series_scripts[script])
+
+  evaluated_series <- eval(parse(text = substituted_script))
+
+  return(evaluated_series)
+}
