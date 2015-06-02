47bb50f6dac06ba7821e17629325e770b71a689b
rich-iannone <riannone@me.com>
2015-06-02 16:56:27 -0700

    Remove references to 'edge_from' and 'edge_to' cols

--- R/edge_info.R
+++ R/edge_info.R
@@ -6,17 +6,17 @@
 
 edge_info <- function(graph){
 
-  if ("edge_from" %in% colnames(graph$edges_df)){
-    edge_from <- graph$edges_df$edge_from
-  }
+#   if ("edge_from" %in% colnames(graph$edges_df)){
+#     edge_from <- graph$edges_df$edge_from
+#   }
 
   if ("from" %in% colnames(graph$edges_df)){
     edge_from <- graph$edges_df$from
   }
 
-  if ("edge_to" %in% colnames(graph$edges_df)){
-    edge_to <- graph$edges_df$edge_to
-  }
+#   if ("edge_to" %in% colnames(graph$edges_df)){
+#     edge_to <- graph$edges_df$edge_to
+#   }
 
   if ("to" %in% colnames(graph$edges_df)){
     edge_to <- graph$edges_df$to
