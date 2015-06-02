42bbb383bc255ad274758300dd7f79f1a6665f7a
rich-iannone <riannone@me.com>
2015-06-02 16:35:27 -0700

    Remove unused statements and modify comments

--- R/create_edges.R
+++ R/create_edges.R
@@ -11,29 +11,15 @@ create_edges <- function(...){
   # Stop function if there are no list components
   stopifnot(!is.null(names(edges)))
 
-  # Attempt to obtain the number of edges from the 'edge_from' column
-  # If 'edge_from' column exists, ensure that it is classed as character
-#   if ("edge_from" %in% names(edges)){
-#     number_of_edges_from <- length(edges$edge_from)
-#     edges$edge_from <- as.character(edges$edge_from)
-#   }
-
-  # Attempt to obtain the number of edges from the 'from' column
-  # If 'from' column exists, ensure that it is classed as character
+  # Obtain the number of edges from the 'from' column
+  # Ensure that it is classed as character
   if ("from" %in% names(edges)){
     number_of_edges_from <- length(edges$from)
     edges$from <- as.character(edges$from)
   }
 
-  # Attempt to obtain the number of edges from the 'edge_to' column
-  # If 'edge_to' column exists, ensure that it is classed as character
-#   if ("edge_to" %in% names(edges)){
-#     number_of_edges_to <- length(edges$edge_to)
-#     edges$edge_to <- as.character(edges$edge_to)
-#   }
-
-  # Attempt to obtain the number of edges from the 'to' column
-  # If 'to' column exists, ensure that it is classed as character
+  # Obtain the number of edges from the 'to' column
+  # Ensure that it is classed as character
   if ("to" %in% names(edges)){
     number_of_edges_to <- length(edges$to)
     edges$to <- as.character(edges$to)
