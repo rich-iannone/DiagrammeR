fe750669e75a57da4d980aabfb4b1005159aa9e6
rich-iannone <riannone@me.com>
2015-06-10 15:14:36 -0700

    Add roxygen @examples statements

--- R/get_successors.R
+++ R/get_successors.R
@@ -3,6 +3,39 @@
 #' @param graph a graph object of class \code{dgr_graph}.
 #' @param node a node ID for the selected node.
 #' @return a vector of node ID values.
+#' @examples
+#' \dontrun{
+#' # Before getting node ID values for successors of
+#' # a specified node, create a simple graph
+#' nodes <-
+#'   create_nodes(nodes = LETTERS,
+#'                label = TRUE,
+#'                type = c(rep("a_to_g", 7),
+#'                         rep("h_to_p", 9),
+#'                         rep("q_to_x", 8),
+#'                         rep("y_and_z",2)))
+#'
+#' edges <-
+#'   create_edges(from = sample(LETTERS, replace = TRUE),
+#'                to = sample(LETTERS, replace = TRUE),
+#'                label = "edge",
+#'                relationship = "letter_to_letter")
+#'
+#' graph <-
+#'   create_graph(nodes_df = nodes,
+#'                edges_df = edges,
+#'                graph_attrs = "layout = neato",
+#'                node_attrs = c("fontname = Helvetica",
+#'                               "shape = circle"))
+#'
+#' # Get sucessors for node "A" in the graph
+#' get_successors(graph, node = "A")
+#' #> [1] "Z" "Y"
+#'
+#' # If there are no successors, NA is returned
+#' get_successors(graph, node = "Z")
+#' #> [1] NA
+#' }
 #' @export get_successors
 
 get_successors <- function(graph,
