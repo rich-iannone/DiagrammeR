958aba65c7b25dcb0c1f3ad876e409280a27d1cb
rich-iannone <riannone@me.com>
2015-06-11 06:59:35 -0700

    Add roxygen @examples statement

--- R/render_graph_from_series.R
+++ R/render_graph_from_series.R
@@ -5,6 +5,35 @@
 #' @param output a string specifying the output type; \code{graph} (the default) renders the graph using the \code{grViz} function, \code{DOT} outputs DOT code for the graph, and \code{SVG} provides SVG code for the rendered graph.
 #' @param width an optional parameter for specifying the width of the resulting graphic in pixels.
 #' @param height an optional parameter for specifying the height of the resulting graphic in pixels.
+#' @examples
+#' \dontrun{
+#' # Create three graphs (using \code{pipeR} for speed)
+#' # and create a graph series using those graphs
+#' library(pipeR)
+#'
+#' graph_1 <- create_graph() %>>%
+#'   add_node("a") %>>% add_node("b") %>>% add_node("c") %>>%
+#'   add_edges(from = c("a", "a", "b"),
+#'             to =   c("c", "b", "c"))
+#'
+#' graph_2 <- graph_1 %>>%
+#'   add_node("d") %>>% add_edges(from = "d", to = "c")
+#'
+#' graph_3 <- graph_2 %>>%
+#'   add_node("e") %>>% add_edges(from = "e", to = "b")
+#'
+#' # Create an empty graph series
+#' series <- create_series(series_type = "sequential")
+#'
+#' # Add graphs to the graph series
+#' series <- graph_1 %>>% add_to_series(series)
+#' series <- graph_2 %>>% add_to_series(series)
+#' series <- graph_3 %>>% add_to_series(series)
+#'
+#' # View the second graph in the series in the Viewer
+#' render_graph_from_series(graph_series = series,
+#'                          graph_no = 2)
+#' }
 #' @export render_graph_from_series
 
 render_graph_from_series <- function(graph_series,
