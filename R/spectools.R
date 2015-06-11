e9c4db89241d0670a127ab43bb943c56e8864087
rich-iannone <riannone@me.com>
2015-06-11 09:45:11 -0700

    Reduce whitespace

--- R/spectools.R
+++ R/spectools.R
@@ -1,7 +1,6 @@
 #' Razor-like template for diagram specification
 #' @description Use Razor-like syntax to define a template for use in a \code{grViz} diagram.
 #' @param spec string spec to be parsed and evaluated
-#'
 #' @examples
 #' \dontrun{
 #'
@@ -37,7 +36,6 @@
 #' }
 #'
 #' @export
-
 
 replace_in_spec <- function(spec){
 
@@ -142,5 +140,4 @@ replace_in_spec <- function(spec){
   if (grepl("@@", spec) == FALSE){
     return(spec)
   }
-
 }
