baac31fd900c1a92c9601207f2121b5d47b6332e
rich-iannone <riannone@me.com>
2015-06-11 09:31:57 -0700

    Add roxygen @examples section

--- R/is_graph_directed.R
+++ R/is_graph_directed.R
@@ -2,6 +2,24 @@
 #' @description Determines whether a graph is set to be directed or not and returns a logical value to that effect.
 #' @param graph a graph object of class \code{dgr_graph}.
 #' @return a logical value.
+#' @examples
+#' \dontrun{
+#' # Create a graph with nodes and edges
+#' nodes <-
+#'   create_nodes(nodes = c("a", "b", "c", "d"))
+#'
+#' edges <-
+#'   create_edges(from = c("a", "b", "c"),
+#'                to = c("d", "c", "a"))
+#'
+#' graph <-
+#'   create_graph(nodes_df = nodes,
+#'                edges_df = edges)
+#'
+#' # Determine whether the graph is directed
+#' is_graph_directed(graph)
+#' #> [1] TRUE
+#' }
 #' @export is_graph_directed
 
 is_graph_directed <- function(graph){
