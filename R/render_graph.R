dcd204ff028b519705e47a33e1ad7b6c3adfda83
rich-iannone <riannone@me.com>
2015-06-14 09:46:10 -0700

    Modify roxygen @param statements

--- R/render_graph.R
+++ R/render_graph.R
@@ -1,7 +1,8 @@
 #' Render the graph or output in various formats
 #' @description Using a \code{dgr_graph} object, either render graph in the Viewer or output in various formats.
 #' @param graph a \code{dgr_graph} object, created using the \code{create_graph} function.
-#' @param output a string specifying the output type; \code{graph} (the default) renders the graph using the \code{grViz} function, \code{DOT} outputs DOT code for the graph, and \code{SVG} provides SVG code for the rendered graph.
+#' @param output a string specifying the output type; \code{graph} (the default) renders the graph using the \code{grViz} function, \code{vivagraph} renders the graph using the \code{vivagraph} function, \code{DOT} outputs DOT code for the graph, and \code{SVG} provides SVG code for the rendered graph.
+#' @param layout a string specifying a layout type for a \code{vivagraph} rendering of the graph, either \code{forceDirected} or \code{constant}.
 #' @param width an optional parameter for specifying the width of the resulting graphic in pixels.
 #' @param height an optional parameter for specifying the height of the resulting graphic in pixels.
 #' @examples
