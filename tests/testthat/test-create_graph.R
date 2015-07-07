ea2cb68b71efeef2cca01f04e3a7f3239af0136a
rich-iannone <riannone@me.com>
2015-07-07 16:50:28 -0700

    Add tests for a graph object with nodes and no edges

--- tests/testthat/test-create_graph.R
+++ tests/testthat/test-create_graph.R
@@ -32,3 +32,51 @@ test_that("an empty graph object can be created and such an object is correct", 
   expect_match(graph$dot_code, "digraph \\{\n\n\\}")
 
 })
+
+test_that("a graph object with nodes can be created correctly", {
+
+  # Create a node data frame
+  nodes <-
+    create_nodes(nodes = c("a", "b", "c", "d"),
+                 label = FALSE,
+                 type = "lower",
+                 style = "filled",
+                 color = "aqua",
+                 shape = c("circle", "circle",
+                           "rectangle", "rectangle"),
+                 data = c(3.5, 2.6, 9.4, 2.7))
+
+  # Create the graph object using the node data frame
+  graph <- create_graph(nodes_df = nodes)
+
+  # Expect that names in this graph object match a prescribed set of names
+  expect_true(all(names(graph) == c("graph_name", "graph_time", "graph_tz",
+                                    "nodes_df", "edges_df", "graph_attrs",
+                                    "node_attrs", "edge_attrs", "directed",
+                                    "dot_code")))
+
+  # Expect a graph object of class 'dgr_graph'
+  expect_true(class(graph) == "dgr_graph")
+
+  # Expect that several of the graph components are NULL
+  expect_null(graph$graph_name)
+  expect_null(graph$graph_time)
+  expect_null(graph$graph_tz)
+  expect_null(graph$edges_df)
+  expect_null(graph$graph_attrs)
+  expect_null(graph$node_attrs)
+  expect_null(graph$edge_attrs)
+
+  # Expect that the 'nodes_df' component is a data frame
+  expect_true(class(graph$nodes_df) == "data.frame")
+
+  # Expect that the graph is a directed graph
+  expect_true(graph$directed == TRUE)
+
+  # Expect that the 'nodes_df' data frame has 7 columns
+  expect_true(ncol(graph$nodes_df) == 7L)
+
+  # Expect that the 'nodes_df' data frame has 4 rows
+  expect_true(nrow(graph$nodes_df) == 4L)
+
+})
