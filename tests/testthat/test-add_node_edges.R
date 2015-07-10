2f2e6fe4e64ab59207533233d1c23d82a6e74cdf
rich-iannone <riannone@me.com>
2015-07-10 07:04:39 -0700

    Add tests for 'add_node' and 'add_edges'

---
+++ tests/testthat/test-add_node_edges.R
@@ -0,0 +1,90 @@
+context("Adding nodes and/or edges to an existing graph object")
+
+test_that("adding a node to a graph is possible", {
+
+  # Create an empty graph
+  graph <- create_graph()
+
+  # Add two nodes
+  graph <- add_node(graph, node = "a")
+  graph <- add_node(graph, node = "b")
+
+  # Create the graph object using the node data frame
+  graph <- create_graph(nodes_df = nodes)
+
+  # Expect that names in this graph object match a prescribed set of names
+  expect_true(all(names(graph) == c("graph_name", "graph_time", "graph_tz",
+                                    "nodes_df", "edges_df", "graph_attrs",
+                                    "node_attrs", "edge_attrs", "directed",
+                                    "dot_code")))
+
+  # Expect a graph object of class 'dgr_graph'
+  expect_true(class(graph) == "dgr_graph")
+
+  # Expect that several of the graph components are NULL
+  expect_null(graph$graph_name)
+  expect_null(graph$graph_time)
+  expect_null(graph$graph_tz)
+  expect_null(graph$edges_df)
+  expect_null(graph$graph_attrs)
+  expect_null(graph$node_attrs)
+  expect_null(graph$edge_attrs)
+
+  # Expect that the 'nodes_df' component is a data frame
+  expect_true(class(graph$nodes_df) == "data.frame")
+
+  # Expect that the graph is a directed graph
+  expect_true(graph$directed == TRUE)
+
+  # Expect that the 'nodes_df' data frame has 2 columns
+  expect_true(ncol(graph$nodes_df) == 2L)
+
+  # Expect that the 'nodes_df' data frame has 2 rows
+  expect_true(nrow(graph$nodes_df) == 2L)
+})
+
+test_that("adding an edge to a graph is possible", {
+
+  # Create an empty graph
+  graph <- create_graph()
+
+  # Add two nodes
+  graph <- add_node(graph, node = "a")
+  graph <- add_node(graph, node = "b")
+
+  # Add an edge
+  graph <- add_edges(graph, from = "a", to = "b",
+                     relationship = "to_get")
+
+  # Expect that names in this graph object match a prescribed set of names
+  expect_true(all(names(graph) == c("graph_name", "graph_time", "graph_tz",
+                                    "nodes_df", "edges_df", "graph_attrs",
+                                    "node_attrs", "edge_attrs", "directed",
+                                    "dot_code")))
+
+  # Expect a graph object of class 'dgr_graph'
+  expect_true(class(graph) == "dgr_graph")
+
+  # Expect that several of the graph components are NULL
+  expect_null(graph$graph_name)
+  expect_null(graph$graph_time)
+  expect_null(graph$graph_tz)
+  expect_null(graph$graph_attrs)
+  expect_null(graph$node_attrs)
+  expect_null(graph$edge_attrs)
+
+  # Expect that the 'nodes_df' component is a data frame
+  expect_true(class(graph$nodes_df) == "data.frame")
+
+  # Expect that the 'edges_df' component is a data frame
+  expect_true(class(graph$edges_df) == "data.frame")
+
+  # Expect that the graph is a directed graph
+  expect_true(graph$directed == TRUE)
+
+  # Expect that the 'nodes_df' data frame has 3 columns
+  expect_true(ncol(graph$edges_df) == 3L)
+
+  # Expect that the 'nodes_df' data frame has 1 row
+  expect_true(nrow(graph$edges_df) == 1L)
+})
