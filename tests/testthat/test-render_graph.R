35b55b3e626ffd25545867617b9ff73082a0e82a
rich-iannone <riannone@me.com>
2015-07-09 20:37:52 -0700

    Add test for exported Graphviz DOT code

--- tests/testthat/test-render_graph.R
+++ tests/testthat/test-render_graph.R
@@ -37,3 +37,42 @@ test_that("rendering a graph is indeed possible", {
   # Expect that the 'rendered_graph' object inherits from 'grViz' & 'htmlwidget'
   expect_is(rendered_graph, c("grViz", "htmlwidget"))
 })
+
+test_that("exporting Graphviz DOT code is indeed possible", {
+
+  # Create a node data frame
+  nodes <-
+    create_nodes(nodes = LETTERS,
+                 type = "letter",
+                 shape = sample(c("circle", "rectangle"),
+                                length(LETTERS),
+                                replace = TRUE),
+                 fillcolor = sample(c("aqua", "gray80",
+                                      "pink", "lightgreen",
+                                      "azure", "yellow"),
+                                    length(LETTERS),
+                                    replace = TRUE))
+
+  # Create an edge data frame
+  edges <-
+    create_edges(from = sample(LETTERS, replace = TRUE),
+                 to = sample(LETTERS, replace = TRUE),
+                 relationship = "letter_to_letter")
+
+  # Create the graph object using the node and edge data frames
+  graph <-
+    create_graph(nodes_df = nodes,
+                 edges_df = edges,
+                 graph_attrs = "layout = twopi",
+                 node_attrs = c("fontname = Helvetica",
+                                "style = filled"),
+                 edge_attrs = c("color = gray20",
+                                "arrowsize = 0.5"))
+
+  # Output the DOT code as a character object
+  graph_dot_output <- render_graph(graph, output = "DOT")
+
+  # Expect that the DOT code exported is the same as that contained
+  # in the graph object
+  expect_equal(graph_dot_output, graph$dot_code)
+})
