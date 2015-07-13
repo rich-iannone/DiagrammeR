8ccd099e5e0b569789aa25d1acb89747947cd51b
rich-iannone <riannone@me.com>
2015-07-13 07:34:27 -0700

    Add tests for 'node_count' and 'edge_count'

---
+++ tests/testthat/test-node_edge_count.R
@@ -0,0 +1,86 @@
+context("Getting counts of nodes and edges in graph objects")
+
+test_that("getting a node count for a graph is possible", {
+
+  set.seed(26)
+
+  # Create a node data frame
+  nodes <-
+    create_nodes(nodes = LETTERS,
+                 label = TRUE,
+                 type = c(rep("a_to_g", 7),
+                          rep("h_to_p", 9),
+                          rep("q_to_x", 8),
+                          rep("y_and_z",2)))
+
+  # Create an edge data frame
+  edges <-
+    create_edges(from = sample(LETTERS, replace = TRUE),
+                 to = sample(LETTERS, replace = TRUE),
+                 label = "edge",
+                 relationship = "letter_to_letter")
+
+  # Create the graph object using the node and edge data frames
+  graph <- create_graph(nodes_df = nodes,
+                        edges_df = edges,
+                        graph_attrs = "layout = neato",
+                        node_attrs = c("fontname = Helvetica",
+                                       "shape = circle"))
+
+  # Obtain a count of nodes by type
+  count_of_nodes <- node_count(graph, type = TRUE)
+
+  # Expect that the 'count_of_nodes' object is a named vector
+  expect_true(!is.null(names(count_of_nodes)))
+  expect_true(all(names(count_of_nodes) == c("a_to_g", "h_to_p",
+                                             "q_to_x", "y_and_z")))
+  expect_true(all(c(7, 9, 8, 2) %in% count_of_nodes))
+
+  # Obtain a total count of nodes
+  total_count_of_nodes <- node_count(graph, type = FALSE)
+
+  # Expect that the 'total_count_of_nodes' object is a named vector
+  expect_is(total_count_of_nodes, "integer")
+  expect_equal(total_count_of_nodes, 26L)
+})
+
+test_that("getting an edge count for a graph is possible", {
+
+  set.seed(26)
+
+  # Create a node data frame
+  nodes <-
+    create_nodes(nodes = LETTERS,
+                 label = TRUE,
+                 type = c(rep("a_to_g", 7),
+                          rep("h_to_p", 9),
+                          rep("q_to_x", 8),
+                          rep("y_and_z",2)))
+
+  # Create an edge data frame
+  edges <-
+    create_edges(from = sample(LETTERS, replace = TRUE),
+                 to = sample(LETTERS, replace = TRUE),
+                 label = "edge",
+                 relationship = "letter_to_letter")
+
+  # Create the graph object using the node and edge data frames
+  graph <- create_graph(nodes_df = nodes,
+                        edges_df = edges,
+                        graph_attrs = "layout = neato",
+                        node_attrs = c("fontname = Helvetica",
+                                       "shape = circle"))
+
+  # Obtain a count of nodes by relationship
+  count_of_edges <- edge_count(graph, relationship = TRUE)
+
+  # Expect that the 'count_of_edges' object is a named vector
+  expect_true(!is.null(names(count_of_edges)))
+
+  # Obtain a total count of edges
+  total_count_of_edges <- edge_count(graph, relationship = FALSE)
+
+  # Expect that the 'total_count_of_edges' object is a named vector
+  expect_is(total_count_of_edges, "integer")
+  expect_equal(total_count_of_edges, 26L)
+})
