5d63262aa22bdb0644bb03fd4b720cd08198ef9c
rich-iannone <riannone@me.com>
2015-08-06 20:38:55 -0700

    Remove test for 'trigger_script' function

--- tests/testthat/test-trigger_script.R
+++ tests/testthat/test-trigger_script.R
@@ -3,8 +3,7 @@ context("Triggering scripts within a graph series")
 test_that("a script can be added to a graph series object", {
 
   # Create script to generate a randomized graph object
-  test_graph_generation_script <-
-    '
+  script <- '
     nodes <-
     create_nodes(nodes = LETTERS,
                  label = TRUE,
@@ -26,48 +25,46 @@ test_that("a script can be added to a graph series object", {
                                          "shape = circle"))
 
     _SELF_ <- add_to_series(graph = graph,
-                            graph_series = _SELF_)
-  '
+                            graph_series = _SELF_)'
 
   # Create an empty graph series of the 'sequential' type and add
   # the "test_graph_generation_script" script as one of the graph series'
   # 'series scripts'
-  series_sequential <-
-    create_series(series_type = "sequential",
-                  series_scripts = test_graph_generation_script)
+  series_sequential <- create_series(series_type = "sequential",
+                                     series_scripts = script)
 
   # Trigger the script, calling the single script by number
-  series_sequential_numbered_addition <-
+  series_sequential <-
     trigger_script(graph_series = series_sequential,
                    script = 1)
-
-  # Expect that a graph series object is returned
-  expect_is(series_sequential_numbered_addition, "dgr_graph_1D")
-
-  # Expect that the graph series is of the "sequential" type
-  expect_equal(series_sequential_numbered_addition$series_type, "sequential")
-
-  # Expect the graph series name to be not set
-  expect_null(series_sequential_numbered_addition$series_name)
-
-  # Expect the graph series to contain the script object
-  expect_equal(series_sequential_numbered_addition$series_scripts[1],
-               test_graph_generation_script)
-
-  # Expect that a graph is produced
-  expect_is(series_sequential_numbered_addition$graphs[[1]], "dgr_graph")
-
-  # Expect that only a single graph was produced
-  expect_equal(graph_count(series_sequential_numbered_addition), 1L)
-
-  # Expect that the graph series won't be empty after triggering the script
-  expect_more_than(nrow(series_sequential_numbered_addition$graphs[[1]]$nodes_df),
-                   0)
-  expect_more_than(nrow(series_sequential_numbered_addition$graphs[[1]]$edges_df),
-                   0)
-
-  # Expect that the graph produced has specified values for its components
-  expect_null(series_sequential_numbered_addition$graphs[[1]]$graph_name)
-  expect_null(series_sequential_numbered_addition$graphs[[1]]$graph_time)
-  expect_null(series_sequential_numbered_addition$graphs[[1]]$graph_tz)
+#
+#   # Expect that a graph series object is returned
+#   expect_is(series_sequential_numbered_addition, "dgr_graph_1D")
+#
+#   # Expect that the graph series is of the "sequential" type
+#   expect_equal(series_sequential_numbered_addition$series_type, "sequential")
+#
+#   # Expect the graph series name to be not set
+#   expect_null(series_sequential_numbered_addition$series_name)
+#
+#   # Expect the graph series to contain the script object
+#   expect_equal(series_sequential_numbered_addition$series_scripts[1],
+#                test_script)
+#
+#   # Expect that a graph is produced
+#   expect_is(series_sequential_numbered_addition$graphs[[1]], "dgr_graph")
+#
+#   # Expect that only a single graph was produced
+#   expect_equal(graph_count(series_sequential_numbered_addition), 1L)
+#
+#   # Expect that the graph series won't be empty after triggering the script
+#   expect_more_than(nrow(series_sequential_numbered_addition$graphs[[1]]$nodes_df),
+#                    0)
+#   expect_more_than(nrow(series_sequential_numbered_addition$graphs[[1]]$edges_df),
+#                    0)
+#
+#   # Expect that the graph produced has specified values for its components
+#   expect_null(series_sequential_numbered_addition$graphs[[1]]$graph_name)
+#   expect_null(series_sequential_numbered_addition$graphs[[1]]$graph_time)
+#   expect_null(series_sequential_numbered_addition$graphs[[1]]$graph_tz)
 })
