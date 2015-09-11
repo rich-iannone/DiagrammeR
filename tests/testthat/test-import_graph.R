e5e3d4540b5ef55e2927733d0a3fef148db9935a
rich-iannone <riannone@me.com>
2015-09-10 23:14:28 -0700

    Modify tests in 'test-import_graph'

--- tests/testthat/test-import_graph.R
+++ tests/testthat/test-import_graph.R
@@ -22,21 +22,25 @@ test_that("importing a .graphml file is possible", {
 
   # Expect that the 'node_attrs' component is a character vector of length 1
   expect_true(class(graphml_graph$node_attrs) == "character")
-  expect_equal(length(graphml_graph$node_attrs), 4L)
+  expect_equal(length(graphml_graph$node_attrs), 4)
 
   # Expect that the graph is a directed graph
   expect_true(graphml_graph$directed == TRUE)
 
   # Expect that the 'nodes_df' data frame has 9 columns
-  expect_true(ncol(graphml_graph$nodes_df) == 9L)
+  expect_true(ncol(graphml_graph$nodes_df) == 9)
 
   # Expect that the 'nodes_df' data frame has 4941 rows
-  expect_true(nrow(graphml_graph$nodes_df) == 4941L)
+  expect_true(nrow(graphml_graph$nodes_df) == 4941)
 
   # Expect that the 'edges_df' data frame has 2 columns
-  expect_true(ncol(graphml_graph$edges_df) == 2L)
+  expect_true(ncol(graphml_graph$edges_df) == 2)
 
   # Expect that the 'edges_df' data frame has 6594 rows
-  expect_true(nrow(graphml_graph$edges_df) == 6594L)
+  expect_true(nrow(graphml_graph$edges_df) == 6594)
 })
 
+
+graphml_graph <-
+  import_graph(system.file("examples/Human_Interactome_May_sif",
+                           package = "DiagrammeR"))
