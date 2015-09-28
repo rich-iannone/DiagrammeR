d8746966d6b38733a17d3fb7cb4bb96f941cf830
Richard Iannone <riannone@me.com>
2015-09-28 00:16:46 -0700

    Modify roxygen @description statement

--- R/is_graph_empty.R
+++ R/is_graph_empty.R
@@ -1,6 +1,6 @@
 #' Is the graph empty?
-#' @description Determines whether a graph contains any nodes and returns a
-#' logical value to reflect emptiness.
+#' @description Provides a logical value on whether the graph is empty (i.e.,
+#' contains no nodes).
 #' @param graph a graph object of class \code{dgr_graph}.
 #' @return a logical value.
 #' @examples
