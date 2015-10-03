eb4c17053f074b178ab600c9371db99481861eb9
Richard Iannone <riannone@me.com>
2015-10-02 21:33:37 -0700

    Modify roxygen @param statement

--- R/combine_graphs.R
+++ R/combine_graphs.R
@@ -1,7 +1,9 @@
 #' Combine two graphs into a single graph
 #' @description Combine two graphs in order to make a new graph, merging nodes
 #' and edges in the process.
-#' @param x a \code{DiagrammeR} graph object to which another graph will be joined.
+#' @param x a \code{DiagrammeR} graph object to which another graph will be
+#' joined. This graph should be considered the host graph as the resulting
+#' graph will retain only the attributes of this graph.
 #' @param y a \code{DiagrammeR} graph object that is to be joined with the graph
 #' suppled as \code{x}.
 #' @return a graph object of class \code{dgr_graph}.
