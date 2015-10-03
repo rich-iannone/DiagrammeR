6f7b2a1042ad0bddcdcf3e9f0042a67361760ea3
Richard Iannone <riannone@me.com>
2015-10-02 21:13:44 -0700

    Add function 'combine_graphs'

---
+++ R/combine_graphs.R
@@ -0,0 +1,12 @@
+#' Combine two graphs into a single graph
+#' @description Combine two graphs in order to make a new graph, merging nodes
+#' and edges in the process.
+#' @param x a \code{DiagrammeR} graph object to which another graph will be joined.
+#' @param y a \code{DiagrammeR} graph object that is to be joined with the graph
+#' suppled as \code{x}.
+#' @return a graph object of class \code{dgr_graph}.
+#' @export combine_graphs
+
+combine_graphs <- function(x, y){
+
+}
