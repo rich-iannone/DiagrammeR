d40570556c164a37ab778df14047e2a93aa69a1c
Richard Iannone <riannone@me.com>
2015-10-22 17:01:44 -0700

    Modify roxygen @param statement

--- R/node_count.R
+++ R/node_count.R
@@ -3,8 +3,9 @@
 #' nodes in the graph and optionally obtain a count of nodes by their type.
 #' @param graph a graph object of class \code{dgr_graph} that is created using
 #' \code{create_graph}.
-#' @param type a logical value, where \code{TRUE} provides a named vector of
-#' node count by type and \code{FALSE} (the default) provides a total count.
+#' @param type either a logical value, where \code{TRUE} provides a named vector
+#' of node count by type and \code{FALSE} (the default) provides a total count,
+#' or, a character vector of \code{type} values to filter the node count.
 #' @return a numeric vector of single length.
 #' @examples
 #' \dontrun{
