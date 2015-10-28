75ee8752b8a778a35a3a9b49eb67b71ae64de0c8
Richard Iannone <riannone@me.com>
2015-10-28 12:47:25 -0700

    Add function 'select_graph'

---
+++ R/select_graph.R
@@ -0,0 +1,11 @@
+#' Select a graph for a pipeline
+#' @description Select a graph and introduce that graph to
+#' a set of pipeline operations.
+#' @param graph a graph of class \code{dgr_graph}
+#' @export select_graph
+
+select_graph <- function(graph){
+
+  # Return the graph object
+  return(graph)
+}
