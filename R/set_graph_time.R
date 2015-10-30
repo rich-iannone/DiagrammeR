d8c7ee68d63aeef79c2d28de49a4cc3910258789
Richard Iannone <riannone@me.com>
2015-10-30 00:12:57 -0700

    Add function 'set_graph_time'

---
+++ R/set_graph_time.R
@@ -0,0 +1,34 @@
+#' Set graph date-time and timezone
+#' @description Set the time and timezone for a graph object of class
+#' \code{dgr_graph}.
+#' @param graph a graph object of class \code{dgr_graph} that is created
+#' using \code{create_graph}.
+#' @param time the date-time to set for the graph.
+#' @param tz the timezone to set for the graph.
+#' @return a graph object of class \code{dgr_graph}.
+#' @export set_graph_time
+
+set_graph_time <- function(graph,
+                           time,
+                           tz){
+
+  dgr_graph <-
+    create_graph(nodes_df = graph$nodes_df,
+                 edges_df = graph$nodes_df,
+                 graph_attrs = graph$graph_attrs,
+                 node_attrs = graph$node_attrs,
+                 edge_attrs = graph$edge_attrs,
+                 directed = ifelse(is_graph_directed(graph),
+                                   TRUE, FALSE),
+                 graph_name = graph$name,
+                 graph_time = ifelse(!is.null(time),
+                                     time, graph$graph_time),
+                 graph_tz = ifelse(!is.null(tz),
+                                   time, graph$graph_tz))
+
+  if (!is.null(graph$selection)){
+    dgr_graph$selection <- graph$selection
+  }
+
+  return(dgr_graph)
+}
