958b384672f58e37e88628f4ea1f31c408a9b2f5
Richard Iannone <riannone@me.com>
2015-11-03 11:59:07 -0800

    Add example to roxygen @examples section

--- R/get_edge_attr_from_selection.R
+++ R/get_edge_attr_from_selection.R
@@ -3,7 +3,35 @@
 #' attribute properties for edges available in a selection.
 #' @param graph a graph object of class \code{dgr_graph} that is created
 #' using \code{create_graph}.
-#' @return a graph object of class \code{dgr_graph}.
+#' @examples
+#' \dontrun{
+#' library(magrittr)
+#'
+#' # Create a simple graph
+#' nodes <-
+#'   create_nodes(nodes = c("a", "b", "c", "d"),
+#'                type = "letter",
+#'                label = TRUE,
+#'                value = c(3.5, 2.6, 9.4, 2.7))
+#'
+#' edges <-
+#'   create_edges(from = c("a", "b", "c"),
+#'                to = c("d", "c", "a"),
+#'                rel = "leading_to",
+#'                color = c("pink", "blue", "red"))
+#'
+#' graph <-
+#'   create_graph(nodes_df = nodes,
+#'                edges_df = edges)
+#'
+#' # Select edges "b" -> "c" and "a" -> "d" in the graph
+#' graph %>% select_edges(from = c("b", "a"), to = c("c", "d")) %>%
+#'   get_edge_attr_from_selection()
+#' #>   from to        rel color
+#' #> 1    a  d leading_to  pink
+#' #> 2    b  c leading_to  blue
+#' }
+#' @return an edge data frame.
 #' @export get_edge_attr_from_selection
 
 get_edge_attr_from_selection <- function(graph){
