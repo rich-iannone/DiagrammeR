9e8a7b4a130b7add3d0f8ffa1f383c142b3f7fff
Richard Iannone <riannone@me.com>
2015-11-03 12:06:20 -0800

    Add example to roxygen @examples section

--- R/get_node_attr_from_selection.R
+++ R/get_node_attr_from_selection.R
@@ -3,7 +3,36 @@
 #' attribute properties for nodes available in a selection.
 #' @param graph a graph object of class \code{dgr_graph} that is created
 #' using \code{create_graph}.
-#' @return a graph object of class \code{dgr_graph}.
+#' @examples
+#' \dontrun{
+#' library(magrittr)
+#'
+#' # Create a simple graph
+#' nodes <-
+#'   create_nodes(nodes = c("a", "b", "c", "d"),
+#'                type = "letter",
+#'                label = TRUE,
+#'                value = c(3.5, 2.6, 9.4, 2.7))
+#'
+#' edges <-
+#'   create_edges(from = c("a", "b", "c"),
+#'                to = c("d", "c", "a"),
+#'                rel = "leading_to",
+#'                color = c("pink", "blue", "red"))
+#'
+#' graph <-
+#'   create_graph(nodes_df = nodes,
+#'                edges_df = edges)
+#'
+#' # Select nodes "a" and "c" in the graph and get the node
+#' # attributes for that selection
+#' graph %>% select_nodes(nodes = c("a", "c")) %>%
+#'   get_node_attr_from_selection()
+#' #>   nodes   type label value
+#' #> 1     a letter     a   3.5
+#' #> 3     c letter     c   9.4
+#' }
+#' @return a node data frame.
 #' @export get_node_attr_from_selection
 
 get_node_attr_from_selection <- function(graph){
