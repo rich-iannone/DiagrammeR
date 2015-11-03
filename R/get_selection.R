ea00c92b7862cd47b1c7d9247fda4a104dc2aedd
Richard Iannone <riannone@me.com>
2015-11-03 09:33:58 -0800

    Add example to roxygen @examples section

--- R/get_selection.R
+++ R/get_selection.R
@@ -5,6 +5,22 @@
 #' using \code{create_graph}.
 #' @examples
 #' \dontrun{
+#' library(magrittr)
+#'
+#' # Create a simple graph
+#' graph <-
+#'   create_graph() %>%
+#'   add_node_df(create_nodes(1:6)) %>%
+#'   add_edges(c("1", "1", "3", "4", "4"),
+#'             c("2", "3", "4", "5", "6"))
+#'
+#' # Select node "4", select all nodes a distance of 1 away from "4",
+#' # and return the selection of nodes as a list object
+#' graph %>% select_nodes(nodes = "4") %>%
+#'   select_nodes_in_neighborhood(node = "4", distance = 1) %>%
+#'   get_selection()
+#' #> $nodes
+#' #> [1] "4" "5" "6" "3"
 #' }
 #' @return a list object with the current selection of nodes or edges.
 #' @export get_selection
