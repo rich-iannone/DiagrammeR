83caa813d33bbf9beb96da7ab0d94e9765da6dff
Richard Iannone <riannone@me.com>
2015-11-03 16:57:24 -0800

    Add example to roxygen @examples section

--- R/get_global_graph_attr.R
+++ R/get_global_graph_attr.R
@@ -6,6 +6,24 @@
 #' @return a list object containing global attributes for a graph.
 #' @examples
 #' \dontrun{
+#' library(magrittr)
+#'
+#' # Create a new graph and set some global attributes
+#' graph <- create_graph() %>%
+#'   set_global_graph_attr("graph", "overlap", "true") %>%
+#'   set_global_graph_attr("node", "fontname", "Helvetica") %>%
+#'   set_global_graph_attr("edge", "color", "gray")
+#'
+#' # Verify that the global attributes have been set
+#' get_global_graph_attr(graph)
+#' #> $graph_attrs
+#' #> [1] "overlap = true"
+#' #>
+#' #> $node_attrs
+#' #> [1] "fontname = Helvetica"
+#' #>
+#' #> $edge_attrs
+#' #> [1] "color = gray"
 #' }
 #' @export get_global_graph_attr
 
