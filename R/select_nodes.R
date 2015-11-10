ab547270753019f5444f014acae7902f02a1f188
Richard Iannone <riannone@me.com>
2015-11-10 00:28:11 -0800

    Modify roxygen @param statements

--- R/select_nodes.R
+++ R/select_nodes.R
@@ -2,14 +2,14 @@
 #' @description Select nodes from a graph object of class \code{dgr_graph}.
 #' @param graph a graph object of class \code{dgr_graph} that is created
 #' using \code{create_graph}.
-#' @param nodes an optional vector of node IDs for filtering list of
-#' nodes present in the graph.
 #' @param node_attr an optional character vector of node attribute values for
 #' filtering the node ID values returned.
-#' @param comparison an optional logical expression for filtering the nodes
-#' returned.
-#' @param regex an optional regular expression (regex) for filtering the
-#' nodes returned.
+#' @param search an option to provide a logical expression with a comparison
+#' operator (\code{>}, \code{<}, \code{==}, or \code{!=}) followed by a number
+#' for numerical filtering, or, a regular expression for filtering the nodes
+#' returned through string matching.
+#' @param nodes an optional vector of node IDs for filtering list of
+#' nodes present in the graph.
 #' @param set_op the set operation to perform upon consecutive selections
 #' of graph nodes. This can either be as a \code{union} (the default), as an
 #' \code{intersection}, or, as a \code{difference} on the previous selection,
