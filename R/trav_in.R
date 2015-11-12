7eedce6aaad87af45a216bd6f7e3d7f3b6c60576
Richard Iannone <riannone@me.com>
2015-11-11 21:10:22 -0800

    Rework functions to filter nodes by attribute

--- R/trav_in.R
+++ R/trav_in.R
@@ -34,70 +34,110 @@ trav_in <- function(graph){
     stop("There is no selection of nodes available.")
   }
 
-  if (is.null(graph$traversals)){
+  # Get the current selection of nodes
+  selected_nodes <- get_selection(graph)$nodes
+
+  # Get all paths leading outward from node in selection
+  for (i in 1:length(selected_nodes)){
+    if (i == 1) predecessors <- vector(mode = "character")
+
+    if (!is.na(get_predecessors(graph, selected_nodes[i])[1])){
+      predecessors <-
+        c(predecessors,
+          get_predecessors(graph = graph, selected_nodes[i]))
+    }
+
+    if (i == length(selected_nodes)){
+      predecessors <- unique(predecessors)
+    }
+  }
+
+  # if no predecessors returned, then there are no paths outward,
+  # so return the same graph object without modifying the node selection
+  if (length(predecessors) == 0){
     return(graph)
   }
 
-  current_selection <- graph$selection$nodes
+  # If a search term provided, filter using a logical expression
+  # or a regex match
+  if (!is.null(search)){
 
-  selection_node <- graph$traversals[[length(graph$traversals)]][1]
+    if (grepl("^>.*", search) | grepl("^<.*", search) |
+        grepl("^==.*", search) | grepl("^!=.*", search)){
+      logical_expression <- TRUE } else {
+        logical_expression <- FALSE
+      }
 
+    if (logical_expression){
 
-  # Determine whether traversals all contain the common starting node
-  for (i in 1:length(graph$traversals)){
-    if (i == 1) starting_node_present <- vector(mode = "logical")
+      for (i in 1:length(predecessors)){
 
-    starting_node <- graph$traversals[[1]][1]
+        if (i == 1){
+          to_nodes <- vector(mode = "character")
+          column_number <- which(colnames(graph$nodes_df) %in% node_attr)
+        }
 
-    if (graph$traversals[[i]][1] == starting_node){
-      starting_node_present <- c(starting_node_present, TRUE)
-    } else {
-      starting_node_present <- c(starting_node_present, FALSE)
+        if (grepl("^>.*", search)){
+          if (as.numeric(get_node_attr(graph,
+                                       nodes = predecessors[i])[1,column_number]) >
+              as.numeric(gsub(">(.*)", "\\1", search))){
+
+            to_nodes <- c(to_nodes, predecessors[i])
+          }
+        }
+
+        if (grepl("^<.*", search)){
+          if (as.numeric(get_node_attr(graph,
+                                       nodes = predecessors[i])[1,column_number]) <
+              as.numeric(gsub("<(.*)", "\\1", search))){
+
+            to_nodes <- c(to_nodes, predecessors[i])
+          }
+        }
+
+        if (grepl("^==.*", search)){
+          if (as.numeric(get_node_attr(graph,
+                                       nodes = predecessors[i])[1,column_number]) ==
+              as.numeric(gsub("==(.*)", "\\1", search))){
+
+            to_nodes <- c(to_nodes, predecessors[i])
+          }
+        }
+
+        if (grepl("^!=.*", search)){
+          if (as.numeric(get_node_attr(graph,
+                                       nodes = predecessors[i])[1,column_number]) !=
+              as.numeric(gsub("!=(.*)", "\\1", search))){
+
+            to_nodes <- c(to_nodes, predecessors[i])
+          }
+        }
+      }
     }
+
+    # Filter using a `search` value as a regular expression
+    if (logical_expression == FALSE){
+
+      for (i in 1:length(predecessors)){
+
+        if (i == 1){
+          to_nodes <- vector(mode = "character")
+          column_number <- which(colnames(graph$nodes_df) %in% node_attr)
+        }
+
+        if (grepl(search, get_node_attr(graph,
+                                        nodes = predecessors[i])[1,column_number])){
+
+          to_nodes <- c(to_nodes, predecessors[i])
+        }
+      }
+    }
+
+    predecessors <- to_nodes
   }
 
-  if (all(starting_node_present)){
+  # Update node selection in graph
+  graph$selection$nodes <- predecessors
 
-    graph$selection$nodes <- selection_node
-    graph$traversals <- NULL
-
-    return(graph)
-  }
-
-  if (length(graph$traversals) > 1){
-    for (i in length(graph$traversals):1){
-      if (graph$traversals[[i]][2] %in% selection_node){
-        predecessor_to_selection_node <- graph$traversals[[i]][1]
-      }
-    }
-
-    for (i in length(graph$traversals):1){
-      if (i == length(graph$traversals)){
-        all_selection_nodes <- vector(mode = "character")
-      }
-
-      if (graph$traversals[[i]][1] %in% predecessor_to_selection_node){
-        all_selection_nodes <-
-          c(all_selection_nodes,
-            graph$traversals[[i]][2])
-      }
-    }
-
-    graph$selection$nodes <- all_selection_nodes
-
-    for (i in length(graph$traversals):1){
-      if (graph$traversals[[i]][2] %in% current_selection){
-        graph$traversals[[i]] <- NULL
-      }
-    }
-
-    if (!is.null(graph$traversals)){
-
-      if (length(graph$traversals) == 0){
-        graph$traversals <- NULL
-      }
-    }
-
-    return(graph)
-  }
+  return(graph)
 }
--- R/trav_out.R
+++ R/trav_out.R
@@ -5,6 +5,12 @@
 #' the current nodes in the selection with those nodes traversed to.
 #' @param graph a graph object of class \code{dgr_graph} that is created
 #' using \code{create_graph}.
+#' @param node_attr an optional character vector of node attribute values for
+#' filtering the node ID values returned.
+#' @param search an option to provide a logical expression with a comparison
+#' operator (\code{>}, \code{<}, \code{==}, or \code{!=}) followed by a number
+#' for numerical filtering, or, a regular expression for filtering the nodes
+#' returned through string matching.
 #' @examples
 #' \dontrun{
 #' library(magrittr)
@@ -18,8 +24,8 @@
 #'
 #' # Starting from node "1", traverse nodes twice then get the node
 #' # attribute information at the landing nodes
-#' graph %>% select_nodes("1") %>%
-#'   trav_out() %>% trav_out() %>%
+#' graph %>% select_nodes(nodes = "1") %>%
+#'   trav_out %>% trav_out %>%
 #'   get_node_attr_from_selection()
 #' #>   nodes type label
 #' #> 4     4          4
@@ -27,42 +33,118 @@
 #' @return a graph object of class \code{dgr_graph}.
 #' @export trav_out
 
-trav_out <- function(graph){
+trav_out <- function(graph,
+                     node_attr = NULL,
+                     search = NULL){
 
   if (is.null(graph$selection$nodes)){
     stop("There is no selection of nodes available.")
   }
 
+  # Get the current selection of nodes
+  selected_nodes <- get_selection(graph)$nodes
+
   # Get all paths leading outward from node in selection
-  distance_1_paths <-
-    get_paths(graph = graph,
-              from = graph$selection$nodes,
-              distance = 1)
+  for (i in 1:length(selected_nodes)){
+    if (i == 1) successors <- vector(mode = "character")
 
-  # if NA returned, then there are no paths outward, so return
-  # the same graph object without modifying the node selection
-  if (length(distance_1_paths) == 1 & is.na(distance_1_paths)[1]){
-    return(graph)
-  }
+    if (!is.na(get_successors(graph, selected_nodes[i])[1])){
+      successors <-
+        c(successors,
+          get_successors(graph = graph, selected_nodes[i]))
+    }
 
-  # For all valid paths returned, extract the nodes traversed to
-  for (i in 1:length(distance_1_paths)){
-
-    if (i == 1) landing_nodes <- vector(mode = "character")
-
-    landing_nodes <-
-      c(landing_nodes,
-        distance_1_paths[[i]][length(distance_1_paths[[i]])])
-
-    if (i == length(distance_1_paths)){
-      landing_nodes <- unique(landing_nodes)
+    if (i == length(selected_nodes)){
+      successors <- unique(successors)
     }
   }
 
-  # Update node selection in graph
-  graph$selection$nodes <- landing_nodes
+  # if no successors returned, then there are no paths outward,
+  # so return the same graph object without modifying the node selection
+  if (length(successors) == 0){
+    return(graph)
+  }
 
-  graph$traversals <- c(graph$traversals, distance_1_paths)
+  # If a search term provided, filter using a logical expression
+  # or a regex match
+  if (!is.null(search)){
+
+    if (grepl("^>.*", search) | grepl("^<.*", search) |
+        grepl("^==.*", search) | grepl("^!=.*", search)){
+      logical_expression <- TRUE } else {
+        logical_expression <- FALSE
+      }
+
+    if (logical_expression){
+
+      for (i in 1:length(successors)){
+
+        if (i == 1){
+          to_nodes <- vector(mode = "character")
+          column_number <- which(colnames(graph$nodes_df) %in% node_attr)
+        }
+
+        if (grepl("^>.*", search)){
+          if (as.numeric(get_node_attr(graph,
+                                       nodes = successors[i])[1,column_number]) >
+              as.numeric(gsub(">(.*)", "\\1", search))){
+
+            to_nodes <- c(to_nodes, successors[i])
+          }
+        }
+
+        if (grepl("^<.*", search)){
+          if (as.numeric(get_node_attr(graph,
+                                       nodes = successors[i])[1,column_number]) <
+              as.numeric(gsub("<(.*)", "\\1", search))){
+
+            to_nodes <- c(to_nodes, successors[i])
+          }
+        }
+
+        if (grepl("^==.*", search)){
+          if (as.numeric(get_node_attr(graph,
+                                       nodes = successors[i])[1,column_number]) ==
+              as.numeric(gsub("==(.*)", "\\1", search))){
+
+            to_nodes <- c(to_nodes, successors[i])
+          }
+        }
+
+        if (grepl("^!=.*", search)){
+          if (as.numeric(get_node_attr(graph,
+                                       nodes = successors[i])[1,column_number]) !=
+              as.numeric(gsub("!=(.*)", "\\1", search))){
+
+            to_nodes <- c(to_nodes, successors[i])
+          }
+        }
+      }
+    }
+
+    # Filter using a `search` value as a regular expression
+    if (logical_expression == FALSE){
+
+      for (i in 1:length(successors)){
+
+        if (i == 1){
+          to_nodes <- vector(mode = "character")
+          column_number <- which(colnames(graph$nodes_df) %in% node_attr)
+        }
+
+        if (grepl(search, get_node_attr(graph,
+                                        nodes = successors[i])[1,column_number])){
+
+          to_nodes <- c(to_nodes, successors[i])
+        }
+      }
+    }
+
+    successors <- to_nodes
+  }
+
+  # Update node selection in graph
+  graph$selection$nodes <- successors
 
   return(graph)
 }
