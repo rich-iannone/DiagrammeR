a219ef35cd799673ca565bb71906ec1acaf70aa6
Richard Iannone <riannone@me.com>
2015-11-14 21:59:47 -0800

    Remove display of edge label in output data frame

--- R/edge_info.R
+++ R/edge_info.R
@@ -18,7 +18,6 @@
 #' edges <-
 #'   create_edges(from = sample(LETTERS, replace = TRUE),
 #'                to = sample(LETTERS, replace = TRUE),
-#'                label = "edge",
 #'                rel = "letter_to_letter")
 #'
 #' graph <-
@@ -29,36 +28,24 @@
 #'                               "shape = circle"))
 #'
 #' edge_info(graph)
-#' #>    from   to              rel label
-#' #> 1     A    Z letter_to_letter  edge
-#' #> 2     H    U letter_to_letter  edge
-#' #> 3     W    O letter_to_letter  edge
-#' #> 4     U    K letter_to_letter  edge
-#' #> 5     I    V letter_to_letter  edge
-#' #>..   ...  ...              ...   ...
+#' #>    from   to              rel
+#' #> 1     A    Z letter_to_letter
+#' #> 2     H    U letter_to_letter
+#' #> 3     W    O letter_to_letter
+#' #> 4     U    K letter_to_letter
+#' #> 5     I    V letter_to_letter
+#' #>..   ...  ...              ...
 #' }
 #' @export edge_info
 
 edge_info <- function(graph){
 
-  #   if ("edge_from" %in% colnames(graph$edges_df)){
-  #     edge_from <- graph$edges_df$edge_from
-  #   }
-
   if ("from" %in% colnames(graph$edges_df)){
     edge_from <- graph$edges_df$from
   }
 
-  #   if ("edge_to" %in% colnames(graph$edges_df)){
-  #     edge_to <- graph$edges_df$edge_to
-  #   }
-
   if ("to" %in% colnames(graph$edges_df)){
     edge_to <- graph$edges_df$to
-  }
-
-  if ("label" %in% colnames(graph$edges_df)){
-    label <- graph$edges_df$label
   }
 
   if ("rel" %in% colnames(graph$edges_df)){
@@ -69,8 +56,8 @@ edge_info <- function(graph){
   # that doesn't need to consider any edge information
   if (is.null(graph$edges_df)){
 
-    edge_properties <- as.data.frame(mat.or.vec(nr = 0, nc = 4))
-    colnames(edge_properties) <- c("from", "to", "rel", "label")
+    edge_properties <- as.data.frame(mat.or.vec(nr = 0, nc = 3))
+    colnames(edge_properties) <- c("from", "to", "rel")
 
     return(edge_properties)
   }
@@ -82,22 +69,19 @@ edge_info <- function(graph){
     for (i in 1:length(edge_from)){
 
       if (i == 1){
-        edge_properties <- as.data.frame(mat.or.vec(nr = 0, nc = 4))
-        colnames(edge_properties) <- c("from", "to", "rel", "label")
+        edge_properties <- as.data.frame(mat.or.vec(nr = 0, nc = 3))
+        colnames(edge_properties) <- c("from", "to", "rel")
       }
 
       # Collect information into the 'edge_properties' data frame
       edge_properties[i, 1] <- edge_from[i]
+
       edge_properties[i, 2] <- edge_to[i]
+
       edge_properties[i, 3] <-
         ifelse(exists("rel"),
                rel[which((edge_from %in% edge_from[i]) &
                            (edge_to %in% edge_to[i]))],
-               rep(NA, length(edge_from)))
-      edge_properties[i, 4] <-
-        ifelse(exists("label"),
-               label[which((edge_from %in% edge_from[i]) &
-                             (edge_to %in% edge_to[i]))],
                rep(NA, length(edge_from)))
     }
 
