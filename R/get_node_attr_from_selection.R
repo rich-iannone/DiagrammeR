0d62c2fce9b4fec0fab93316d92c16cf12b479da
Richard Iannone <riannone@me.com>
2015-11-19 02:41:27 -0800

    Make correction to function call

--- R/get_node_attr_from_selection.R
+++ R/get_node_attr_from_selection.R
@@ -46,28 +46,28 @@ get_node_attr_from_selection <- function(graph,
     stop("There is no selection of nodes available.")
   }
 
-  nodes_df <- get_node_attr(graph, nodes = graph$selection$nodes)
+  nodes_df <-
+    get_node_df(graph)[which(get_node_df(graph)[,1]
+                             %in% graph$selection$nodes),]
 
-  if (!is.null(node_attr)){
+  if (any(node_attr %in% colnames(nodes_df)[-1])){
 
-    if (any(node_attr %in% colnames(nodes_df)[-1])){
+    nodes_attr_vector <-
+      nodes_df[,which(colnames(nodes_df) %in% node_attr)]
 
-      nodes_attr_vector <-
-        nodes_df[,which(colnames(nodes_df) %in% node_attr)]
+    if (!is.null(mode)){
+      if (mode == "numeric"){
+        nodes_attr_vector <- as.numeric(nodes_attr_vector)
 
-      if (!is.null(mode)){
-        if (mode == "numeric"){
-          nodes_attr_vector <- as.numeric(nodes_attr_vector)
+        nodes_attr_vector <-
+          nodes_attr_vector[which(!is.na(nodes_attr_vector))]
+      }
 
-          nodes_attr_vector <-
-            nodes_attr_vector[which(!is.na(nodes_attr_vector))]
-        }
-
-        if (mode == "character"){
-          nodes_attr_vector <- as.character(nodes_attr_vector)
-        }
+      if (mode == "character"){
+        nodes_attr_vector <- as.character(nodes_attr_vector)
       }
     }
+
 
     # Place vector of node attributes as a deposit in the graph
     graph$deposit <- nodes_attr_vector
