170f2355ed935f04186c1e7886f6077d88d3fe84
Richard Iannone <riannone@me.com>
2015-11-24 16:57:35 -0800

    Add several tests for traversals

--- tests/testthat/test-traversals.R
+++ tests/testthat/test-traversals.R
@@ -25,10 +25,19 @@ test_that("simple traversals across nodes is possible", {
   # Expect that node `1` is the current selection
   expect_equal(get_selection(graph)[[1]], "1")
 
+  # Traverse from node `1` to `2`, then, traverse nodes
+  # in both directions
+  graph <-
+    graph %>% trav_out %>% trav_both
+
+  # Expect that nodes `1` and `3` are in the current selection
+  expect_true(all(c("1", "3") %in% get_selection(graph)[[1]]))
+
   # Traverse by moving from nodes onto edges, then,
   # onto nodes; from `1` to `4`
   graph <-
-    graph %>% trav_out_edge %>% trav_in_node %>%
+    graph %>% clear_selection %>% select_nodes(nodes = 1) %>%
+    trav_out_edge %>% trav_in_node %>%
     trav_out_edge %>% trav_in_node %>%
     trav_out_edge %>% trav_in_node
 
@@ -75,4 +84,16 @@ test_that("simple traversals across nodes is possible", {
 
   # Expect that nodes `4` and `5` are still in the current selection
   expect_equal(get_selection(graph)[[1]], c("4", "5"))
+
+  # Expect an error if attempting to perform a node traversal without
+  # any selection of nodes
+  graph <- graph %>% clear_selection()
+  expect_error(graph %>% trav_in)
+  expect_error(graph %>% trav_out)
+  expect_error(graph %>% trav_both)
+  expect_error(graph %>% trav_in_node)
+  expect_error(graph %>% trav_out_node)
+  expect_error(graph %>% trav_in_edge)
+  expect_error(graph %>% trav_out_edge)
+
 })
