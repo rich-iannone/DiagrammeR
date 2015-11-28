888e9149a9378cd47a688fb1b81cc08edea017d6
Richard Iannone <riannone@me.com>
2015-11-27 23:01:18 -0800

    Remove redundant statement

--- R/trav_both.R
+++ R/trav_both.R
@@ -41,9 +41,6 @@ trav_both <- function(graph,
     }
   }
 
-  # Get the current selection of nodes
-  selected_nodes <- get_selection(graph)$nodes
-
   # Get all paths leading inward from node in selection
   for (i in 1:length(selected_nodes)){
     if (i == 1) predecessors <- vector(mode = "character")
