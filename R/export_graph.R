707e5d80643425579e02cc48fcbe4b746c170e86
Richard Iannone <riannone@me.com>
2016-02-02 23:20:36 -0800

    Add function 'export_graph'

---
+++ R/export_graph.R
@@ -0,0 +1,50 @@
+#' Export a graph to various file formats
+#' @description Export a graph to a variety of file formats such as
+#' PNG, PDF, SVG, and PostScript.
+#' @param graph a graph object.
+#' @param file_name the name of the exported file (including it's extension).
+#' @param file_type the type of file to be exported. Options are:
+#' \code{png}, \code{pdf}, \code{svg}, and \code{ps}.
+#' @param width output width in pixels or \code{NULL} for default.
+#' @param height output height in pixels or \code{NULL} for default.
+#' @import rsvg
+#' @export export_graph
+
+export_graph <- function(graph,
+                         file_name = NULL,
+                         file_type = NULL,
+                         width = NULL,
+                         height = NULL){
+
+  if (!("DiagrammeRsvg" %in% rownames(installed.packages()))){
+    print("To use this function, please install the `DiagrammeRsvg` package using `devtools::install_github('rich-iannone/DiagrammeR')")
+  }
+
+  if (file_type == "PNG"){
+    rsvg_png(charToRaw(DiagrammeRsvg::export_svg(grViz(graph$dot_code))),
+             file = file_name,
+             width = width,
+             height = height)
+  }
+
+  if (file_type == "PDF" | file_type == "pdf"){
+    rsvg_pdf(charToRaw(DiagrammeRsvg::export_svg(grViz(graph$dot_code))),
+             file = file_name,
+             width = width,
+             height = height)
+  }
+
+  if (file_type == "SVG" | file_type == "svg"){
+    rsvg_svg(charToRaw(DiagrammeRsvg::export_svg(grViz(graph$dot_code))),
+             file = file_name,
+             width = width,
+             height = height)
+  }
+
+  if (file_type == "PS" | file_type == "ps"){
+    rsvg_ps(charToRaw(DiagrammeRsvg::export_svg(grViz(graph$dot_code))),
+            file = file_name,
+            width = width,
+            height = height)
+  }
+}
