7213872ff6f999922aa8bfae0460e1c9a6d7fb23
Richard Iannone <riannone@me.com>
2016-04-27 19:11:07 -0700

    Clean up code throughout

--- R/series_info.R
+++ R/series_info.R
@@ -1,27 +1,34 @@
-#' Get detailed information on a graph series
-#' @description Obtain a data frame with detailed information on the graphs
-#' within a graph series.
-#' @param graph_series a graph series object of type \code{dgr_graph_1D}.
-#' @return a data frame containing information on the graphs within the
-#' supplied graph series.
+#' Get information on a graph series
+#' @description Obtain a data frame with information
+#' on the graphs within a graph series.
+#' @param graph_series a graph series object of type
+#' \code{dgr_graph_1D}.
+#' @return a data frame containing information on the
+#' graphs within the supplied graph series.
 #' @examples
 #' \dontrun{
-#' # Create three graphs (using \code{pipeR} for speed)
-#' # and create a graph series using those graphs
-#' library(pipeR)
+#' # Create three graphs
+#' graph_1 <-
+#'   create_graph() %>%
+#'   add_node("a") %>%
+#'   add_node("b") %>%
+#'   add_node("c") %>%
+#'   add_edge("a", "c") %>%
+#'   add_edge("a", "b") %>%
+#'   add_edge("b", "c")
 #'
-#' graph_1 <- create_graph() %>%
-#'   add_node("a") %>% add_node("b") %>% add_node("c") %>%
-#'   add_edge("a", "c") %>% add_edge("a", "b") %>% add_edge("b", "c")
+#' graph_2 <-
+#'   graph_1 %>%
+#'   add_node("d") %>%
+#'   add_edge("d", "c")
 #'
-#' graph_2 <- graph_1 %>%
-#'   add_node("d") %>% add_edge("d", "c")
-#'
-#' graph_3 <- graph_2 %>%
-#'   add_node("e") %>% add_edge("e", "b")
+#' graph_3 <-
+#'   graph_2 %>%
+#'   add_node("e") %>%
+#'   add_edge("e", "b")
 #'
 #' # Create an empty graph series
-#' series <- create_series(series_type = "sequential")
+#' series <- create_series()
 #'
 #' # Add graphs to the graph series
 #' series <- graph_1 %>>% add_to_series(series)
@@ -37,60 +44,72 @@
 #' }
 #' @export series_info
 
-series_info <- function(graph_series){
+series_info <- function(graph_series) {
 
-  graphs_in_series <- graph_count(graph_series)
+  graphs_in_series <-
+    graph_count(graph_series)
 
   series_properties <-
-    as.data.frame(mat.or.vec(nr = graphs_in_series, nc = 7))
+    as.data.frame(
+      mat.or.vec(nr = graphs_in_series, nc = 7))
+
   colnames(series_properties) <-
-    c("graph", "name", "date_time", "tz", "nodes", "edges", "directed")
+    c("graph", "name", "date_time", "tz",
+      "nodes", "edges", "directed")
 
-  series_properties[,1] <- as.numeric(series_properties[,1])
-  series_properties[,2] <- as.character(series_properties[,2])
-  series_properties[,3] <- as.character(series_properties[,3])
-  series_properties[,4] <- as.character(series_properties[,4])
-  series_properties[,5] <- as.numeric(series_properties[,5])
-  series_properties[,6] <- as.numeric(series_properties[,6])
-  series_properties[,7] <- as.logical(series_properties[,7])
+  series_properties[,1] <-
+    as.numeric(series_properties[,1])
+  series_properties[,2] <-
+    as.character(series_properties[,2])
+  series_properties[,3] <-
+    as.character(series_properties[,3])
+  series_properties[,4] <-
+    as.character(series_properties[,4])
+  series_properties[,5] <-
+    as.numeric(series_properties[,5])
+  series_properties[,6] <-
+    as.numeric(series_properties[,6])
+  series_properties[,7] <-
+    as.logical(series_properties[,7])
 
-  if (graphs_in_series == 0){
-
+  if (graphs_in_series == 0) {
     return(series_properties)
   }
 
-  for (i in 1:graphs_in_series){
-
+  for (i in 1:graphs_in_series) {
     series_properties[i, 1] <- i
 
-    if (!is.null(graph_series$graphs[[i]]$graph_name)){
-    series_properties[i, 2] <-
-      graph_series$graphs[[i]]$graph_name
+    if (!is.null(graph_series$graphs[[i]]$graph_name)) {
+      series_properties[i, 2] <-
+        graph_series$graphs[[i]]$graph_name
     } else {
       series_properties[i, 2] <- NA
     }
 
-    if (!is.null(graph_series$graphs[[i]]$graph_time)){
+    if (!is.null(graph_series$graphs[[i]]$graph_time)) {
       series_properties[i, 3] <-
         graph_series$graphs[[i]]$graph_time
     } else {
       series_properties[i, 3] <- NA
     }
 
-    if (!is.null(graph_series$graphs[[i]]$graph_tz)){
+    if (!is.null(graph_series$graphs[[i]]$graph_tz)) {
       series_properties[i, 4] <-
         graph_series$graphs[[i]]$graph_tz
     } else {
       series_properties[i, 4] <- NA
     }
 
-    series_properties[i, 5] <- ifelse(!is.null(nrow(graph_series$graphs[[i]]$nodes_df)),
-                                      nrow(graph_series$graphs[[i]]$nodes_df), 0)
+    series_properties[i, 5] <-
+      ifelse(!is.null(nrow(graph_series$graphs[[i]]$nodes_df)),
+             nrow(graph_series$graphs[[i]]$nodes_df), 0)
 
-    series_properties[i, 6] <- ifelse(!is.null(nrow(graph_series$graphs[[i]]$edges_df)),
-                                      nrow(graph_series$graphs[[i]]$edges_df), 0)
+    series_properties[i, 6] <-
+      ifelse(!is.null(nrow(graph_series$graphs[[i]]$edges_df)),
+             nrow(graph_series$graphs[[i]]$edges_df), 0)
 
-    series_properties[i, 7] <- is_graph_directed(graph_series$graphs[[i]])
+    series_properties[i, 7] <-
+      is_graph_directed(graph_series$graphs[[i]])
   }
 
   return(series_properties)
