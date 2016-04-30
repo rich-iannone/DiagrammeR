578ca6092bb8b0be1402f42ac626afbce30a8a77
Richard Iannone <riannone@me.com>
2016-04-29 17:45:53 -0700

    Clean up code throughout

--- R/export_csv.R
+++ R/export_csv.R
@@ -18,6 +18,7 @@
 #' The \code{graphframes} option modifies column names
 #' to match those required by the Spark GraphFrames
 #' package.
+#'
 #' @export export_csv
 
 export_csv <- function(graph,
@@ -29,41 +30,43 @@ export_csv <- function(graph,
   nodes_df <- get_node_df(graph)
   edges_df <- get_edge_df(graph)
 
-  # Modify column names for easier import into Neo4J
-  # via `LOAD CSV`
-  if (colnames_type == "neo4j") {
+  if (!is.null(colnames_type)) {
 
-  # Modify column names in the ndf
-  colnames(nodes_df)[1:3] <-
-    c("nodes:ID", ":LABEL", "label")
+    # Modify column names for easier import into Neo4J
+    # via `LOAD CSV` or the `neo4j-import` tool
+    if (colnames_type == "neo4j") {
 
-  # Modify column names in the edf
-  colnames(edges_df)[1:3] <-
-    c(":START_ID", ":END_ID", ":TYPE")
-  }
+      # Modify column names in the ndf
+      colnames(nodes_df)[1:3] <-
+        c("nodes:ID", ":LABEL", "label")
 
-  # Modify column names for easier import into a
-  # Spark GraphFrame after using the `csvreader`
-  # package
-  if (colnames_type == "graphframes") {
+      # Modify column names in the edf
+      colnames(edges_df)[1:3] <-
+        c(":START_ID", ":END_ID", ":TYPE")
+    }
 
-    # Modify column names in the ndf
-    colnames(nodes_df)[1] <- "id"
+    # Modify column names for easier import into a
+    # Spark GraphFrame after using the `csvreader`
+    # package
+    if (colnames_type == "graphframes") {
 
-    # Modify column names in the edf
-    colnames(edges_df)[1:2] <-
-      c("src", "dst")
+      # Modify column names in the ndf
+      colnames(nodes_df)[1] <- "id"
+
+      # Modify column names in the edf
+      colnames(edges_df)[1:2] <-
+        c("src", "dst")
+    }
   }
 
   # Write the CSV files to the output directory
   write.csv(nodes_df,
             file = paste0(output_path,
                           "/", ndf_name),
-            row.names = FALSE)
+            row.names = FALSE, quote = FALSE)
 
   write.csv(edges_df,
             file = paste0(output_path,
                           "/", edf_name),
-            row.names = FALSE)
+            row.names = FALSE, quote = FALSE)
 }
-
