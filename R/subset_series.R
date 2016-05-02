68a5076e6e8e60c2e4221f21c78a7b3cb896b020
Richard Iannone <riannone@me.com>
2016-05-01 23:01:39 -0700

    Clean up code throughout

--- R/subset_series.R
+++ R/subset_series.R
@@ -1,69 +1,101 @@
 #' Subset a graph series object
-#' @description Subsetting a graph series by the graphs' index positions in
-#' the graph series or through selection via graphs' date-time attributes.
-#' @param graph_series a graph series object of type \code{dgr_graph_1D}.
-#' @param by either \code{number}, which allows for subsetting of the graph
-#' series by graph indices, or \code{time} which for graph series objects of
-#' type \code{temporal} allows for a subsetting of graphs by a date-time or
-#' time range.
-#' @param values where the subsetting of the graph series by to occur via graph
-#' indices (where \code{by = number}), provide a vector of those indices; when
-#' subsetting by time (where \code{by = time}), a range of times can be
-#' provided as a vector.
-#' @param tz the time zone (\code{tz}) corresponding to dates or date-time
-#' string provided in \code{values} (if \code{by = "date"}).
-#' @return a graph series object of type \code{dgr_graph_1D}.
+#' @description Subsetting a graph series by the
+#' graphs' index positions in the graph series or
+#' through selection via graphs' date-time attributes.
+#' @param graph_series a graph series object of type
+#' \code{dgr_graph_1D}.
+#' @param by either \code{number}, which allows for
+#' subsetting of the graph series by graph indices, or
+#' \code{time} which for graph series objects of type
+#' \code{temporal} allows for a subsetting of graphs by
+#' a date-time or time range.
+#' @param values where the subsetting of the graph
+#' series by to occur via graph indices (where
+#' \code{by = number}), provide a vector of those
+#' indices; when subsetting by time (where
+#' \code{by = time}), a range of times can be provided
+#' as a vector.
+#' @param tz the time zone (\code{tz}) corresponding to
+#' dates or date-time string provided in \code{values}
+#' (if \code{by = "date"}).
+#' @return a graph series object of type
+#' \code{dgr_graph_1D}.
 #' @examples
 #' \dontrun{
-#' # Create three graphs (using \code{pipeR} for speed)
-#' # and create a graph series using those graphs
 #' library(magrittr)
 #'
+#' # Create three graphs
 #' graph_time_1 <-
-#'   create_graph(graph_name = "graph_with_time_1",
-#'                graph_time = "2015-03-25 03:00",
-#'                graph_tz = "GMT") %>%
-#'   add_node("a") %>% add_node("b") %>% add_node("c") %>%
-#'   add_edge("a", "c") %>% add_edge("a", "b") %>% add_edge("b", "c")
-#' 
+#'   create_graph(
+#'     graph_name = "graph_with_time_1",
+#'     graph_time = "2015-03-25 03:00",
+#'     graph_tz = "GMT") %>%
+#'   add_node("a") %>%
+#'   add_node("b") %>%
+#'   add_node("c") %>%
+#'   add_edge("a", "c") %>%
+#'   add_edge("a", "b") %>%
+#'   add_edge("b", "c")
+#'
 #' graph_time_2 <-
-#'   create_graph(graph_name = "graph_with_time_2",
-#'                graph_time = "2015-03-26 03:00",
-#'                graph_tz = "GMT") %>%
-#'   add_node("d") %>% add_node("e") %>% add_node("f") %>%
-#'   add_edge("d", "f") %>% add_edge("d", "e") %>% add_edge("e", "f")
-#' 
+#'   create_graph(
+#'     graph_name = "graph_with_time_2",
+#'     graph_time = "2015-03-26 03:00",
+#'     graph_tz = "GMT") %>%
+#'   add_node("d") %>%
+#'   add_node("e") %>%
+#'   add_node("f") %>%
+#'   add_edge("d", "f") %>%
+#'   add_edge("d", "e") %>%
+#'   add_edge("e", "f")
+#'
 #' graph_time_3 <-
-#'   create_graph(graph_name = "graph_with_time_3",
-#'                graph_time = "2015-03-27 15:00",
-#'                graph_tz = "GMT") %>%
-#'   add_node("x") %>% add_node("y") %>% add_node("z") %>%
-#'   add_edge("x", "z") %>% add_edge("x", "y") %>% add_edge("y", "z")
+#'   create_graph(
+#'     graph_name = "graph_with_time_3",
+#'     graph_time = "2015-03-27 15:00",
+#'     graph_tz = "GMT") %>%
+#'   add_node("x") %>%
+#'   add_node("y") %>%
+#'   add_node("z") %>%
+#'   add_edge("x", "z") %>%
+#'   add_edge("x", "y") %>%
+#'   add_edge("y", "z")
 #'
 #' # Create an empty graph series
-#' series_temporal <- create_series(series_type = "temporal")
+#' series_temporal <-
+#'   create_series(series_type = "temporal")
 #'
 #' # Add graphs to the graph series
-#' series_temporal <- graph_time_1 %>% add_to_series(series_temporal)
-#' series_temporal <- graph_time_2 %>% add_to_series(series_temporal)
-#' series_temporal <- graph_time_3 %>% add_to_series(series_temporal)
+#' series_temporal <-
+#'   graph_time_1 %>%
+#'     add_to_series(series_temporal)
+#'
+#' series_temporal <-
+#'   graph_time_2 %>%
+#'     add_to_series(series_temporal)
+#'
+#' series_temporal <-
+#'   graph_time_3 %>%
+#'     add_to_series(series_temporal)
 #'
 #' # Subset graph series by sequence
 #' series_sequence_subset <-
-#'   subset_series(graph_series = series_temporal,
-#'                 by = "number",
-#'                 values = 2)
+#'   subset_series(
+#'     graph_series = series_temporal,
+#'     by = "number",
+#'     values = 2)
 #'
 #' graph_count(series_sequence_subset)
 #' #> [1] 1
 #'
 #' # Subset graph series by date-time
 #' series_time_subset <-
-#'   subset_series(graph_series = series_temporal,
-#'                 by = "time",
-#'                 values = c("2015-03-25 12:00",
-#'                            "2015-03-26 12:00"),
-#'                 tz = "GMT")
+#'   subset_series(
+#'     graph_series = series_temporal,
+#'     by = "time",
+#'     values = c("2015-03-25 12:00",
+#'                "2015-03-26 12:00"),
+#'     tz = "GMT")
 #'
 #' graph_count(series_time_subset)
 #' #> [1] 1
@@ -73,17 +105,17 @@
 subset_series <- function(graph_series,
                           by = "number",
                           values,
-                          tz = NULL){
+                          tz = NULL) {
 
-  if (graph_count(graph_series = graph_series) == 0){
+  if (graph_count(graph_series = graph_series) == 0) {
 
     return(graph_series)
   }
 
-  if (by == "number"){
+  if (by == "number") {
 
     # validate the value provided for 'values'
-    if (class(values) != "numeric"){
+    if (class(values) != "numeric") {
 
       return(graph_series)
     }
@@ -100,7 +132,7 @@ subset_series <- function(graph_series,
     graphs_to_remove <-
       sort(graphs_to_remove, decreasing = TRUE)
 
-    for (i in graphs_to_remove){
+    for (i in graphs_to_remove) {
 
       graph_series <-
         remove_from_series(graph_series = graph_series,
@@ -110,10 +142,10 @@ subset_series <- function(graph_series,
     return(graph_series)
   }
 
-  if (by == "time"){
+  if (by == "time") {
 
     # validate the value provided for 'values'
-    if (class(values) == "numeric"){
+    if (class(values) == "numeric") {
 
       return(graph_series)
     }
@@ -121,12 +153,12 @@ subset_series <- function(graph_series,
     is_tz_in_correct_format <-
       ifelse(tz %in% OlsonNames(), TRUE, FALSE)
 
-    if (is_tz_in_correct_format == FALSE){
+    if (is_tz_in_correct_format == FALSE) {
 
       return(graph_series)
     }
 
-    for (i in 1:length(values)){
+    for (i in 1:length(values)) {
 
       is_time_in_correct_format <-
         ifelse(grepl("^[0-9]{4}-[0-9]{2}-[0-9]{2}$",
@@ -136,18 +168,18 @@ subset_series <- function(graph_series,
                  grepl("^[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}$",
                        values[i]), TRUE, FALSE)
 
-      if (is_time_in_correct_format == FALSE){
+      if (is_time_in_correct_format == FALSE) {
 
         return(graph_series)
       }
     }
 
     # Create subset based on range
-    if (length(values) == 2){
+    if (length(values) == 2) {
 
-      for (i in 1:graph_count(graph_series = graph_series)){
+      for (i in 1:graph_count(graph_series = graph_series)) {
 
-        if (i == 1){
+        if (i == 1) {
           dates_times_in_series <- vector(mode = "numeric", length = 0)
           tz_in_series <- vector(mode = "character", length = 0)
         }
@@ -160,9 +192,9 @@ subset_series <- function(graph_series,
 
       }
 
-      for (i in 1:length(dates_times_in_series)){
+      for (i in 1:length(dates_times_in_series)) {
 
-        if (i == 1){
+        if (i == 1) {
           dates_times_in_series_with_tz <-
             as.POSIXct(rep("1970-01-01", length(dates_times_in_series)),
                        tz = "GMT")
@@ -184,7 +216,7 @@ subset_series <- function(graph_series,
         sort(graphs_to_remove, decreasing = TRUE)
 
       # Remove selected graphs from the series
-      for (i in graphs_to_remove){
+      for (i in graphs_to_remove) {
 
         graph_series <-
           remove_from_series(graph_series = graph_series,
@@ -195,7 +227,7 @@ subset_series <- function(graph_series,
     }
 
     # Create subset based on exact time
-    if (length(values) == 1){
+    if (length(values) == 1) {
 
 
     }
--- R/trav_in_node.R
+++ R/trav_in_node.R
@@ -1,80 +1,103 @@
-#' Traverse from one or more selected edges toward adjacent inward nodes
-#' @description From a graph object of class \code{dgr_graph} move to
-#' adjacent nodes from a selection of one or more selected edges where the
-#' edges are inward edges to those nodes. This creates a selection of nodes.
-#' An optional filter by node attribute can limit the set of nodes traversed to.
-#' @param graph a graph object of class \code{dgr_graph} that is created
-#' using \code{create_graph}.
-#' @param node_attr an optional character vector of node attribute values for
-#' filtering the node ID values returned.
-#' @param match an option to provide a logical expression with a comparison
-#' operator (\code{>}, \code{<}, \code{==}, or \code{!=}) followed by a number
-#' for numerical filtering, or, a character string for filtering the edges
-#' returned through string matching.
+#' Traverse from one or more selected edges toward
+#' adjacent inward nodes
+#' @description From a graph object of class
+#' \code{dgr_graph} move to adjacent nodes from a
+#' selection of one or more selected edges where the
+#' edges are inward edges to those nodes. This creates
+#' a selection of nodes. An optional filter by node
+#' attribute can limit the set of nodes traversed to.
+#' @param graph a graph object of class
+#' \code{dgr_graph} that is created using
+#' \code{create_graph}.
+#' @param node_attr an optional character vector of
+#' node attribute values for filtering the node ID
+#' values returned.
+#' @param match an option to provide a logical
+#' expression with a comparison operator (\code{>},
+#' \code{<}, \code{==}, or \code{!=}) followed by a
+#' number for numerical filtering, or, a character
+#' string for filtering the edges returned through
+#' string matching.
 #' @return a graph object of class \code{dgr_graph}.
 #' @export trav_in_node
 
 trav_in_node <- function(graph,
                          node_attr = NULL,
-                         match = NULL){
+                         match = NULL) {
 
-  if (is.null(graph$selection$edges)){
+  if (is.null(graph$selection$edges)) {
     stop("There is no selection of edges available.")
   }
 
-  # Create a selection of nodes using the edge selection
+  # Create a selection of nodes using the
+  # edge selection
   landing_nodes <- unique(graph$selection$edges$to)
 
-  # If a match term provided, filter using a logical expression
-  # or a regex match
-  if (!is.null(match)){
+  # If a match term provided, filter using a
+  # logical expression or a regex match
+  if (!is.null(match)) {
 
-    if (grepl("^>.*", match) | grepl("^<.*", match) |
-        grepl("^==.*", match) | grepl("^!=.*", match)){
+    if (grepl("^>.*", match) |
+        grepl("^<.*", match) |
+        grepl("^==.*", match) |
+        grepl("^!=.*", match)) {
       logical_expression <- TRUE } else {
         logical_expression <- FALSE
       }
 
-    if (logical_expression){
-
-      for (i in 1:length(landing_nodes)){
-
-        if (i == 1){
+    if (logical_expression) {
+      for (i in 1:length(landing_nodes)) {
+        if (i == 1) {
           to_nodes <- vector(mode = "character")
-          column_number <- which(colnames(graph$nodes_df) %in% node_attr)
+
+          column_number <-
+            which(colnames(graph$nodes_df) %in%
+                    node_attr)
         }
 
-        if (grepl("^>.*", match)){
-          if (as.numeric(get_node_df(graph)[which(get_node_df(graph)[,1] %in%
-                                                  landing_nodes[i]), column_number]) >
-              as.numeric(gsub(">(.*)", "\\1", match))){
+        if (grepl("^>.*", match)) {
+          if (as.numeric(
+            get_node_df(graph)[
+              which(get_node_df(graph)[,1] %in%
+                    landing_nodes[i]),
+              column_number]) >
+            as.numeric(gsub(">(.*)", "\\1", match))) {
 
             to_nodes <- c(to_nodes, landing_nodes[i])
           }
         }
 
-        if (grepl("^<.*", match)){
-          if (as.numeric(get_node_df(graph)[which(get_node_df(graph)[,1] %in%
-                                                  landing_nodes[i]), column_number]) <
-              as.numeric(gsub("<(.*)", "\\1", match))){
+        if (grepl("^<.*", match)) {
+          if (as.numeric(
+            get_node_df(graph)[
+              which(get_node_df(graph)[,1] %in%
+                    landing_nodes[i]),
+              column_number]) <
+            as.numeric(gsub("<(.*)", "\\1", match))) {
 
             to_nodes <- c(to_nodes, landing_nodes[i])
           }
         }
 
-        if (grepl("^==.*", match)){
-          if (as.numeric(get_node_df(graph)[which(get_node_df(graph)[,1] %in%
-                                                  landing_nodes[i]), column_number]) ==
-              as.numeric(gsub("==(.*)", "\\1", match))){
+        if (grepl("^==.*", match)) {
+          if (as.numeric(
+            get_node_df(graph)[
+              which(get_node_df(graph)[,1] %in%
+                    landing_nodes[i]),
+              column_number]) ==
+            as.numeric(gsub("==(.*)", "\\1", match))) {
 
             to_nodes <- c(to_nodes, landing_nodes[i])
           }
         }
 
-        if (grepl("^!=.*", match)){
-          if (as.numeric(get_node_df(graph)[which(get_node_df(graph)[,1] %in%
-                                                  landing_nodes[i]), column_number]) !=
-              as.numeric(gsub("!=(.*)", "\\1", match))){
+        if (grepl("^!=.*", match)) {
+          if (as.numeric(
+            get_node_df(graph)[
+              which(get_node_df(graph)[,1] %in%
+                    landing_nodes[i]),
+              column_number]) !=
+            as.numeric(gsub("!=(.*)", "\\1", match))) {
 
             to_nodes <- c(to_nodes, landing_nodes[i])
           }
@@ -83,22 +106,26 @@ trav_in_node <- function(graph,
     }
 
     # Filter using a `match` value
-    if (logical_expression == FALSE){
+    if (logical_expression == FALSE) {
 
-      if (is.numeric(match)){
+      if (is.numeric(match)) {
         match <- as.character(match)
       }
 
-      for (i in 1:length(landing_nodes)){
+      for (i in 1:length(landing_nodes)) {
 
-        if (i == 1){
+        if (i == 1) {
           to_nodes <- vector(mode = "character")
-          column_number <- which(colnames(graph$nodes_df) %in% node_attr)
+          column_number <-
+            which(colnames(graph$nodes_df) %in%
+                    node_attr)
         }
 
         if (match ==
-            get_node_df(graph)[which(get_node_df(graph)[,1] %in%
-                                     landing_nodes[i]), column_number]){
+            get_node_df(graph)[
+              which(get_node_df(graph)[,1] %in%
+                    landing_nodes[i]),
+              column_number]) {
 
           to_nodes <- c(to_nodes, landing_nodes[i])
         }
@@ -108,9 +135,9 @@ trav_in_node <- function(graph,
     landing_nodes <- to_nodes
   }
 
-  # If there are no valid traversals, return the same graph object
-  # without modifying the node selection
-  if (length(landing_nodes) == 0){
+  # If there are no valid traversals, return the same
+  # graph object without modifying the node selection
+  if (length(landing_nodes) == 0) {
     return(graph)
   }
 
