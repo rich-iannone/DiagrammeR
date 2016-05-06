e14e217ba3bbcb351ff763c1910a13bd29b284eb
Richard Iannone <riannone@me.com>
2016-05-05 23:36:58 -0700

    Clean up code throughout

--- R/withdraw_values.R
+++ R/withdraw_values.R
@@ -1,15 +1,18 @@
 #' Withdraw a stored vector from a graph object
-#' @description Get the vector of node or edge attribute values stored
-#' in a graph object of class \code{dgr_graph}.
-#' @param graph a graph object of class \code{dgr_graph} that is created
-#' using \code{create_graph}.
+#' @description Get the vector of node or edge
+#' attribute values stored in a graph object of class
+#' \code{dgr_graph}.
+#' @param graph a graph object of class
+#' \code{dgr_graph} that is created using
+#' \code{create_graph}.
 #' @return a vector of node or edge attribute values.
 #' @export withdraw_values
 
-withdraw_values <- function(graph){
+withdraw_values <- function(graph) {
 
-  # If there is no deposited vector available, return NA value
-  if (is.null(graph$deposit)){
+  # If there is no deposited vector available,
+  # return NA value
+  if (is.null(graph$deposit)) {
     return(NA)
   }
 
