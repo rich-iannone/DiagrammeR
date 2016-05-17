2c90071a824b4c1e913c7cc662231b8d76b9e6e1
Richard Iannone <riannone@me.com>
2016-05-16 17:38:14 -0700

    Add example to roxygen @examples section

--- R/get_node_df.R
+++ R/get_node_df.R
@@ -5,6 +5,41 @@
 #' \code{dgr_graph} that is created using
 #' \code{create_graph}.
 #' @return a node data frame.
+#' @examples
+#' \dontrun{
+#' library(magrittr)
+#'
+#' # Create a graph using several piped functions
+#' graph <-
+#'   create_graph() %>%
+#'   add_n_nodes(1, "a") %>%
+#'   select_last_node %>%
+#'   add_n_nodes_from_selection(5, "b") %>%
+#'   select_nodes_by_id(1) %>%
+#'   set_node_attr_with_selection(
+#'     "value", 25.3) %>%
+#'   clear_selection %>%
+#'   select_nodes_by_id(2:4) %>%
+#'   set_node_attr_with_selection(
+#'     "color", "grey70") %>%
+#'   invert_selection %>%
+#'   set_node_attr_with_selection(
+#'     "color", "grey80") %>%
+#'   clear_selection %>%
+#'   select_nodes("value", "<15") %>%
+#'   set_node_attr_with_selection(
+#'     "value", 10.0)
+#'
+#' # Get the graph's internal node data frame (ndf)
+#' graph %>% get_node_df
+#' #> nodes type label value  color
+#' #> 1     1    a        25.3 grey80
+#' #> 2     2    b          10 grey70
+#' #> 3     3    b          10 grey70
+#' #> 4     4    b          10 grey70
+#' #> 5     5    b          10 grey80
+#' #> 6     6    b          10 grey80
+#' }
 #' @export get_node_df
 
 get_node_df <- function(graph) {
