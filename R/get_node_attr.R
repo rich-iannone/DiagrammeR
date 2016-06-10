37c5dcf9f91764fca75ffc2c0276768e44b2cd68
Richard Iannone <riannone@me.com>
2016-06-09 17:43:38 -0700

    Add example to roxygen @examples section

--- R/get_node_attr.R
+++ R/get_node_attr.R
@@ -1,7 +1,7 @@
-#' Get node attributes
+#' Get node attribute values
 #' @description From a graph object of class
 #' \code{dgr_graph} or a node data frame, get node
-#' attribute properties for one or more nodes.
+#' attribute values for one or more nodes.
 #' @param x either a graph object of class
 #' \code{dgr_graph} that is created using
 #' \code{create_graph}, or a node data frame.
@@ -12,6 +12,35 @@
 #' to get values.
 #' @return a named vector of node attribute values for
 #' the attribute given by \code{node_attr} by node ID.
+#' @examples
+#' \dontrun{
+#' library(magrittr)
+#'
+#' # With the `create_random_graph()` function, get
+#' # a simple graph with a node attribute called
+#' # `value`
+#' random_graph <-
+#'   create_random_graph(
+#'     n = 4,
+#'     m = 4,
+#'     directed = TRUE,
+#'     fully_connected = TRUE,
+#'     set_seed = 20)
+#'
+#' # Get all of the values from the `value` node
+#' # attribute as a named vector
+#' random_graph %>%
+#'   get_node_attr("value")
+#' #> 1   2   3   4
+#' #> 9.0 8.0 3.0 5.5
+#'
+#' # To only return node attribute values for specified
+#' # nodes, use the `nodes` argument
+#' random_graph %>%
+#'   get_node_attr("value", nodes = c(1, 3))
+#' #> 1 3
+#' #> 9 3
+#' }
 #' @export get_node_attr
 
 get_node_attr <- function(x,
