cdae9d35fbbf19adaf1f2a2850941140221a3a42
Richard Iannone <riannone@me.com>
2016-06-16 10:15:22 -0700

    Add example to roxygen @examples section

--- R/get_graph_diameter.R
+++ R/get_graph_diameter.R
@@ -5,6 +5,17 @@
 #' \code{dgr_graph}.
 #' @return the graph diameter as a single-length
 #' vector.
+#' @examples
+#' \dontrun{
+#' # Get the graph diameter for a randomly-created
+#' # graph
+#' get_graph_diameter(
+#'   create_random_graph(
+#'     5, 7,
+#'     fully_connected = TRUE,
+#'     set_seed = 20))
+#' #> [1] 4
+#' }
 #' @export get_graph_diameter
 
 get_graph_diameter <- function(graph) {
