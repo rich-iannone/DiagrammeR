64fa94e31f6c689a1915ad375eb77badbe424d48
Richard Iannone <riannone@me.com>
2016-06-16 10:00:17 -0700

    Add function 'get_periphery'

---
+++ R/get_periphery.R
@@ -0,0 +1,33 @@
+#' Get nodes that form the graph periphery
+#' @description Get those nodes that are part of the
+#' graph periphery (i.e., have the maximum eccentricity
+#' in the graph).
+#' @param graph a graph object of class
+#' \code{dgr_graph}.
+#' @return a vector of node IDs.
+#' @examples
+#' \dontrun{
+#' # Get the nodes that are in the graph periphery of
+#' # a randomly-created graph
+#' get_periphery(
+#'   create_random_graph(
+#'     15, 24,
+#'     fully_connected = TRUE,
+#'     set_seed = 20))
+#' #> [1] "4"  "8"  "15"
+#' }
+#' @export get_periphery
+
+get_periphery <- function(graph) {
+
+  # Get the eccentricity for each of the graph's nodes
+  eccentricity <- get_eccentricity(graph)
+
+  # Return the node IDs for all nodes where the
+  # eccentricity is equal to the graph diameter
+  # (i.e., maximum eccentricity)
+  return(names(
+    eccentricity[
+      which(eccentricity %in%
+              max(eccentricity))]))
+}
