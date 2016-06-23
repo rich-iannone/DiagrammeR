934388efca3f1c6d5fc3b27c0762543f48fbcbb5
Richard Iannone <riannone@me.com>
2016-06-22 17:20:14 -0700

    Rename function and file

--- R/cache_edge_count_ws.R
+++ R/cache_edge_count_ws.R
@@ -1,25 +1,25 @@
-#' Deposit a count of edges (available in a
+#' Cache a count of edges (available in a
 #' selection) in the graph
 #' @description From a graph object of class
 #' \code{dgr_graph}, get a count of edges available in
-#' a selection and deposit that value in the graph for
+#' a selection and cache that value in the graph for
 #' later retrieval using \code{withdraw_values}.
 #' @param graph a graph object of class
 #' \code{dgr_graph} that is created using
 #' \code{create_graph}.
 #' @return a graph object of class \code{dgr_graph}.
-#' @export deposit_edge_count_in_selection
+#' @export cache_edge_count_ws
 
-deposit_edge_count_in_selection <- function(graph) {
+cache_edge_count_ws <- function(graph) {
 
   # If no edge selection is available, return the
   # graph unchanged
   if (is.null(graph$selection$edges)) {
     return(graph)
   } else {
-    # Place numeric vector of single length as a
-    # deposit in the graph
-    graph$deposit <- length(graph$selection$edges$from)
+    # Cache numeric vector of single length
+    # in the graph
+    graph$cache <- length(graph$selection$edges$from)
     return(graph)
   }
 }
