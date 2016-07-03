bffcee7ac511cb309537b2f98bd298dae0f34108
Richard Iannone <riannone@me.com>
2016-07-03 12:13:41 -0700

    Add function 'add_edges'

---
+++ R/add_edges.R
@@ -0,0 +1,52 @@
+#' Add one or more edges using a vector object
+#' @description With a graph object of class
+#' \code{dgr_graph}, add one or more edges to the graph
+#' using a character vector.
+#' @param graph a graph object of class
+#' \code{dgr_graph} that is created using
+#' \code{create_graph}.
+#' @param edges a single-length vector with a character
+#' string specifying the edges.
+#' @param rel an optional vector specifying the
+#' relationship between the connected nodes.
+#' @return a graph object of class \code{dgr_graph}.
+#' @examples
+#' \dontrun{
+#' library(magrittr)
+#'
+#' # Create a graph with 10 nodes
+#' graph <-
+#'   create_graph() %>%
+#'   add_n_nodes(10)
+#'
+#' # Add edges between nodes using a character string
+#' graph %<>%
+#'   add_edges(
+#'     "1->2 1->3 2->4 2->5 3->6 3->7 4->8 4->9 5->10")
+#' }
+#' @export add_edges
+
+add_edges <- function(graph,
+                      edges,
+                      rel = NULL) {
+
+  edges_split <-
+    unlist(strsplit(edges, " "))
+
+  from <-
+    sapply(strsplit(edges_split, "->"), "[[", 1)
+
+  to <-
+    sapply(strsplit(edges_split, "->"), "[[", 2)
+
+  new_edges <-
+    create_edges(
+      from = from,
+      to = to)
+
+  dgr_graph <-
+    add_edge_df(graph,
+                new_edges)
+
+  return(dgr_graph)
+}
