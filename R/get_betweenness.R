90253aa167b5eb9065b4844f09b110dbad50af48
Richard Iannone <riannone@me.com>
2016-07-11 01:13:21 -0700

    Add function 'get_betweenness'

---
+++ R/get_betweenness.R
@@ -0,0 +1,49 @@
+#' Get betweenness centrality scores
+#' @description Get the betweenness centrality scores
+#' for all nodes in a graph.
+#' @param graph a graph object of class
+#' \code{dgr_graph}.
+#' @return a named vector of betweenness scores for
+#' each of the nodes (where the node ID values comprise
+#' the names in the vector).
+#' @examples
+#' \dontrun{
+#' # Create a random graph
+#' graph <-
+#'   create_random_graph(
+#'     10, 22, set_seed = 1)
+#'
+#' # Get the betweeness scores for nodes in the graph
+#' get_betweenness(graph)
+#' #>    node     score
+#' #> 1     1  6.633333
+#' #> 2     2  5.638095
+#' #> 3     3  1.904762
+#' #> 4     4  4.019048
+#' #> 5     5  8.157143
+#' #> 6     6  2.000000
+#' #> 7     7 10.157143
+#' #> 8     8  8.857143
+#' #> 9     9  3.466667
+#' #> 10   10  1.166667
+#' }
+#' @importFrom influenceR betweenness
+#' @export get_betweenness
+
+get_betweenness <- function(graph) {
+
+  # Convert the graph to an igraph object
+  ig_graph <- to_igraph(graph)
+
+  # Get the betweeness scores for each of the
+  # graph's nodes
+  betweenness_scores <- influenceR::betweenness(ig_graph)
+
+  # Create df with betweenness scores
+  betweenness_scores_df <-
+    data.frame(node = names(betweenness_scores),
+               score = betweenness_scores,
+               stringsAsFactors = FALSE)
+
+  return(betweenness_scores_df)
+}
