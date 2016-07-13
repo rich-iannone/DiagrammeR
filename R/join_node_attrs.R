dcdbf7864d37d35fc239995d4946eea11ed6d802
Richard Iannone <riannone@me.com>
2016-07-12 21:32:18 -0700

    Add option to specify column names for join

--- R/join_node_attrs.R
+++ R/join_node_attrs.R
@@ -47,20 +47,46 @@
 #' @export join_node_attrs
 
 join_node_attrs <- function(graph,
-                            df) {
+                            df,
+                            by_graph = NULL,
+                            by_df = NULL) {
+
+  if (is.null(by_graph) & !is.null(by_df)) {
+    stop("Both column specifications must be provided.")
+  }
+
+  if (!is.null(by_graph) & is.null(by_df)) {
+    stop("Both column specifications must be provided.")
+  }
 
   # Extract the graph's ndf
   nodes <- get_node_df(graph)
 
   # Get column names from the graph's ndf
-  column_names <- colnames(nodes)
+  column_names_graph <- colnames(nodes)
 
-  # Perform a left join on the `nodes` data frame
-  nodes <- merge(nodes, df, all.x = TRUE)
+  # Get column names from the df
+  column_names_df <- colnames(df)
+
+  if (is.null(by_graph) & is.null(by_df)) {
+
+    # Perform a left join on the `nodes` data frame
+    nodes <- merge(nodes, df, all.x = TRUE)
+  }
+
+  if (!is.null(by_graph) & !is.null(by_df)) {
+
+    # Perform a left join on the `nodes` data frame
+    nodes <-
+      merge(nodes, df,
+            all.x = TRUE,
+            by.x = by_graph,
+            by.y = by_df)
+  }
 
   # Get new column names in the revised ndf
   new_col_names <-
-    setdiff(colnames(nodes), column_names)
+    setdiff(colnames(nodes), column_names_graph)
 
   # Get the column numbers for the new columns
   col_numbers <-
@@ -68,7 +94,8 @@ join_node_attrs <- function(graph,
 
   # Replace string <NA> values with empty strings
   for (i in 1:length(col_numbers)) {
-    nodes[,col_numbers[i]][is.na(nodes[,col_numbers[i]])] <- ""
+    nodes[,col_numbers[i]][
+      is.na(nodes[,col_numbers[i]])] <- ""
   }
 
   # Create a new graph object
