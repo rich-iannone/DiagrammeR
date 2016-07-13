a5d4fffd2780d9b426a5b1ffb21912453d1baa9e
Richard Iannone <riannone@me.com>
2016-07-12 21:32:49 -0700

    Update roxygen documentation

--- R/get_betweenness.R
+++ R/get_betweenness.R
@@ -7,12 +7,14 @@
 #' each of the nodes.
 #' @examples
 #' \dontrun{
+#' library(magrittr)
+#'
 #' # Create a random graph
 #' graph <-
 #'   create_random_graph(
 #'     10, 22, set_seed = 1)
 #'
-#' # Get the betweeness scores for nodes in the graph
+#' # Get the betweenness scores for nodes in the graph
 #' get_betweenness(graph)
 #' #>    node betweenness
 #' #> 1     1    6.633333
@@ -25,6 +27,15 @@
 #' #> 8     8    8.857143
 #' #> 9     9    3.466667
 #' #> 10   10    1.166667
+#'
+#' # Add the betweenness values the the graph
+#' # as a node attribute
+#' graph <-
+#'   graph %>%
+#'   join_node_attrs(
+#'     get_betweenness(.),
+#'     by_graph = "nodes",
+#'     by_df = "node")
 #' }
 #' @importFrom influenceR betweenness
 #' @export get_betweenness
@@ -34,9 +45,10 @@ get_betweenness <- function(graph) {
   # Convert the graph to an igraph object
   ig_graph <- to_igraph(graph)
 
-  # Get the betweeness scores for each of the
+  # Get the betweenness scores for each of the
   # graph's nodes
-  betweenness_scores <- influenceR::betweenness(ig_graph)
+  betweenness_scores <-
+    influenceR::betweenness(ig_graph)
 
   # Create df with betweenness scores
   betweenness_scores_df <-
