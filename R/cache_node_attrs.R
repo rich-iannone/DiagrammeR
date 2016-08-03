b5013ed8d44b596d9adc53e912401070552a72f4
Richard Iannone <riannone@me.com>
2016-08-03 08:27:28 -0700

    Add example to roxygen @examples section

--- R/cache_node_attrs.R
+++ R/cache_node_attrs.R
@@ -13,6 +13,32 @@
 #' @param nodes an optional vector of node IDs for
 #' filtering the list of nodes present in the graph.
 #' @return a graph object of class \code{dgr_graph}.
+#' @examples
+#' library(magrittr)
+#'
+#' # Set a seed
+#' set.seed(25)
+#'
+#' # Create a graph with 10 nodes and 9 edges
+#' graph <-
+#'   create_graph() %>%
+#'   add_n_nodes(10) %>%
+#'   set_node_attrs(
+#'     "value", rnorm(node_count(.), 5, 2)) %>%
+#'   add_edges_w_string(
+#'     "1->2 1->3 2->4 2->5 3->6 3->7 4->8 4->9 5->10")
+#'
+#'
+#' # Cache all values from the node attribute `value`
+#' # as a numeric vector
+#' graph <-
+#'   graph %>%
+#'   cache_node_attrs("value", "numeric")
+#'
+#' # Get the mean from all values available in
+#' # the cache
+#' graph %>% get_cache %>% mean
+#' # [1] 4.651246
 #' @export cache_node_attrs
 
 cache_node_attrs <- function(graph,
