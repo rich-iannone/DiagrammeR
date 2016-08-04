11c41aa829d42273993728140e1148a9abce26b5
Richard Iannone <riannone@me.com>
2016-08-03 20:26:26 -0700

    Add example to roxygen @examples section

--- R/cache_node_count_ws.R
+++ R/cache_node_count_ws.R
@@ -8,6 +8,25 @@
 #' \code{dgr_graph} that is created using
 #' \code{create_graph}.
 #' @return a graph object of class \code{dgr_graph}.
+#' @examples
+#' library(magrittr)
+#'
+#' # Create a graph with 10 nodes and 9 edges
+#' graph <-
+#'   create_graph() %>%
+#'   add_n_nodes(10) %>%
+#'   add_edges_w_string(
+#'     "1->2 1->3 2->4 2->5 3->6 3->7 4->8 4->9 5->10")
+#'
+#' # Cache a count of nodes after creating a selection
+#' graph <-
+#'   graph %>%
+#'   select_nodes_by_id(2:5) %>%
+#'   cache_node_count_ws()
+#'
+#' # Get the number of nodes stored in the cache
+#' graph %>% get_cache
+#' #> [1] 4
 #' @export cache_node_count_ws
 
 cache_node_count_ws <- function(graph) {
