47dfffa068c4dc63679d33062942bc7b20be4c50
Richard Iannone <riannone@me.com>
2016-08-21 22:57:10 -0700

    Add help file to make the magrittr pipe available

---
+++ R/pipe.R
@@ -0,0 +1,10 @@
+#' Pipe graphics
+#'
+#' @description DiagrammeR uses the pipe function,
+#' \code{\%>\%} to turn function composition into a
+#' series of imperative statements.
+#' @importFrom magrittr %>%
+#' @name %>%
+#' @rdname pipe
+#' @export
+NULL
