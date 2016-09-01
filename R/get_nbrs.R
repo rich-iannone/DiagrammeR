90ff6b9a0844318e18b833572080d97998142f4f
rich-iannone <riannone@me.com>
2016-09-01 10:02:38 -0700

    Update example in roxygen @examples section

--- R/get_nbrs.R
+++ R/get_nbrs.R
@@ -23,26 +23,31 @@
 #'
 #' # Find all neighbor nodes for node `5`
 #' random_graph %>% get_nbrs(5)
-#' #> [1] "1"  "2"  "12" "18"
+#' #> [1]  1  2 12 18
 #'
 #' # Find all neighbor nodes for nodes `5`, `7`,
 #' # and `15`
 #' random_graph %>% get_nbrs(c(5, 7, 15))
-#' #> [1] "1"  "2"  "6"  "12" "18"
+#' #> [1]  1  2  6 12 18
 #'
-#' # Get neighbors for node `11` and add a node
-#' # attribute to color the nodes green, then, color
-#' # all other nodes light gray
+#' # Color node `11` with purple, get it's
+#' # neighbors and color those nodes green
 #' random_graph <-
 #'   random_graph %>%
+#'   select_nodes_by_id(11) %>%
+#'   set_node_attrs_ws("color", "purple") %>%
+#'   clear_selection %>%
 #'   select_nodes_by_id(get_nbrs(., 11)) %>%
-#'   set_node_attrs_ws('color', 'green') %>%
-#'   invert_selection %>%
-#'   set_node_attrs_ws('color', 'gray85') %>%
-#'   clear_selection
+#'   set_node_attrs_ws("color", "green")
 #'
 #' # Render the graph to see the change
 #' random_graph %>% render_graph
+#'
+#' # A node with no neighbors with return `NA`
+#' create_graph() %>%
+#'   add_node %>%
+#'   get_nbrs(1)
+#' #> [1] NA
 #' @export get_nbrs
 
 get_nbrs <- function(graph,
