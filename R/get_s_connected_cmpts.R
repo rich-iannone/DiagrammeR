d470bdc1a87a73b793adb6f17ad6837ada579a77
rich-iannone <riannone@me.com>
2016-09-03 20:57:14 -0700

    Add example to roxygen @examples section

--- R/get_s_connected_cmpts.R
+++ R/get_s_connected_cmpts.R
@@ -6,6 +6,30 @@
 #' \code{create_graph}.
 #' @return a data frame with nodes and their membership
 #' in different strongly connected components.
+#' @examples
+#' # Create a graph with a random connection
+#' # between 2 different cycles
+#' graph <-
+#'   create_graph() %>%
+#'   add_cycle(3, "cycle_1") %>%
+#'   add_cycle(4, "cycle_2") %>%
+#'   add_edge(
+#'     from = get_nodes(., "type", "cycle_1") %>%
+#'       sample(1),
+#'     to = get_nodes(., "type", "cycle_2") %>%
+#'       sample(1))
+#'
+#' # Get the strongly connected components as a
+#' # data frame of nodes and their groupings
+#' get_s_connected_cmpts(graph)
+#' #>   node sc_component
+#' #> 1    1            1
+#' #> 2    2            1
+#' #> 3    3            1
+#' #> 4    4            2
+#' #> 5    5            2
+#' #> 6    6            2
+#' #> 7    7            2
 #' @importFrom igraph components
 #' @export get_s_connected_cmpts
 
@@ -16,7 +40,7 @@ get_s_connected_cmpts <- function(graph) {
 
   # Get the component list
   components <-
-    igraph::components(ig_graph, mode = 'strong')
+    igraph::components(ig_graph, mode = "strong")
 
   # Create the output data frame
   components_df <-
