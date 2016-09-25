a7d1e19d2383e3a8f42e68986d02bbf587e32f12
Richard Iannone <riannone@me.com>
2016-09-24 23:57:26 -0700

    Add function 'combine_ndfs'

---
+++ R/combine_ndfs.R
@@ -0,0 +1,35 @@
+#' Combine multiple node data frames
+#' @description Combine several node data frames
+#' into a single node data frame.
+#' @param ... two or more node data frames, which
+#' contain node IDs and associated attributes.
+#' @return a combined node data frame.
+#' @export combine_ndfs
+
+combine_ndfs <- function(...) {
+
+  ndfs <- list(...)
+
+  for (l in 1:length(ndfs)) {
+    if (l == 1) {
+      df1 <- ndfs[l][[1]]
+      df2 <- ndfs[l + 1][[1]]
+    }
+
+    if (l > 1 & l < length(ndfs)) {
+      df1 <- ndf_new
+      df2 <- ndfs[l + 1][[1]]
+    }
+
+    ndf_new <-
+      dplyr::bind_rows(df1, df2)
+
+    if (l == length(ndfs)) {
+      break
+    }
+  }
+
+  ndf_new[, 1] <- as.integer(1:nrow(ndf_new))
+
+  return(ndf_new)
+}
