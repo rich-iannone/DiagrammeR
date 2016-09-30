c6444d7b1afadf177050a11c58ff415ba8738b09
Richard Iannone <riannone@me.com>
2016-09-29 21:42:54 -0700

    Modify examples in roxygen @examples section

--- R/node_present.R
+++ R/node_present.R
@@ -15,8 +15,8 @@
 #'
 #' # Create a node data frame (ndf)
 #' nodes <-
-#'   create_nodes(
-#'     nodes = 1:26,
+#'   create_node_df(
+#'     n = 26,
 #'     label = TRUE,
 #'     type = c(rep("a", 7),
 #'              rep("b", 9),
@@ -25,7 +25,7 @@
 #'
 #' # Create an edge data frame (edf)
 #' edges <-
-#'   create_edges(
+#'   create_edge_df(
 #'     from = sample(1:26, replace = TRUE),
 #'     to = sample(1:26, replace = TRUE),
 #'     rel = c(rep("rel_a", 7),
