3d3a545abd8f6c5fa1b296b0bee77a9fc1dc8b85
Richard Iannone <riannone@me.com>
2016-10-17 09:10:28 -0700

    Add tests for the `add_full_graph()` function

--- tests/testthat/test-add_graphs.R
+++ tests/testthat/test-add_graphs.R
@@ -294,3 +294,104 @@ test_that("Adding a star is possible", {
   expect_identical(unique(graph$edges_df$rel),
                    c("z", "y"))
 })
+
+test_that("Adding a full graph is possible", {
+
+  # Create a graph and add a full graph
+  # with 3 nodes to it; keep loops
+  graph <-
+    create_graph() %>%
+    add_full_graph(
+      n = 3, keep_loops = TRUE)
+
+  # Expect 3 nodes to have been created
+  expect_equal(node_count(graph), 3)
+
+  # Expect 9 edges to have been created
+  expect_equal(edge_count(graph), 9)
+
+  # Expect that there are 3 loops in the graph
+  expect_equal(
+    length(
+      which(
+        get_edge_df(graph)[, 1] ==
+          get_edge_df(graph)[, 2])),
+    3)
+
+  # Expect node ID values from 1 to 3
+  expect_identical(get_node_ids(graph), 1:3)
+
+  # Expect label values from 1 to 3
+  # expect_identical(graph$nodes_df$label,
+  #                  as.character(1:3))
+
+  # Expect type values to be unset
+  expect_equal(unique(graph$nodes_df[, 2]), "")
+
+  # Create a graph and add a full graph
+  # with 3 nodes to it; discard loops
+  graph <-
+    create_graph() %>%
+    add_full_graph(
+      n = 3, keep_loops = FALSE)
+
+  # Expect 3 nodes to have been created
+  expect_equal(node_count(graph), 3)
+
+  # Expect 6 edges to have been created
+  expect_equal(edge_count(graph), 6)
+
+  # Expect that there are no loops in the graph
+  expect_equal(
+    length(
+      which(
+        get_edge_df(graph)[, 1] ==
+          get_edge_df(graph)[, 2])),
+    0)
+
+  # Create a graph, add a full graph
+  # (with no loops), set values for the
+  # node `label`, node `type`, and edge `rel`
+  graph <-
+    create_graph() %>%
+    add_full_graph(
+      n = 3,
+      type = "connected",
+      label = c("1st", "2nd", "3rd"),
+      rel = "connected_to")
+
+  # Expect 3 nodes to have been created
+  expect_equal(node_count(graph), 3)
+
+  # Expect 6 edges to have been created
+  expect_equal(edge_count(graph), 6)
+
+  # Expect that there are no loops in the graph
+  expect_equal(
+    length(
+      which(
+        get_edge_df(graph)[, 1] ==
+          get_edge_df(graph)[, 2])),
+    0)
+
+  # Expect that the `type` node attr
+  # has the value `connected` for all
+  # nodes created
+  expect_equal(
+    unique(get_node_df(graph)[, 2]),
+    "connected")
+
+  # Expect that the `label` values
+  # assigned to the nodes are in the
+  # graph's internal node data frame
+  expect_equal(
+    get_node_df(graph)[, 3],
+    c("1st", "2nd", "3rd"))
+
+  # Expect that the `label` edge attr
+  # has the value `connected_to` for
+  # all edges created
+  expect_equal(
+    unique(get_edge_df(graph)[, 3]),
+    "connected_to")
+})
