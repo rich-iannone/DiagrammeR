582ee3a0828a6fea12e1263c27c7f0d5646ff484
Richard Iannone <riannone@me.com>
2016-10-19 08:38:25 -0700

    Use function `translate_to_node_id()`

--- R/add_edges_w_string.R
+++ R/add_edges_w_string.R
@@ -106,43 +106,14 @@ add_edges_w_string <- function(graph,
   # If `use_label` is set to TRUE, treat values in
   # list as labels; need to map to node ID values
   if (use_label) {
+    from_to_node_id <-
+      translate_to_node_id(
+        graph = graph,
+        from = from,
+        to = to)
 
-    # Node labels must be unique
-    if (length(unique(graph$nodes_df$label)) !=
-        node_count(graph)) {
-      stop("You cannot use labels to form edges because they are not distinct")
-    }
-
-    # No node labels can be empty
-    if (any(graph$nodes_df$label == "")) {
-      stop("You cannot use labels to form edges if there are empty strings for labels")
-    }
-
-    # Create the `from_id` and `to_id` vectors
-    from_id <- vector("integer")
-    to_id <- vector("integer")
-
-    # Get an ordered vector of node ID values
-    # as `from` nodes
-    for (i in 1:length(from)) {
-      from_id <-
-        c(from_id,
-          graph$nodes_df[
-            which(graph$nodes_df$label %in% from[i]), 1])
-    }
-
-    # Get an ordered vector of node ID values
-    # as `to` nodes
-    for (i in 1:length(to)) {
-      to_id <-
-        c(to_id,
-          graph$nodes_df[
-            which(graph$nodes_df$label %in% to[i]), 1])
-    }
-
-    # Reassign these nodes back to `from` and `to`
-    from <- from_id
-    to <- to_id
+    from <- from_to_node_id$from
+    to = from_to_node_id$to
   }
 
   # Create an edge data frame (edf) without
