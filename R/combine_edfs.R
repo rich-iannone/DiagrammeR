340aa247e69b66d464ca8473ea4dfebf0c3a3615
Richard Iannone <riannone@me.com>
2016-10-23 23:45:24 -0700

    Simplify function by using `bind_rows()`

--- R/combine_edfs.R
+++ R/combine_edfs.R
@@ -33,14 +33,14 @@
 #' # View the combined edge data frame
 #' all_edges
 #' #>   from to      rel color data arrowhead
-#' #> 1    1  2 requires green  2.7
-#' #> 2    1  4 requires green  8.9
-#' #> 3    2  4 requires green  2.6
-#' #> 4    3  1 requires green  0.6
-#' #> 5    5  7 receives   red            dot
-#' #> 6    7  8 receives   red            dot
-#' #> 7    8  6 receives   red            dot
-#' #> 8    8  5 receives   red            dot
+#' #> 1    1  2 requires green  2.7      <NA>
+#' #> 2    1  4 requires green  8.9      <NA>
+#' #> 3    2  4 requires green  2.6      <NA>
+#' #> 4    3  1 requires green  0.6      <NA>
+#' #> 5    5  7 receives   red   NA       dot
+#' #> 6    7  8 receives   red   NA       dot
+#' #> 7    8  6 receives   red   NA       dot
+#' #> 8    8  5 receives   red   NA       dot
 #' @export combine_edfs
 
 combine_edfs <- function(...) {
@@ -53,94 +53,17 @@ combine_edfs <- function(...) {
       df2 <- data_frames[l + 1][[1]]
     }
     if (l > 1 & l < length(data_frames)) {
-      df1 <- df_new
+      df1 <- edf_new
       df2 <- data_frames[l + 1][[1]]
     }
+
+    # Bind rows from df1 and df2
+    edf_new <- dplyr::bind_rows(df1, df2)
+
     if (l == length(data_frames)) {
       break
     }
-
-    # Examine the column names for each data frame
-    # and determine which columns are not common to
-    # each df
-    if (any(!(colnames(df2) %in% colnames(df1)))) {
-      df_1_missing <-
-        colnames(df2)[which(!(colnames(df2) %in%
-                                colnames(df1)))]
-    } else {
-      df_1_missing <- NA
-    }
-
-    if (any(!(colnames(df1) %in% colnames(df2)))) {
-      df_2_missing <-
-        colnames(df1)[which(!(colnames(df1) %in%
-                                colnames(df2)))]
-    } else {
-      df_2_missing <- NA
-    }
-
-    # Determine the new column names for the appended
-    # data frames
-    if (any(!is.na(df_1_missing))) {
-      new_column_names_df1 <-
-        c(colnames(df1), df_1_missing)
-    } else {
-      new_column_names_df1 <- NA
-    }
-
-    if (any(!is.na(df_2_missing))) {
-      new_column_names_df2 <-
-        c(colnames(df2), df_2_missing)
-    } else {
-      new_column_names_df2 <- NA
-    }
-
-    # Add missing columns to `df1`, with no values
-    if (any(!is.na(df_1_missing))) {
-      for (i in 1:length(df_1_missing)) {
-        df1 <-
-          cbind(df1, df_1_missing = rep("", nrow(df1)))
-
-        if (i == length(df_1_missing)) {
-          colnames(df1) <- new_column_names_df1
-        }
-      }
-    }
-
-    # Add missing columns to `df2`, with no values
-    if (any(!is.na(df_2_missing))) {
-      for (i in 1:length(df_2_missing)) {
-        df2 <-
-          cbind(df2, df_2_missing = rep("", nrow(df2)))
-
-        if (i == length(df_2_missing)) {
-          colnames(df2) <- new_column_names_df2
-        }
-      }
-    }
-
-    # Create a new data frame with combined rows
-    for (i in 1:length(colnames(df1))) {
-      if (i == 1) df_new <-
-          data.frame(
-            mat.or.vec(nr = nrow(df1) + nrow(df2),
-                       nc = 0),
-            stringsAsFactors = FALSE)
-
-      df_col <-
-        c(as.character(df1[,colnames(df1)[i]]),
-          as.character(df2[,colnames(df1)[i]]))
-
-      df_new <- cbind(df_new, df_col)
-      colnames(df_new)[i] <- colnames(df1)[i]
-
-      if (i == length(colnames(df1))) {
-        for (j in 1:ncol(df_new)) {
-          df_new[,j] <- as.character(df_new[,j])
-        }
-      }
-    }
   }
 
-  return(df_new)
+  return(edf_new)
 }
