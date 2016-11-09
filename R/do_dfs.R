3678b1591a2592f0de514eb4dc50d32fbcc5d1c3
Richard Iannone <riannone@me.com>
2016-11-08 21:24:30 -0600

    Rewrite `do_dfs()` function

--- R/do_dfs.R
+++ R/do_dfs.R
@@ -1,18 +1,28 @@
-#' Perform a depth-first search
-#' @description With either a single node serving as
-#' the starting point or using the whole graph,
-#' perform a depth-first search.
+#' Perform the depth-first search (dfs) algorithm
+#' @description With a chosen or random node
+#' serving as the starting point, perform a
+#' depth-first search of the whole graph and return
+#' the node ID values visited. The dfs algorithm
+#' differs from breadth-first search (bfs) in that
+#' dfs will follow tree branches as far as possible
+#' until terminating at leaf node (bfs traverses
+#' branches one level at a time).
 #' @param graph a graph object of class
 #' \code{dgr_graph} that is created using
 #' \code{create_graph}.
 #' @param node an optional node ID value to specify
-#' a single starting point for the dfs.
-#' @param direction using \code{all} (the default), the
-#' dfs will ignore edge direction while traversing
-#' through the graph. With \code{out}, traversals
-#' between adjacent nodes will respect the edge
-#' direction.
-#' @return a list object containing dfs information.
+#' a single starting point for the dfs. If not
+#' provided, a random node from the graph will be
+#' chosen.
+#' @param direction using \code{all} (the default),
+#' the dfs will ignore edge direction while
+#' traversing through the graph. With \code{out},
+#' traversals between adjacent nodes will respect
+#' the edge direction.
+#' @return a vector containing node ID values for
+#' nodes visited during the depth-first search. The
+#' order of the node IDs corresponds to the order
+#' visited.
 #' @examples
 #' # Create a graph containing two balanced trees
 #' graph <-
@@ -20,263 +30,80 @@
 #'   add_balanced_tree(2, 2) %>%
 #'   add_balanced_tree(3, 2)
 #'
-#' # Perform a depth-first search of the smaller tree,
-#' # beginning at the root node `1`
-#' dfs_info <-
-#'   graph %>% do_dfs(1)
+#' # Perform a depth-first search of the graph,
+#' # beginning at the root node `1` (the default
+#' # `direction = "all"` doesn't take edge
+#' # direction into account)
+#' graph %>% do_dfs(1)
+#' #> [1]  1  2  4  5  3  6  7  8  9 12 13 14 10
+#' #> [14] 15 16 17 11 18 19 20
 #'
-#' # The do_dfs function creates a list with
-#' # information on the nodes visited (in the order
-#' # visited) and the complete search path
-#' dfs_info
-#' #> $visited
-#' #> [1] "1" "2" "4" "5" "3" "6" "7"
-#' #>
-#' #> $search_path
-#' #>  [1] "1" "2" "4" "2" "5" "2" "1" "3" "6" "3" "7"
-#' #> [12] "3" "1"
-#'
-#' # The dfs can be done on the entire graph, where
-#' # random nodes are chosen as starting points until
-#' # the entire graph has been traversed; here, dfs
-#' # started at one tree and finished on the other
-#' graph %>% do_dfs
-#' #> $visited
-#' #>  [1] "3"  "1"  "2"  "4"  "5"  "6"  "7"  "11"
-#' #>  [9] "8"  "9"  "12" "13" "14" "10" "15" "16"
-#' #> [17] "17" "18" "19" "20"
-#' #>
-#' #> $search_path
-#' #>  [1] "3"  "1"  "2"  "4"  "2"  "5"  "2"  "1"
-#' #>  [9] "3"  "6"  "3"  "7"  "3"  "11" "8"  "9"
-#' #> [17] "12" "9"  "13" "9"  "14" "9"  "8"  "10"
-#' #> [25] "15" "10" "16" "10" "17" "10" "8"  "11"
-#' #> [33] "18" "11" "19" "11" "20" "11"
+#' # If not specifying a starting node, the function
+#' # will begin the search from a random node
+#' graph %>% do_dfs()
+#' #> [1]  14  9  8 10 15 16 17 11 18 19 20 12 13
+#' #> [14]  1  2  4  5  3  6  7
 #'
 #' # It's also possible to perform dfs while
 #' # taking into account edge direction; using
-#' # `direction = out` causes the dfs routine to
-#' # visit nodes along outward edges
-#' graph %>%
-#'   do_dfs(3, "out")
-#' #> $visited
-#' #> [1] "3" "6" "7"
-#' #>
-#' #> $search_path
-#' #> [1] "3" "6" "3" "7" "3"
+#' # `direction = "in"` causes the dfs routine to
+#' # visit nodes along inward edges
+#' graph %>% do_dfs(1, "in")
+#' #> [1]  1  2  3  4  5  6  7  8  9 10 11 12 13
+#' #> [14] 14 15 16 17 18 19 20
 #'
-#' # Reversing the edge directions with the
-#' # `rev_edge_dir()` function and performing
-#' # dfs at the same node as before results in a
-#' # different set of visited nodes
-#' graph %>%
-#'   rev_edge_dir %>%
-#'   do_dfs(3, "out")
-#' #> $visited
-#' #> [1] "3" "1"
-#' #>
-#' #> $search_path
-#' #> [1] "3" "1" "3"
+#' # Using `direction = "out"` results in the dfs
+#' # moving along solely outward edges
+#' graph %>% do_dfs(1, "out")
+#' #> [1]  1  2  4  5  3  6  7  8  9 12 13 14 10
+#' #> [14] 15 16 17 11 18 19 20
+#' @importFrom igraph dfs
 #' @export do_dfs
 
 do_dfs <- function(graph,
                    node = NULL,
                    direction = "all") {
 
+  # Stop function if there is no node data frame
+  # in the graph
+  if (is.null(graph$nodes_df)) {
+    stop("This graph does not contain any nodes.")
+  }
+
+  # If no node provided, choose a random node
+  if (is.null(node)) {
+    node <- sample(graph$nodes_df[, 1], 1)
+  }
+
+  # Convert the graph to an igraph object
+  ig_graph <- to_igraph(graph)
+
+  # Perform the depth first search algorithm in
+  # the direction requested
   if (direction == "all") {
-
-    if (is.null(node)) {
-
-      # Get all nodes available in the graph
-      graph_nodes <- get_node_ids(graph)
-
-      # Initialize the `search_path` and `visited` vectors
-      search_path <- vector(mode = "character")
-      visited <- vector(mode = "character")
-
-      repeat {
-
-        if (all(graph_nodes %in% unique(search_path))) {
-          break
-        } else {
-          starting_node <-
-            sample(setdiff(graph_nodes,
-                           unique(search_path)), 1)
-
-          current <- starting_node
-          stack <- starting_node
-          search_path <- c(search_path, starting_node)
-          visited <- c(visited, starting_node)
-        }
-
-        repeat {
-
-          if (stack[length(stack)] == starting_node &
-              all(get_nbrs(graph, current) %in% visited)) {
-
-            # Entirely traversed, empty stack
-            stack <- stack[-length(stack)]
-            break
-
-          } else if (!all(get_nbrs(graph, current) %in% visited)) {
-
-            # Available neighbor to visit, add to stack
-            current <-
-              setdiff(get_nbrs(graph, current),
-                      visited)[1]
-
-            stack <- unique(c(stack, current))
-            visited <- c(visited, current)
-            search_path <- c(search_path, current)
-
-          } else if (all(get_nbrs(graph, current) %in% visited)) {
-
-            # No neighbors to visit, revisit previous in stack
-            stack <- stack[-length(stack)]
-            current <- stack[length(stack)]
-            search_path <- c(search_path, current)
-
-          } else {
-            break
-          }
-        }
-      }
-    }
-
-    if (!is.null(node)) {
-
-      starting_node <- node
-      stack <- starting_node
-      search_path <- node
-      current <- node
-      visited <- node
-
-      repeat {
-
-        if (stack[length(stack)] == starting_node &
-            all(get_nbrs(graph, current) %in% visited)) {
-
-          # Entirely traversed, empty stack
-          stack <- stack[-length(stack)]
-          break
-
-        } else if (!all(get_nbrs(graph, current) %in% visited)) {
-
-          # Available neighbor to visit, add to stack
-          current <-
-            setdiff(get_nbrs(graph, current),
-                    visited)[1]
-
-          stack <- unique(c(stack, current))
-          visited <- c(visited, current)
-          search_path <- c(search_path, current)
-
-        } else if (all(get_nbrs(graph, current) %in% visited)) {
-
-          # No neighbors to visit, revisit previous in stack
-          stack <- stack[-length(stack)]
-          current <- stack[length(stack)]
-          search_path <- c(search_path, current)
-
-        } else {
-          break
-        }
-      }
-    }
+    dfs_result <-
+      igraph::dfs(
+        graph = ig_graph,
+        root = node,
+        neimode = "all")
+  } else if (direction == "out") {
+    dfs_result <-
+      igraph::dfs(
+        graph = ig_graph,
+        root = node,
+        neimode = "out")
+  } else if (direction == "in") {
+    dfs_result <-
+      igraph::dfs(
+        graph = ig_graph,
+        root = node,
+        neimode = "in")
+  } else if (!(direction %in% c("all", "out", "in"))) {
+    stop("The value for `direction` must be either `all`, `out`, or `in`.")
   }
 
-  if (direction == "out") {
+  # Get the nodes visited during the dfs
+  dfs_nodes <- as.integer(as.numeric(dfs_result$order))
 
-    if (is.null(node)) {
-
-      # Get all nodes available in the graph
-      graph_nodes <- get_node_ids(graph)
-
-      # Initialize the `search_path` vector
-      search_path <- vector(mode = "character")
-
-      repeat {
-
-        if (all(graph_nodes %in% unique(search_path))) {
-          break
-        } else {
-          starting_node <-
-            sample(setdiff(graph_nodes,
-                           unique(search_path)), 1)
-          search_path <- c(search_path, starting_node)
-          current <- starting_node
-        }
-
-        repeat {
-
-          if (all(current == starting_node &
-                  (all(get_successors(graph, current) %in%
-                       search_path) |
-                   is.na(get_successors(graph, current))))) {
-            break
-          } else if (!any(is.na(get_successors(graph, current))) &
-                     length(get_successors(graph, current)) > 0 &
-                     !all(get_successors(graph, current) %in%
-                          search_path)) {
-            current <-
-              setdiff(get_successors(graph, current),
-                      search_path)[1]
-          } else if (any(is.na(get_successors(graph, current))) |
-                     all(get_successors(graph, current) %in%
-                         search_path)) {
-            current <-
-              intersect(get_predecessors(graph, current),
-                        search_path)
-          } else {
-            break
-          }
-
-          search_path <- c(search_path, current)
-        }
-      }
-    }
-
-    if (!is.null(node)) {
-
-      starting_node <- node
-      search_path <- starting_node
-      current <- starting_node
-
-      repeat {
-
-        if (all(current == starting_node &
-                (all(get_successors(graph, current) %in%
-                     search_path) |
-                 is.na(get_successors(graph, current))))) {
-          break
-        } else if (!any(is.na(get_successors(graph, current))) &
-                   length(get_successors(graph, current)) > 0 &
-                   !all(get_successors(graph, current) %in%
-                        search_path)) {
-          current <-
-            setdiff(get_successors(graph, current),
-                    search_path)[1]
-        } else if (any(is.na(get_successors(graph, current))) |
-                   all(get_successors(graph, current) %in%
-                       search_path)) {
-          current <-
-            intersect(get_predecessors(graph, current),
-                      search_path)
-        } else {
-          break
-        }
-
-        search_path <- c(search_path, current)
-      }
-    }
-  }
-
-  # Create an empty list object `dfs`
-  dfs <- list()
-
-  # Add the `visited` and `search_path` vectors to `dfs`
-  dfs$visited <- unique(search_path)
-  dfs$search_path <- search_path
-
-  return(dfs)
+  return(dfs_nodes)
 }
