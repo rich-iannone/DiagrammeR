06e184fd03efe5acc128155e366846d1f8a73c84
Richard Iannone <riannone@me.com>
2016-11-25 18:18:42 -0800

    Make correction to function name in graph log

--- R/add_nodes_from_df_cols.R
+++ R/add_nodes_from_df_cols.R
@@ -179,7 +179,7 @@ add_nodes_from_df_cols <- function(graph,
     add_action_to_log(
       graph_log = graph$graph_log,
       version_id = nrow(graph$graph_log) + 1,
-      function_used = "add_n_nodes",
+      function_used = "add_nodes_from_df_cols",
       time_modified = time_function_start,
       duration = graph_function_duration(time_function_start),
       nodes = nrow(graph$nodes_df),
