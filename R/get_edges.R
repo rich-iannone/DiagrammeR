377e2018ef0c71eaccddd3e6aecc82efb9c42999
Richard Iannone <riannone@me.com>
2016-12-08 10:42:10 -0800

    Add in option to return edges with node labels

--- R/get_edges.R
+++ R/get_edges.R
@@ -15,6 +15,10 @@
 #' associated with edges. With \code{df}, a data frame
 #' containing outgoing and incoming node IDs associated
 #' with edges.
+#' @param return_values using \code{id} (the default)
+#' results in node ID values returned in the edge
+#' definitions. With \code{label}, the node labels will
+#' instead be used to define edges.
 #' @return a list, data frame, or a vector object,
 #' depending on the value given to \code{return_type}.
 #' @examples
@@ -22,6 +26,7 @@
 #' ndf <-
 #'   create_node_df(
 #'     n = 4,
+#'     label = c("one", "two", "three", "four"),
 #'     type = "letter",
 #'     color = c("red", "green", "grey", "blue"),
 #'     value = c(3.5, 2.6, 9.4, 2.7))
@@ -85,15 +90,38 @@
 #'                  "value > 3"),
 #'   return_type = "vector")
 #' #> [1] "3 -> 1"
-#' @importFrom dplyr filter_ select_
+#'
+#' # Use `return_values = "label"` to return
+#' # the labels of the connected nodes
+#' get_edges(
+#'   graph,
+#'   conditions = c("color == 'blue'",
+#'                  "value > 3"),
+#'   return_type = "vector",
+#'   return_values = "label")
+#' #> [1] "three -> one"
+#' @importFrom dplyr filter_ select_ left_join rename
 #' @export get_edges
 
 get_edges <- function(x,
                       conditions = NULL,
-                      return_type = "vector") {
+                      return_type = "vector",
+                      return_values = "id") {
+
+  # Create bindings for specific variables
+  label <- NULL
 
   if (inherits(x, "dgr_graph")) {
     edges_df <- x$edges_df
+
+    if (return_values == "label") {
+      edges_df <-
+        edges_df %>%
+        dplyr::left_join(x$nodes_df %>% select_("id", "label"), by = c("from" = "id")) %>%
+        dplyr::rename(from_label_ = label) %>%
+        dplyr::left_join(x$nodes_df %>% select_("id", "label"), by = c("to" = "id")) %>%
+        dplyr::rename(to_label_ = label)
+    }
   }
 
   if (inherits(x, "data.frame")) {
@@ -127,25 +155,42 @@ get_edges <- function(x,
 
     edges_list <- vector(mode = "list")
     edges_list[[1]] <- edges_list[[2]] <- vector(mode = "integer")
-    edges_list[[1]] <- c(edges_list[[1]], edges_df$from)
-    edges_list[[2]] <- c(edges_list[[2]], edges_df$to)
+
+    if (return_values == "id") {
+      edges_list[[1]] <- c(edges_list[[1]], edges_df$from)
+      edges_list[[2]] <- c(edges_list[[2]], edges_df$to)
+    } else if (return_values == "label") {
+      edges_list[[1]] <- c(edges_list[[1]], edges_df$from_label_)
+      edges_list[[2]] <- c(edges_list[[2]], edges_df$to_label_)
+    }
 
     return(edges_list)
   }
 
   if (return_type == "df") {
 
-    edges_df <-
-      edges_df %>%
-      dplyr::select_("from", "to")
+    if (return_values == "id") {
+      edges_df <-
+        edges_df %>%
+        dplyr::select_("from", "to")
+    } else if (return_values == "label") {
+      edges_df <-
+        edges_df %>%
+        dplyr::select_("from_label_", "to_label_")
+    }
 
     return(edges_df)
   }
 
   if (return_type == "vector") {
 
-    edges_vector <-
-      paste(edges_df$from, "->", edges_df$to)
+    if (return_values == "id") {
+      edges_vector <-
+        paste(edges_df$from, "->", edges_df$to)
+    } else if (return_values == "label") {
+      edges_vector <-
+        paste(edges_df$from_label_, "->", edges_df$to_label_)
+    }
 
     return(edges_vector)
   }
