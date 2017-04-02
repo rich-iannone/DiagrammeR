a8121c8ef3e15513787e9362f7ca712f783b362e
Richard Iannone <riannone@me.com>
2017-04-02 00:11:43 -0700

    Simplify function by only returning a df

--- R/get_eccentricity.R
+++ R/get_eccentricity.R
@@ -1,55 +1,39 @@
 #' Get node eccentricities
-#' @description Get a named vector or data frame with
-#' node eccentricity values.
+#' @description Get a data frame with node
+#' eccentricity values.
 #' @param graph a graph object of class
 #' \code{dgr_graph}.
-#' @param nodes an optional vector of node IDs for
-#' filtering the list of nodes present in the graph.
-#' @param return_type using \code{vector} (the default)
-#' will provide a named vector of eccentricity values
-#' (the node IDs serve as names). With \code{df},
-#' a data frame containing node IDs and eccentricity
-#' values is provided.
-#' @return a either a named vector or a data frame
-#' containing eccentricity values by node, depending
-#' on the value supplied to \code{return_type}.
+#' @return a data frame containing eccentricity values
+#' by node ID value.
 #' @examples
-#' # Get the eccentricities for all nodes in
-#' # a randomly-created graph
+#' # Get the eccentricities for all nodes
+#' # in a randomly-created graph
 #' get_eccentricity(
 #'   graph = create_random_graph(
-#'             15, 20, set_seed = 23))
-#' #> 1  2  3  4  5  6  7  8  9 10 11 12 13 14 15
-#' #> 2  4  3  0  2  3  1  2  0  0  1  1  0  1  0
+#'             5, 7, set_seed = 23))
+#' #>   id eccentricity
+#' #> 1  1            2
+#' #> 2  2            3
+#' #> 3  3            2
+#' #> 4  4            1
+#' #> 5  5            0
 #' @export get_eccentricity
 
-get_eccentricity <- function(graph,
-                             nodes = NULL,
-                             return_type = "vector") {
+get_eccentricity <- function(graph) {
 
   # Validation: Graph object is valid
   if (graph_object_valid(graph) == FALSE) {
     stop("The graph object is not valid.")
   }
 
-  if (is.null(nodes)) {
-    nodes_to_process <- get_node_ids(graph)
-    node_ids <- nodes_to_process
+  # Validation: Graph contains nodes
+  if (graph_contains_nodes(graph) == FALSE) {
+    stop("The graph contains no nodes, so, eccentricity values cannot be determined.")
   }
 
-  if (!is.null(nodes)) {
+  node_ids <- get_node_ids(graph)
 
-    # Stop function if nodes provided are not all
-    # in the graph
-    if (any(!(as.character(nodes) %in% get_node_ids(graph)))) {
-      stop('Not all nodes specified are present in the graph.')
-    }
-
-    nodes_to_process <- nodes
-    node_ids <- nodes_to_process
-  }
-
-  for (i in 1:length(nodes_to_process)) {
+  for (i in 1:length(node_ids)) {
 
     if (i == 1) {
       eccentricity <- vector(mode = 'integer')
@@ -66,24 +50,18 @@ get_eccentricity <- function(graph,
     eccentricity <-
       c(eccentricity, longest_path)
 
-    if (i == length(nodes_to_process)) {
+    if (i == length(node_ids)) {
       names(eccentricity) <- node_ids
     }
   }
 
-  if (return_type == 'vector') {
-    return(eccentricity)
-  }
+  # Create a data frame with node ID values
+  # and eccentrity values
+  eccentricity <-
+    data.frame(
+      id = names(eccentricity),
+      eccentricity = eccentricity,
+      stringsAsFactors = FALSE)
 
-  if (return_type == 'df') {
-
-    # Create a data frame with node ID values
-    # and eccentrity values
-    eccentricity <-
-      data.frame(id = names(eccentricity),
-                 eccentricity = eccentricity,
-                 stringsAsFactors = FALSE)
-
-    return(eccentricity)
-  }
+  return(eccentricity)
 }
