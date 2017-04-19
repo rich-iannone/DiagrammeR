b244bb36396fff6fd9b4290759b48e44f9aeb99b
Richard Iannone <riannone@me.com>
2017-04-18 23:55:50 -0700

    Add new layout options when rendering a graph

--- R/render_graph.R
+++ R/render_graph.R
@@ -3,6 +3,10 @@
 #' render the graph in the RStudio Viewer.
 #' @param graph a graph object of class
 #' \code{dgr_graph}.
+#' @param layout a string specifying a layout type to
+#' use for node placement in this rendering. Possible
+#' layouts include: \code{nicely}, \code{circle},
+#' \code{tree}, \code{kk}, and \code{fr}.
 #' @param output a string specifying the output type;
 #' \code{graph} (the default) renders the graph using
 #' the \code{grViz} function and \code{visNetwork}
@@ -15,45 +19,43 @@
 #' the height of the resulting graphic in pixels.
 #' @examples
 #' \dontrun{
-#' # Set a seed
-#' set.seed(23)
+#' # Render a graph that's a balanced tree
+#' create_graph() %>%
+#'   add_balanced_tree(2, 3) %>%
+#'   render_graph()
 #'
-#' # Create a node data frame (ndf)
-#' ndf <-
-#'   create_node_df(
-#'     n = 26,
-#'     type = "basic",
-#'     shape = sample(c("circle", "square"),
-#'                    length(1:26),
-#'                    replace = TRUE),
-#'     fillcolor = sample(c("aqua", "orange",
-#'                          "pink", "lightgreen",
-#'                          "black", "yellow"),
-#'                        length(1:26),
-#'                        replace = TRUE))
+#' # Use the `tree` layout for better node
+#' # placement in this hierarchical graph
+#' create_graph() %>%
+#'   add_balanced_tree(2, 3) %>%
+#'   render_graph(layout = "tree")
 #'
-#' # Create an edge data frame (edf)
-#' edf <-
-#'   create_edge_df(
-#'     from = sample(1:26, replace = TRUE),
-#'     to = sample(1:26, replace = TRUE),
-#'     rel = "to_node")
+#' # Plot the same tree graph but don't
+#' # show the node ID values
+#' create_graph() %>%
+#'   add_balanced_tree(2, 3) %>%
+#'   set_node_attr_to_display() %>%
+#'   render_graph(layout = "tree")
 #'
-#' # Create a graph object using the ndf and edf
-#' graph <-
-#'   create_graph(
-#'     nodes_df = ndf,
-#'     edges_df = edf)
+#' # Create a circle graph with
+#' create_random_graph(
+#'   n = 55, m = 75,
+#'   set_seed = 23) %>%
+#' render_graph(layout = "circle")
 #'
-#' # Render the graph using Graphviz
-#' render_graph(graph)
-#'
-#' # Render the graph using visNetwork
-#' render_graph(graph, output = "visNetwork")
+#' # Render the graph using the `visNetwork`
+#' # output option
+#' create_graph() %>%
+#'   add_balanced_tree(2, 3) %>%
+#'   render_graph(graph, output = "visNetwork")
 #' }
+#' @importFrom dplyr select rename mutate
+#' @importFrom igraph layout_in_circle layout_with_sugiyama layout_with_kk layout_with_fr layout_nicely
+#' @importFrom tibble as_tibble
 #' @export render_graph
 
 render_graph <- function(graph,
+                         layout = NULL,
                          output = NULL,
                          title = NULL,
                          width = NULL,
@@ -69,7 +71,6 @@ render_graph <- function(graph,
   }
 
   if (output == "graph") {
-
     if (!is.null(title)) {
 
       graph <-
@@ -93,6 +94,84 @@ render_graph <- function(graph,
           graph, "fontcolor", "gray30", "graph")
     }
 
+    if (!is.null(layout)) {
+      if (layout %in% c("circle", "tree", "kk", "fr", "nicely")) {
+
+        graph <-
+          graph %>%
+          add_global_graph_attrs(
+            attr = "layout",
+            value = "neato",
+            attr_type = "graph")
+
+        if ("x" %in% colnames(graph$nodes_df)) {
+          graph$nodes_df <-
+            graph$nodes_df %>%
+            dplyr::select(-x)
+        }
+
+        if ("y" %in% colnames(graph$nodes_df)) {
+          graph$nodes_df <-
+            graph$nodes_df %>%
+            dplyr::select(-y)
+        }
+
+        if (layout == "circle") {
+          coords <-
+            graph %>%
+            to_igraph() %>%
+            igraph::layout_in_circle() %>%
+            tibble::as_tibble() %>%
+            dplyr::rename(x = V1, y = V2) %>%
+            dplyr::mutate(x = x * 10) %>%
+            dplyr::mutate(y = y * 10)
+        }
+
+        if (layout == "tree") {
+          coords <-
+            graph %>%
+            to_igraph() %>%
+            layout_with_sugiyama() %>%
+            .[[2]] %>%
+            as_tibble() %>%
+            rename(x = V1, y = V2)
+        }
+
+        if (layout == "kk") {
+          coords <-
+            graph %>%
+            to_igraph() %>%
+            igraph::layout_with_kk() %>%
+            tibble::as_tibble() %>%
+            dplyr::rename(x = V1, y = V2)
+        }
+
+        if (layout == "fr") {
+          coords <-
+            graph %>%
+            to_igraph() %>%
+            igraph::layout_with_fr() %>%
+            tibble::as_tibble() %>%
+            dplyr::rename(x = V1, y = V2)
+        }
+
+        if (layout == "nicely") {
+          coords <-
+            graph %>%
+            to_igraph() %>%
+            igraph::layout_nicely() %>%
+            tibble::as_tibble() %>%
+            dplyr::rename(x = V1, y = V2)
+        }
+
+        # Bind (x, y) coordinates to the graph's
+        # internal NDF
+        graph$nodes_df <-
+          graph$nodes_df %>%
+          bind_cols(coords)
+      }
+    }
+
     # Generate DOT code
     dot_code <- generate_dot(graph)
 
@@ -106,7 +185,6 @@ render_graph <- function(graph,
     grVizObject
 
   } else if (output == "visNetwork") {
-
     visnetwork(graph)
   }
 }
