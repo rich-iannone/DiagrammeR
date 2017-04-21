91190178e37fca4acd9323b0367e3676f0163924
Richard Iannone <riannone@me.com>
2017-04-20 23:48:49 -0700

    Make corrections to logging

--- R/add_cycle.R
+++ R/add_cycle.R
@@ -105,7 +105,7 @@ add_cycle <- function(graph,
       add_action_to_log(
         graph_log = graph_log,
         version_id = nrow(graph_log) + 1,
-        function_used = "add_balanced_tree",
+        function_used = "add_cycle",
         time_modified = time_function_start,
         duration = graph_function_duration(time_function_start),
         nodes = nrow(combined_graph$nodes_df),
--- R/add_prism.R
+++ R/add_prism.R
@@ -123,7 +123,7 @@ add_prism <- function(graph,
       add_action_to_log(
         graph_log = graph_log,
         version_id = nrow(graph_log) + 1,
-        function_used = "add_balanced_tree",
+        function_used = "add_prism",
         time_modified = time_function_start,
         duration = graph_function_duration(time_function_start),
         nodes = nrow(combined_graph$nodes_df),
