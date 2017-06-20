4444c3605fd0e58c0085afbfa0c9679bf292be9e
Richard Iannone <riannone@me.com>
2017-06-20 09:15:10 -0700

    Add function 'get_multiedge_count'

---
+++ R/get_multiedge_count.R
@@ -0,0 +1,59 @@
+#' Get the count of multiple edges in the graph
+#' @description Get a count of the number of mutiple
+#' edges in the graph. Included in the count is the
+#' number of separate edges that share the same edge
+#' definition (i.e., same pair of nodes) across the
+#' entire graph. So, for example, if there are 2
+#' edge definitions in the graph that involve 6
+#' separate edge IDs, the count will be \code{4}.
+#' @param graph a graph object of class
+#' \code{dgr_graph}.
+#' @return a vector with a single, numerical value.
+#' @examples
+#' # Create a node data frame (ndf)
+#' ndf <-
+#'   create_node_df(
+#'     n = 5,
+#'     label = TRUE)
+#'
+#' # Create an edge data frame (edf)
+#' edf <-
+#'   create_edge_df(
+#'     from = c(1, 4, 4, 3, 5, 1, 3, 4),
+#'       to = c(4, 1, 1, 2, 2, 2, 2, 1))
+#'
+#' # Create a graph with the ndf and edf
+#' graph <-
+#'   create_graph(
+#'     nodes_df = ndf,
+#'     edges_df = edf)
+#'
+#' # Get the total number of multiple
+#' # edges (those edges that share an
+#' # edge definition) in the graph
+#' get_multiedge_count(graph)
+#' #> [1] 3
+#' @importFrom dplyr distinct
+#' @export get_multiedge_count
+
+get_multiedge_count <- function(graph) {
+
+  # Validation: Graph object is valid
+  if (graph_object_valid(graph) == FALSE) {
+    stop("The graph object is not valid.")
+  }
+
+  # Validation: Graph contains edges
+  if (graph_contains_edges(graph) == FALSE) {
+    stop("The graph contains no edges, so, no selections can be made.")
+  }
+
+  # Check for the number of multiple edges
+  # regardless of which definitions these
+  # edges have
+  multiedge_count <-
+    (graph$edges_df %>% nrow()) -
+    (graph$edges_df %>% select(from, to) %>% distinct() %>% nrow())
+
+  return(multiedge_count)
+}
