ccaa22be1ae3f45bd61d3631d0ff21375fbc49e2
Richard Iannone <riannone@me.com>
2017-06-23 14:47:11 -0700

    Modify `add_action_to_log()` fcn call

--- R/add_n_node_clones.R
+++ R/add_n_node_clones.R
@@ -165,7 +165,7 @@ add_n_node_clones <- function(graph,
     add_action_to_log(
       graph_log = graph$graph_log,
       version_id = nrow(graph$graph_log) + 1,
-      function_used = "add_node_clones",
+      function_used = "add_n_node_clones",
       time_modified = time_function_start,
       duration = graph_function_duration(time_function_start),
       nodes = nrow(graph$nodes_df),
