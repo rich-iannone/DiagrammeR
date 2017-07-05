871e51b5b264e67bcdf9c5da4fe41fd162dca8b2
Richard Iannone <riannone@me.com>
2017-07-04 17:37:15 -0700

    Add examples to roxygen @examples section

--- R/get_graph_actions.R
+++ R/get_graph_actions.R
@@ -7,6 +7,37 @@
 #' @param graph a graph object of class
 #' \code{dgr_graph}.
 #' @return a \code{df_tbl} object.
+#' @examples
+#' # Create a random graph
+#' graph <-
+#'   create_random_graph(
+#'     n = 10, m = 22,
+#'     set_seed = 23)
+#'
+#' # Add a graph action that sets a node
+#' # attr column with a function; the
+#' # main function `set_node_attr_w_fcn()`
+#' # uses the `get_betweenness()` function
+#' # to provide betweenness values in the
+#' # `btwns` column
+#' graph <-
+#'   graph %>%
+#'   add_graph_action(
+#'     fcn = "set_node_attr_w_fcn",
+#'     node_attr_fcn = "get_betweenness",
+#'     column_name = "btwns",
+#'     action_name = "get_btwns")
+#'
+#' # To ensure that the action is
+#' # available in the graph, use the
+#' # `get_graph_actions()` function
+#' graph %>%
+#'   get_graph_actions()
+#' #> # A tibble: 1 x 3
+#' #>   action_index action_name
+#' #>          <dbl>       <chr>
+#' #> 1            1   get_btwns
+#' #> # ... with 1 more variables: expression <chr>
 #' @importFrom tibble as_tibble
 #' @export get_graph_actions
 
