74f4196ae25c13d1f6bb24d8389362643041b0fd
Richard Iannone <riannone@me.com>
2017-07-04 22:04:38 -0700

    Add examples to roxygen @examples section

--- R/get_graph_log.R
+++ R/get_graph_log.R
@@ -6,6 +6,30 @@
 #' @param graph a graph object of class
 #' \code{dgr_graph}.
 #' @return a \code{df_tbl} object.
+#' @examples
+#' # Create a random graph and
+#' # remove two nodes from it
+#' graph <-
+#'   create_random_graph(
+#'     n = 10, m = 22,
+#'     set_seed = 23) %>%
+#'   delete_node(node = 5) %>%
+#'   delete_node(node = 7)
+#'
+#' # Get the graph log, which is a
+#' # record of all graph transformations
+#' graph %>%
+#'   get_graph_log()
+#' #> # A tibble: 3 x 6
+#' #>   version_id       function_used
+#' #>        <int>               <chr>
+#' #> 1          1 create_random_graph
+#' #> 2          2         delete_node
+#' #> 3          3         delete_node
+#' #> # ... with 4 more variables:
+#' #> #   time_modified <dttm>,
+#' #> #   duration <dbl>, nodes <int>,
+#' #> #   edges <int>
 #' @importFrom tibble as_tibble
 #' @export get_graph_log
 
