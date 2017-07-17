3bcaf6a10481227ef0825866dca6bf8934b55378
Richard Iannone <riannone@me.com>
2017-07-17 15:22:56 -0700

    Reformat some testthat tests

--- tests/testthat/test-node_edge_count.R
+++ tests/testthat/test-node_edge_count.R
@@ -29,24 +29,38 @@ test_that("getting a node count for a graph is possible", {
       edges_df = edges)
 
   # Obtain a count of nodes by type
-  count_of_nodes <- node_count(graph, type = TRUE)
+  count_of_nodes <-
+    node_count(
+      graph = graph,
+      type = TRUE)
 
   # Expect that the `count_of_nodes` object is a named vector
-  expect_true(!is.null(names(count_of_nodes)))
   expect_true(
-    all(names(count_of_nodes) ==
-          c("a_to_g", "h_to_p",
-            "q_to_x", "y_and_z")))
-  expect_true(all(c(7, 9, 8, 2) %in% count_of_nodes))
+    !is.null(names(count_of_nodes)))
+
+  expect_true(
+    all(
+      names(count_of_nodes) ==
+        c("a_to_g", "h_to_p",
+          "q_to_x", "y_and_z")))
+
+  expect_true(
+    all(
+      c(7, 9, 8, 2) %in% count_of_nodes))
 
   # Obtain a total count of nodes
   total_count_of_nodes <-
-    node_count(graph, type = FALSE)
+    node_count(
+      graph = graph,
+      type = FALSE)
 
   # Expect that the `total_count_of_nodes` object
   # is a named vector
-  expect_is(total_count_of_nodes, "integer")
-  expect_equal(total_count_of_nodes, 26)
+  expect_is(
+    total_count_of_nodes, "integer")
+
+  expect_equal(
+    total_count_of_nodes, 26)
 })
 
 test_that("getting an edge count for a graph is possible", {
@@ -78,20 +92,29 @@ test_that("getting an edge count for a graph is possible", {
       edges_df = edges)
 
   # Obtain a count of nodes by relationship
-  count_of_edges <- edge_count(graph, rel = TRUE)
+  count_of_edges <-
+    edge_count(
+      graph = graph,
+      rel = TRUE)
 
   # Expect that the `count_of_edges` object is a
   # named vector
-  expect_true(!is.null(names(count_of_edges)))
+  expect_true(
+    !is.null(names(count_of_edges)))
 
   # Obtain a total count of edges
   total_count_of_edges <-
-    edge_count(graph, rel = FALSE)
+    edge_count(
+      graph = graph,
+      rel = FALSE)
 
   # Expect that the `total_count_of_edges` object is
   # a named vector
-  expect_is(total_count_of_edges, "integer")
-  expect_equal(total_count_of_edges, 26)
+  expect_is(
+    total_count_of_edges, "integer")
+
+  expect_equal(
+    total_count_of_edges, 26)
 })
 
 test_that("getting a node/edge count for an empty graph is possible", {
@@ -102,22 +125,32 @@ test_that("getting a node/edge count for an empty graph is possible", {
   # Expect that a node count of an empty graph
   # will return `0`
   expect_equal(
-    node_count(empty_graph, type = FALSE), 0)
+    node_count(
+      graph = empty_graph,
+      type = FALSE), 0)
 
   expect_equal(
-    node_count(empty_graph, type = TRUE), 0)
+    node_count(
+      graph = empty_graph,
+      type = TRUE), 0)
 
   # Expect that an edge count of an empty graph
   # will return `0`
   expect_equal(
-    edge_count(empty_graph, rel = FALSE), 0)
+    edge_count(
+      graph = empty_graph,
+      rel = FALSE), 0)
 
   expect_equal(
-    edge_count(empty_graph, rel = TRUE), 0)
+    edge_count(
+      graph = empty_graph,
+      rel = TRUE), 0)
 
   # Expect that an edge count with a relationship value
   # set to any character vector will automatically
   # return `0`
   expect_equal(
-    edge_count(empty_graph, rel = "rel"), 0)
+    edge_count(
+      graph = empty_graph,
+      rel = "rel"), 0)
 })
