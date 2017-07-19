16b332f065c3d62ae255386366ea20c616a4d090
Richard Iannone <riannone@me.com>
2017-07-19 00:05:36 -0700

    Remove argument from function

--- R/create_series.R
+++ R/create_series.R
@@ -10,8 +10,6 @@
 #' (the default) or a \code{temporal} type (which
 #' requires date-time strings and time zone codes
 #' to be supplied).
-#' @param series_scripts a vector of R scripts or paths
-#' to R scripts.
 #' @return a graph series object of type
 #' \code{dgr_graph_1D}.
 #' @examples
@@ -51,8 +49,7 @@
 
 create_series <- function(graph = NULL,
                           series_name = NULL,
-                          series_type = "sequential",
-                          series_scripts = NULL) {
+                          series_type = "sequential") {
 
   # Validation: Graph object is valid
   if (!is.null(graph))  {
@@ -63,10 +60,10 @@ create_series <- function(graph = NULL,
 
   # Initialize an empty graph series object
   graph_series <-
-    list(graphs = NULL,
-         series_name = series_name,
-         series_type = series_type,
-         series_scripts = series_scripts)
+    list(
+      graphs = NULL,
+      series_name = series_name,
+      series_type = series_type)
 
   attr(graph_series, "class") <- "dgr_graph_1D"
 
@@ -77,5 +74,5 @@ create_series <- function(graph = NULL,
   # Add a graph to the initialized graph series
   graph_series$graphs[[length(graph_series$graphs) + 1]] <- graph
 
-  return(graph_series)
+  graph_series
 }
