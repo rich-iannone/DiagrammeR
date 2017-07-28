1e4588a67658cbe6c4f3188463fd66bbb773e0ce
Richard Iannone <riannone@me.com>
2017-07-27 18:32:25 -0700

    Rename file/function

--- R/get_diameter.R
+++ R/get_diameter.R
@@ -13,13 +13,13 @@
 #'     set_seed = 23)
 #'
 #' # Get the graph diameter
-#' get_graph_diameter(graph)
+#' get_diameter(graph)
 #' #> [1] 4
 #' @importFrom dplyr group_by summarize ungroup
 #' @importFrom purrr flatten_dbl
-#' @export get_graph_diameter
+#' @export get_diameter
 
-get_graph_diameter <- function(graph) {
+get_diameter <- function(graph) {
 
   # Validation: Graph object is valid
   if (graph_object_valid(graph) == FALSE) {
