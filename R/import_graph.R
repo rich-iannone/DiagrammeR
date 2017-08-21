2749214ae9ac814d17b525c0660ac85cabbf8710
Richard Iannone <riannone@me.com>
2017-08-20 20:29:26 -0700

    Remove GraphML import support

--- R/import_graph.R
+++ R/import_graph.R
@@ -7,11 +7,10 @@
 #' \code{https://}, \code{ftp://}, or \code{ftps://}
 #' will be automatically downloaded.
 #' @param file_type the type of file to be imported.
-#' Options are: \code{graphml} (GraphML), \code{gml}
-#' (GML), \code{sif} (SIF), \code{edges} (a .edges
-#' file), and \code{mtx} (MatrixMarket format). If not
-#' supplied, the type of graph file will be inferred by
-#' its file extension.
+#' Options are: \code{gml} (GML), \code{sif} (SIF),
+#' \code{edges} (a .edges file), and \code{mtx}
+#' (MatrixMarket format). If not supplied, the type
+#' of graph file will be inferred by its file extension.
 #' @param edges_extra_attr_names for \code{edges} files,
 #' a vector of attribute names beyond the \code{from}
 #' and \code{to} data columns can be provided in the
@@ -30,23 +29,6 @@
 #' @return a graph object of class \code{dgr_graph}.
 #' @examples
 #' \dontrun{
-#' # Import a GraphML graph file
-#' graphml_graph <-
-#'   import_graph(
-#'     system.file(
-#'       "extdata/power_grid.graphml",
-#'       package = "DiagrammeR"))
-#'
-#' # Get a count of the graph's nodes
-#' graphml_graph %>%
-#'   node_count()
-#' #> [1] 4941
-#'
-#' # Get a count of the graph's edges
-#' graphml_graph %>%
-#'   edge_count()
-#' #> [1] 6594
-#'
 #' # Import an SIF graph file
 #' sif_graph <-
 #'   import_graph(
@@ -106,7 +88,7 @@ import_graph <- function(graph_file,
   # of the group that can be imported
   if (!is.null(file_type)) {
     if (!(tolower(file_type) %in%
-          c("graphml", "gml", "sif", "edges", "mtx"))) {
+          c("gml", "sif", "edges", "mtx"))) {
       stop("The file type as specified cannot be imported.")
     }
   }
@@ -148,9 +130,7 @@ import_graph <- function(graph_file,
     file_extension <- gsub(".*\\.([a-zA-Z]*?)", "\\1", graph_file)
 
     # Determine file type from file extension
-    if (file_extension == "graphml") {
-      file_type <- "graphml"
-    } else if (file_extension == "gml") {
+    if (file_extension == "gml") {
       file_type <- "gml"
     } else if (file_extension == "sif") {
       file_type <- "sif"
@@ -276,118 +256,6 @@ import_graph <- function(graph_file,
       create_graph(
         nodes_df = nodes,
         edges_df = edges)
-
-    # Return the graph
-    return(the_graph)
-  }
-
-  if (file_type == "graphml") {
-
-    # Read in the .graphml document as a vector object
-    graphml_document <- readLines(graph_file)
-
-    # Determine the starting and ending indices of
-    # the <node> tags
-    xml_nodes <-
-      list(node_start = grep("<node ", graphml_document),
-           node_end = grep("</node>", graphml_document))
-
-    # Determine the starting and ending indices of the
-    # <edge> tags
-    xml_edges <-
-      list(edge_start = grep("<edge ", graphml_document),
-           edge_end = grep("</edge>", graphml_document))
-
-    # Determine all node ID values for the graph
-    for (i in 1:length(xml_nodes[[1]])) {
-
-      if (i == 1) nodes_ids <- vector(mode = "character")
-
-      nodes_ids <-
-        c(nodes_ids,
-          str_replace_all(
-            str_extract(graphml_document[xml_nodes[[1]][i]],
-                        "\".*?\""), "\"", ""))
-    }
-
-    # Determine indices that contain first
-    # node attributes
-    node_key_indices <-
-      xml_nodes[[1]][1] - 1 +
-      grep("key",
-           graphml_document[xml_nodes[[1]][1]:xml_nodes[[2]][1]])
-
-    # Obtain names of keys
-    node_key_names <-
-      gsub(".*?\"(.*?)\".*", "\\1",
-           graphml_document[node_key_indices])
-
-    # Obtain list of vectors for all node attributes
-    node_attributes <- list()
-
-    for (i in 1:length(node_key_names)) {
-      for (j in 1:length(xml_nodes[[1]])) {
-        if (j == 1) {
-          attribute <- vector(mode = "character")
-        }
-
-        attribute <-
-          c(attribute,
-            gsub(".*?>(.*?)<.*", "\\1",
-                 graphml_document[xml_nodes[[1]][j] + i]))
-
-        if (j == length(xml_nodes[[1]])) {
-          node_attributes[[i]] <-  attribute
-        }
-      }
-
-      if (i == length(node_key_names)) {
-        names(node_attributes) <- node_key_names
-      }
-    }
-
-    # Create all nodes for graph
-    all_nodes <-
-      cbind(
-        tibble::tibble(
-          id = as.integer(nodes_ids),
-          type = as.character(NA)),
-        data.frame(node_attributes))
-
-    # Determine all edge values for the graph
-    for (i in 1:length(xml_edges[[1]])) {
-
-      if (i == 1) {
-        edges_from <- vector(mode = "character")
-        edges_to <- vector(mode = "character")
-      }
-
-      edges_from <-
-        c(edges_from,
-          str_replace_all(
-            unlist(str_extract_all(
-              graphml_document[xml_edges[[1]][i]],
-              "\".*?\""))[1], "\"", ""))
-
-      edges_to <-
-        c(edges_to,
-          str_replace_all(
-            unlist(str_extract_all(
-              graphml_document[xml_edges[[1]][i]],
-              "\".*?\""))[2], "\"", ""))
-    }
-
-    # Create all edges for graph
-    all_edges <-
-      create_edge_df(
-        from = edges_from,
-        to = edges_to)
-
-    # Create the graph
-    the_graph <-
-      create_graph(
-        nodes_df = all_nodes,
-        edges_df = all_edges)
 
     # Return the graph
     return(the_graph)
