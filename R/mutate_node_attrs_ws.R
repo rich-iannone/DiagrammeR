6884ea55fdee88daec1933d7083a23aa47abccd5
Richard Iannone <riannone@me.com>
2017-09-04 23:56:43 -0700

    Modify `add_action_to_log()` statement

--- R/mutate_node_attrs_ws.R
+++ R/mutate_node_attrs_ws.R
@@ -220,7 +220,7 @@ mutate_node_attrs_ws <- function(graph,
     add_action_to_log(
       graph_log = graph$graph_log,
       version_id = nrow(graph$graph_log) + 1,
-      function_used = "mutate_node_attrs",
+      function_used = "mutate_node_attrs_ws",
       time_modified = time_function_start,
       duration = graph_function_duration(time_function_start),
       nodes = nrow(graph$nodes_df),
