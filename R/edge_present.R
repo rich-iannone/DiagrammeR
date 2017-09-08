59b7491ad92a7953c96339d7a2d4d6c0dcb1d8d0
Richard Iannone <riannone@me.com>
2017-09-07 23:05:11 -0700

    Revise function to accept node labels

--- R/edge_present.R
+++ R/edge_present.R
@@ -1,53 +1,65 @@
 #' Determine whether a specified edge is present
 #' @description From a graph object of class
-#' \code{dgr_graph}, determine whether a directed edge
-#' (defined by a pair of node IDs extant in the graph)
-#' is present.
+#' \code{dgr_graph}, determine whether an edge
+#' (defined by a pair of node IDs or node label
+#' values) is present.
 #' @param graph a graph object of class
 #' \code{dgr_graph}.
-#' @param from a node ID from which the edge to be
-#' queried is outgoing.
-#' @param to a node ID to which the edge to be queried
-#' is incoming.
+#' @param from a node ID from which the edge
+#' is outgoing. For an undirected graph, the
+#' value in \code{from} can be interchangeable
+#' with that in \code{to}.
+#' @param to a node ID to which the edge is
+#' incoming. For an undirected graph, the
+#' value in \code{to} can be interchangeable
+#' with that in \code{from}.
 #' @return a logical value.
 #' @examples
-#' # Set a seed
-#' set.seed(23)
-#'
-#' # Create a node data frame (ndf)
-#' ndf <-
-#'   create_node_df(
-#'     n = 26,
-#'     label = TRUE,
-#'     type = c(rep("a", 7),
-#'              rep("b", 9),
-#'              rep("c", 8),
-#'              rep("d", 2)))
-#'
-#' # Create an edge data frame (edf)
-#' edf <-
-#'   create_edge_df(
-#'     from = sample(1:26, replace = TRUE),
-#'     to = sample(1:26, replace = TRUE),
-#'     rel = c(rep("rel_a", 7),
-#'             rep("rel_b", 9),
-#'             rep("rel_c", 8),
-#'             rep("rel_d", 2)))
-#'
-#' # Create a graph using the ndf and edf
+#' # Create a simple graph with
+#' # a path of four nodes
 #' graph <-
-#'   create_graph(
-#'     nodes_df = ndf,
-#'     edges_df = edf)
+#'   create_graph() %>%
+#'   add_path(
+#'     n = 4,
+#'     type = "path",
+#'     label = c("one", "two",
+#'               "three", "four"))
 #'
-#' # Is there any edge between nodes with IDs
-#' # `1` and `2`?
-#' edge_present(graph, from = 1, to = 2)
+#' # Determine if there are any edges
+#' # with the definition `1` -> `2`
+#' edge_present(
+#'   graph = graph,
+#'   from = 1,
+#'   to = 2)
+#' #> TRUE
+#'
+#' # Determine if there are any edges
+#' # with the definition `4` -> `5`
+#' edge_present(
+#'   graph = graph,
+#'   from = 4,
+#'   to = 5)
 #' #> FALSE
 #'
-#' # Verify that there is an edge between nodes
-#' # `9` and `5`
-#' edge_present(graph, from = 9, to = 5)
+#' # Determine whether an edge,
+#' # defined by its labels as
+#' # `two` -> `three`, exists in
+#' # the graph
+#' edge_present(
+#'   graph = graph,
+#'   from = "two",
+#'   to = "three")
+#' #> TRUE
+#'
+#' # Set the graph as undirected
+#' # and determine whether an
+#' # edge between nodes with labels
+#' # `three` and `two` exists
+#' graph %>%
+#'   set_graph_undirected() %>%
+#'   edge_present(
+#'     from = "three",
+#'     to = "two")
 #' #> TRUE
 #' @export edge_present
 
@@ -73,26 +85,103 @@ edge_present <- function(graph,
     stop("Only single nodes for `from` and `to` should be specified.")
   }
 
-  # Determine whether pair of nodes provided are in
-  # the graph
-  if (from_is_single_value & to_is_single_value) {
+  if (inherits(from, "character") & inherits(to, "character")) {
+
+    # Determine whether the pair of
+    # labels provided are in the graph
     nodes_available_in_graph <-
       ifelse(all(c(from, to) %in%
-                   get_node_ids(graph)), TRUE, FALSE)
+                   graph$nodes_df$label), TRUE, FALSE)
+
+    # Return FALSE if both nodes not
+    # present in graph
+    if (nodes_available_in_graph == FALSE) {
+      return(FALSE)
+    }
+
+    # Return FALSE if the label for
+    # `from` does not exist in the graph
+    if (!(from %in% graph$nodes_df$label)) {
+      return(FALSE)
+    }
+
+    # Return FALSE if the label for
+    # `to` does not exist in the graph
+    if (!(to %in% graph$nodes_df$label)) {
+      return(FALSE)
+    }
+
+    # Use the `translate_to_node_id()` helper function to map
+    # node `label` values to node `id` values
+    from_to_node_id <-
+      translate_to_node_id(
+        graph = graph,
+        from = from,
+        to = to)
+
+    from <- from_to_node_id$from
+    to <- from_to_node_id$to
+
+    # Determine whether a matching edge is
+    # available in the graph
+    if (graph$directed) {
+
+      edge_is_in_graph <-
+        ifelse(
+          any(graph$edges_df$from == from &
+                graph$edges_df$to == to),
+          TRUE, FALSE)
+
+    } else if (graph$directed == FALSE) {
+
+      edge_is_in_graph <-
+        ifelse(
+          any(graph$edges_df$from == from &
+                graph$edges_df$to == to) |
+            any(graph$edges_df$from == to &
+                  graph$edges_df$to == from),
+          TRUE, FALSE)
+    }
+
+    return(edge_is_in_graph)
   }
 
-  # Stop function if both nodes not present in graph
-  if (nodes_available_in_graph == FALSE) {
-    stop("The nodes specified are not both present in the graph.")
-  }
+  if (inherits(from, "numeric") & inherits(to, "numeric")) {
 
-  # Determine whether a matching edge is available in
-  # the graph
-  if (nodes_available_in_graph) {
-    edge_is_in_graph <-
-      ifelse(any(graph$edges_df$from == from &
-                   graph$edges_df$to == to),
-             TRUE, FALSE)
+    # Determine whether the pair of
+    # labels provided are in the graph
+    if (from_is_single_value & to_is_single_value) {
+      nodes_available_in_graph <-
+        ifelse(all(c(from, to) %in%
+                     get_node_ids(graph)), TRUE, FALSE)
+    }
+
+    # Return FALSE if both nodes not
+    # present in graph
+    if (nodes_available_in_graph == FALSE) {
+      return(FALSE)
+    }
+
+    # Determine whether a matching edge is
+    # available in the graph
+    if (graph$directed) {
+
+      edge_is_in_graph <-
+        ifelse(
+          any(graph$edges_df$from == from &
+                graph$edges_df$to == to),
+          TRUE, FALSE)
+
+    } else if (graph$directed == FALSE) {
+
+      edge_is_in_graph <-
+        ifelse(
+          any(graph$edges_df$from == from &
+                graph$edges_df$to == to) |
+            any(graph$edges_df$from == to &
+                  graph$edges_df$to == from),
+          TRUE, FALSE)
+    }
 
     return(edge_is_in_graph)
   }
