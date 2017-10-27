c15c2c18efd3fe2f23db7a8726ccb15c1a132034
Richard Iannone <riannone@me.com>
2017-10-27 14:14:57 -0700

    Add function 'deselect_nodes'

---
+++ R/deselect_nodes.R
@@ -0,0 +1,84 @@
+#' Deselect any selected nodes in a graph
+#' @description Deselect nodes in a graph object of
+#' class \code{dgr_graph}.
+#' @param graph a graph object of class
+#' \code{dgr_graph}.
+#' @param nodes a vector of node IDs that
+#' should be deselected.
+#' @return a graph object of class \code{dgr_graph}.
+#' @examples
+#' # Create a node data frame (ndf)
+#' ndf <-
+#'   create_node_df(
+#'     n = 4,
+#'     type = c("a", "a", "z", "z"),
+#'     label = TRUE,
+#'     value = c(3.5, 2.6, 9.4, 2.7))
+#'
+#' # Create an edge data frame (edf)
+#' edf <-
+#'   create_edge_df(
+#'     from = c(1, 2, 3),
+#'     to = c(4, 3, 1),
+#'     rel = c("a", "z", "a"))
+#'
+#' # Create a graph with the ndf and edf
+#' graph <-
+#'   create_graph(
+#'     nodes_df = ndf,
+#'     edges_df = edf)
+#'
+#' # Explicitly select nodes `1` and `3`
+#' graph <-
+#'   graph %>%
+#'   select_nodes(nodes = c(1, 3)) %>%
+#'   deselect_nodes(nodes = 8)
+#'
+#' # Verify that the node selection
+#' # has been made for nodes `1` and
+#' # `3` and that node `1` has been
+#' # deselected (leaving only `3`)
+#' get_selection(graph)
+#' #> [1] 3
+#' @importFrom dplyr filter
+#' @export deselect_nodes
+
+deselect_nodes <- function(graph,
+                           nodes) {
+
+  # Get the time of function start
+  time_function_start <- Sys.time()
+
+  # Validation: Graph object is valid
+  if (graph_object_valid(graph) == FALSE) {
+    stop("The graph object is not valid.")
+  }
+
+  # If no node selection available, return graph
+  if (graph_contains_node_selection(graph) == FALSE) {
+    return(graph)
+  }
+
+  # Extract the graph's node selection
+  graph$node_selection <-
+    graph$node_selection %>%
+    dplyr::filter(!(node %in% nodes))
+
+  # Update the `graph_log` df with an action
+  graph$graph_log <-
+    add_action_to_log(
+      graph_log = graph$graph_log,
+      version_id = nrow(graph$graph_log) + 1,
+      function_used = "deselect_nodes",
+      time_modified = time_function_start,
+      duration = graph_function_duration(time_function_start),
+      nodes = nrow(graph$nodes_df),
+      edges = nrow(graph$edges_df))
+
+  # Write graph backup if the option is set
+  if (graph$graph_info$write_backups) {
+    save_graph_as_rds(graph = graph)
+  }
+
+  graph
+}
