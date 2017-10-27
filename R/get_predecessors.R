6fcdb56ae81ebd9c9f8fb87216f0e950b107d378
Richard Iannone <riannone@me.com>
2017-10-26 23:54:04 -0700

    Modify examples in roxygen @examples section

--- R/get_predecessors.R
+++ R/get_predecessors.R
@@ -13,24 +13,34 @@
 #' # Create a node data frame (ndf)
 #' ndf <- create_node_df(n = 26)
 #'
-#' # Create an edge data frame (edf)
+#' # Create an edge data
+#' # frame (edf)
 #' edf <-
 #'   create_edge_df(
-#'     from = sample(1:26, replace = TRUE),
-#'     to = sample(1:26, replace = TRUE))
+#'     from = sample(
+#'       1:26, replace = TRUE),
+#'     to = sample(
+#'       1:26, replace = TRUE))
 #'
-#' # From the ndf and edf, create a graph object
+#' # From the ndf and edf,
+#' # create a graph object
 #' graph <-
 #'   create_graph(
 #'     nodes_df = ndf,
 #'     edges_df = edf)
 #'
-#' # Get predecessors for node `23` in the graph
-#' get_predecessors(graph, node = 23)
+#' # Get predecessors for node
+#' # `23` in the graph
+#' graph %>%
+#'   get_predecessors(
+#'     node = 23)
 #' #> [1] 6
 #'
-#' # If there are no predecessors, `NA` is returned
-#' get_predecessors(graph, node = 26)
+#' # If there are no predecessors,
+#' # `NA` is returned
+#' graph %>%
+#'   get_predecessors(
+#'     node = 26)
 #' #> [1] NA
 #' @export get_predecessors
 
