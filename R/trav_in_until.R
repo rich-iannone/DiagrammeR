e2e5ea140651e1aba7d866ae6103c451fbea181a
Richard Iannone <riannone@me.com>
2017-10-30 23:52:33 -0700

    Add function 'trav_in_until'

---
+++ R/trav_in_until.R
@@ -0,0 +1,265 @@
+#' Traverse inward node-by_node until stopping conditions are met
+#' @description From a graph object of
+#' class \code{dgr_graph}, move along
+#' inward edges from one or more nodes
+#' present in a selection to other
+#' connected nodes, replacing the current
+#' nodes in the selection with those nodes
+#' traversed to until reaching nodes that
+#' satisfy one or more conditions.
+#' @param graph a graph object of class
+#' \code{dgr_graph}.
+#' @param conditions an option to use a
+#' stopping condition for the traversal.
+#' If the condition is met during the
+#' traversal (i.e., the node(s) traversed
+#' to match the condition), then those
+#' traversals will terminate at those
+#' nodes. Otherwise, traversals with
+#' continue and terminate when the number
+#' of steps provided in \code{max_steps}
+#' is reached.
+#' @param max_steps the maximum number
+#' of \code{trav_in()} steps (i.e.,
+#' node-to-node traversals in the inward
+#' direction) to allow before stopping.
+#' @param exclude_unmatched if \code{TRUE}
+#' (the default value) then any nodes not
+#' satisfying the conditions provided in
+#' \code{conditions} that are in the ending
+#' selection are excluded.
+#' @return a graph object of class
+#' \code{dgr_graph}.
+#' @examples
+#' # Create a path graph and add
+#' # values of 1 to 10 across the
+#' # nodes from beginning to end;
+#' # select the first path node
+#' graph <-
+#'   create_graph() %>%
+#'   add_path(
+#'     n = 10,
+#'     node_data = node_data(
+#'       value = 1:10)) %>%
+#'   select_nodes_by_id(
+#'     nodes = 10)
+#'
+#' # Traverse inward, node-by-node
+#' # until stopping at a node where
+#' # the `value` attribute is 8
+#' graph <-
+#'   graph %>%
+#'   trav_in_until(
+#'     conditions =
+#'       value == 1)
+#'
+#' # Get the graph's node selection
+#' graph %>%
+#'   get_selection()
+#' #> [1] 1
+#'
+#' # Create two cycles in graph and
+#' # add values of 1 to 6 to the
+#' # first cycle, and values 7 to
+#' # 12 in the second; select nodes
+#' # `6` and `12`
+#' graph <-
+#'   create_graph() %>%
+#'   add_cycle(
+#'     n = 6,
+#'     node_data = node_data(
+#'       value = 1:6)) %>%
+#'   add_cycle(
+#'     n = 6,
+#'     node_data = node_data(
+#'       value = 7:12)) %>%
+#'   select_nodes_by_id(
+#'     nodes = c(6, 12))
+#'
+#' # Traverse inward, node-by-node
+#' # from `6` and `12` until stopping
+#' # at the first nodes where the
+#' # `value` attribute is 1, 2, or 10;
+#' # specify that we should only
+#' # keep the finally traversed to
+#' # nodes that satisfy the conditions
+#' graph <-
+#'   graph %>%
+#'   trav_in_until(
+#'     conditions =
+#'       value %in% c(1, 2, 10),
+#'     exclude_unmatched = TRUE)
+#'
+#' # Get the graph's node selection
+#' graph %>%
+#'   get_selection()
+#' #> [1] 2 10
+#' @importFrom rlang enquo UQ
+#' @export trav_in_until
+
+trav_in_until <- function(graph,
+                          conditions,
+                          max_steps = 30,
+                          exclude_unmatched = TRUE) {
+
+  conditions <- rlang::enquo(conditions)
+
+  # Get the time of function start
+  time_function_start <- Sys.time()
+
+  # Validation: Graph object is valid
+  if (graph_object_valid(graph) == FALSE) {
+    stop("The graph object is not valid.")
+  }
+
+  # Validation: Graph contains nodes
+  if (graph_contains_nodes(graph) == FALSE) {
+    stop("The graph contains no nodes, so, no traversal can occur.")
+  }
+
+  # Validation: Graph contains edges
+  if (graph_contains_edges(graph) == FALSE) {
+    stop("The graph contains no edges, so, no traversal can occur.")
+  }
+
+  # Validation: Graph object has valid node selection
+  if (graph_contains_node_selection(graph) == FALSE) {
+    stop("There is no selection of nodes, so, no traversal can occur.")
+  }
+
+  # Initialize the node stack and
+  # the step count
+  node_stack <- vector(mode = "integer")
+  step <- 1
+
+  # Determine which nodes satisfy the
+  # conditions provided
+  all_nodes_conditions_met <-
+    get_node_ids(x = graph, conditions = rlang::UQ(conditions))
+
+  if (exclude_unmatched & all(is.na(all_nodes_conditions_met))) {
+
+    # Clear the active selection
+    graph <-
+      graph %>% clear_selection()
+
+    # Remove action from graph log
+    graph$graph_log <-
+      graph$graph_log[-nrow(graph$graph_log), ]
+
+    # Update the `graph_log` df with an action
+    graph$graph_log <-
+      add_action_to_log(
+        graph_log = graph$graph_log,
+        version_id = nrow(graph$graph_log) + 1,
+        function_used = "trav_in_until",
+        time_modified = time_function_start,
+        duration = graph_function_duration(time_function_start),
+        nodes = nrow(graph$nodes_df),
+        edges = nrow(graph$edges_df))
+
+    # Perform graph actions, if any are available
+    if (nrow(graph$graph_actions) > 0) {
+      graph <-
+        graph %>%
+        trigger_graph_actions()
+    }
+
+    # Write graph backup if the option is set
+    if (graph$graph_info$write_backups) {
+      save_graph_as_rds(graph = graph)
+    }
+
+    return(graph)
+  }
+
+  repeat {
+
+    # Perform traversal
+    graph <- graph %>% trav_in()
+
+    # Remove action from graph log
+    graph$graph_log <-
+      graph$graph_log[-nrow(graph$graph_log), ]
+
+    # If any nodes are `all_nodes_conditions_met` nodes
+    # deselect that node and save the node in a stack
+    if (any(graph %>% get_selection() %in% all_nodes_conditions_met)) {
+
+      node_stack <-
+        c(node_stack,
+          intersect(
+            graph %>% get_selection(),
+            all_nodes_conditions_met))
+
+      # Remove the node from the active selection
+      graph <-
+        graph %>% deselect_nodes(nodes = node_stack)
+
+      # Remove action from graph log
+      graph$graph_log <-
+        graph$graph_log[-nrow(graph$graph_log), ]
+    }
+
+    if (all(is.na(get_selection(graph)))) break
+
+    step <- step + 1
+
+    if (step == max_steps) break
+  }
+
+  if (length(node_stack > 0)) {
+
+    graph <-
+      graph %>%
+      select_nodes_by_id(unique(node_stack))
+
+    # Remove action from graph log
+    graph$graph_log <-
+      graph$graph_log[-nrow(graph$graph_log), ]
+  }
+
+  if (exclude_unmatched & !all(is.na(get_selection(graph)))) {
+
+    new_selection <- get_selection(graph)
+
+    graph <-
+      graph %>%
+      clear_selection() %>%
+      select_nodes_by_id(
+        intersect(new_selection, all_nodes_conditions_met))
+
+    # Remove action from graph log
+    graph$graph_log <-
+      graph$graph_log[-nrow(graph$graph_log), ]
+
+    # Remove action from graph log
+    graph$graph_log <-
+      graph$graph_log[-nrow(graph$graph_log), ]
+  }
+
+  # Update the `graph_log` df with an action
+  graph$graph_log <-
+    add_action_to_log(
+      graph_log = graph$graph_log,
+      version_id = nrow(graph$graph_log) + 1,
+      function_used = "trav_in_until",
+      time_modified = time_function_start,
+      duration = graph_function_duration(time_function_start),
+      nodes = nrow(graph$nodes_df),
+      edges = nrow(graph$edges_df))
+
+  # Perform graph actions, if any are available
+  if (nrow(graph$graph_actions) > 0) {
+    graph <-
+      graph %>%
+      trigger_graph_actions()
+  }
+
+  # Write graph backup if the option is set
+  if (graph$graph_info$write_backups) {
+    save_graph_as_rds(graph = graph)
+  }
+
+  graph
+}
