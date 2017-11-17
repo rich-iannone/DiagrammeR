29ebd34c4f3a288040b6673ad612dd38b3192161
Richard Iannone <riannone@me.com>
2017-11-17 00:40:54 -0800

    Add function 'is_graph_undirected'

---
+++ R/is_graph_undirected.R
@@ -0,0 +1,45 @@
+#' Is the graph an undirected graph?
+#' @description Determines whether a graph
+#' is set as undirected or not and
+#' returns a logical value to that effect.
+#' @param graph a graph object of class
+#' \code{dgr_graph}.
+#' @return a logical value.
+#' @examples
+#' # Create an empty graph; by
+#' # default, new graphs made
+#' # by `create_graph()` are
+#' # directed graph, so, use
+#' # `directed = FALSE` to create
+#' # an undirected graph
+#' graph <-
+#'   create_graph(
+#'     directed = FALSE)
+#'
+#' # Determine whether the
+#' # graph is undirected
+#' is_graph_undirected(graph)
+#' #> [1] TRUE
+#'
+#' # Use the `set_graph_directed()`
+#' # function and check again
+#' # as to whether the graph is
+#' # undirected
+#' graph %>%
+#'   set_graph_directed() %>%
+#'   is_graph_undirected()
+#' #> [1] FALSE
+#' @export is_graph_undirected
+
+is_graph_undirected <- function(graph) {
+
+  # Validation: Graph object is valid
+  if (graph_object_valid(graph) == FALSE) {
+    stop("The graph object is not valid.")
+  }
+
+  # Determine if graph is
+  # undirected by using a
+  # statement with `graph$directed`
+  graph$directed == FALSE
+}
