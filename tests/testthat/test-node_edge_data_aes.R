12982bdc379a18386fea572e7e25defc12a9f0e0
Richard Iannone <riannone@me.com>
2017-11-20 00:21:22 -0800

    Add tests for the `node_aes()` function

--- tests/testthat/test-node_edge_data_aes.R
+++ tests/testthat/test-node_edge_data_aes.R
@@ -85,3 +85,88 @@ test_that("creating edge data attribute values is possible", {
     edge_data(
       penwidth = c(1, 2, 4, 5)))
 })
+
+test_that("creating node aesthetic attribute values is possible", {
+
+  # Create a node aesthetic list object
+  # using the `node_aes()` function
+  node_aes <-
+    node_aes(
+      shape = c("circle", "rectangle"),
+      style = c("filled", "filled"),
+      penwidth = c(2.3, 5.3),
+      color = c("red", "#FFFFFF"),
+      fillcolor = c("orange", "#000000"),
+      fontname = c("Helvetica", "Helvetica"),
+      fontsize = c(12, 10.5),
+      fontcolor = c("gray25", "#000000"),
+      peripheries = c(1, 2),
+      height = c(6.2, 5),
+      width = c(5.5, 6.0),
+      x = c(2.1, 5.3),
+      y = c(2, 4.7),
+      group = c(1, 1),
+      tooltip = c("tooltip_1", "tooltip_2"),
+      xlabel = c("xlabel_1", "xlabel_2"),
+      URL = c("http://www.website.net", "http:://www.internet.org"),
+      sides = c(4, 5),
+      orientation = c(0, 45.5),
+      skew = c(0.4, -0.8),
+      distortion = c(-0.7, 0.3),
+      gradientangle = c(1.2, 45.0),
+      fixedsize = c(TRUE, FALSE),
+      labelloc = c("T", "C"),
+      margin = c("0.5,0.01", "0.1,0.05"))
+
+  # Expect that a list object has
+  # been generated
+  expect_is(
+    node_aes, "list")
+
+  # Expect specific names in the object
+  expect_equal(
+    names(node_aes),
+    c("shape", "style", "penwidth", "color",
+      "fillcolor", "fontname", "fontsize",
+      "fontcolor", "peripheries", "height",
+      "width", "x", "y", "group", "tooltip",
+      "xlabel", "URL", "sides", "orientation",
+      "skew", "distortion", "gradientangle",
+      "fixedsize", "labelloc", "margin"))
+
+  # Expect specific values in each
+  # of the list components
+  expect_equal(node_aes[[1]], c("circle", "rectangle"))
+  expect_equal(node_aes[[2]], c("filled", "filled"))
+  expect_equal(node_aes[[3]], c(2.3, 5.3))
+  expect_equal(node_aes[[4]], c("red", "#FFFFFF"))
+  expect_equal(node_aes[[5]], c("orange", "#000000"))
+  expect_equal(node_aes[[6]], c("Helvetica", "Helvetica"))
+  expect_equal(node_aes[[7]], c(12, 10.5))
+  expect_equal(node_aes[[8]], c("gray25", "#000000"))
+  expect_equal(node_aes[[9]], c(1, 2))
+  expect_equal(node_aes[[10]], c(6.2, 5))
+  expect_equal(node_aes[[11]], c(5.5, 6.0))
+  expect_equal(node_aes[[12]], c(2.1, 5.3))
+  expect_equal(node_aes[[13]], c(2, 4.7))
+  expect_equal(node_aes[[14]], c(1, 1))
+  expect_equal(node_aes[[15]], c("tooltip_1", "tooltip_2"))
+  expect_equal(node_aes[[16]], c("xlabel_1", "xlabel_2"))
+  expect_equal(node_aes[[17]], c("http://www.website.net", "http:://www.internet.org"))
+  expect_equal(node_aes[[18]], c(4, 5))
+  expect_equal(node_aes[[19]], c(0, 45.5))
+  expect_equal(node_aes[[20]], c(0.4, -0.8))
+  expect_equal(node_aes[[21]], c(-0.7, 0.3))
+  expect_equal(node_aes[[22]], c(1.2, 45.0))
+  expect_equal(node_aes[[23]], c(TRUE, FALSE))
+  expect_equal(node_aes[[24]], c("T", "C"))
+  expect_equal(node_aes[[25]], c("0.5,0.01", "0.1,0.05"))
+
+  # Expect the function to return
+  # an error when supplying any
+  # argument name that is not an
+  # aesthetic attribute
+  expect_error(
+    node_aes(
+      value_1 = c(1, 2, 4, 5)))
+})
