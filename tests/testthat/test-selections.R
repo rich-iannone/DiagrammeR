7f80c066959c77c03912d9f0842299f2f57b867b
Richard Iannone <riannone@me.com>
2017-11-24 08:34:08 -0800

    Add tests for deselecting nodes and edges

--- tests/testthat/test-selections.R
+++ tests/testthat/test-selections.R
@@ -420,6 +420,78 @@ test_that("selecting an edge in a graph is possible", {
       select_edges())
 })
 
+test_that("deselecting nodes in a graph is possible", {
+
+  # Create a graph and select
+  # all nodes in the graph
+  graph <-
+    create_graph() %>%
+    add_path(n = 8) %>%
+    select_nodes()
+
+  # Deselect nodes `1` and `2`
+  graph <-
+    graph %>%
+    deselect_nodes(nodes = c(1, 2))
+
+  # Expect nodes `3`-`8` to be
+  # in the active selection of nodes
+  expect_equal(
+    get_selection(graph = graph),
+    3:8)
+
+  # Expect that using `deselect_nodes()`
+  # on a graph with no active selection
+  # of nodes will just return the graph
+  # (and the function won't appear in the
+  # graph log)
+  graph_2 <-
+    create_graph() %>%
+    add_path(n = 8) %>%
+    deselect_nodes(nodes = 1:4)
+
+  expect_equal(
+    (graph_2 %>% get_graph_log())$function_used[
+      nrow(graph_2 %>% get_graph_log())],
+    "add_path")
+})
+
+test_that("deselecting edges in a graph is possible", {
+
+  # Create a graph and select
+  # all edges in the graph
+  graph <-
+    create_graph() %>%
+    add_path(n = 8) %>%
+    select_edges()
+
+  # Deselect edges `1` and `2`
+  graph <-
+    graph %>%
+    deselect_edges(edges = c(1, 2))
+
+  # Expect edges `3`-`7` to be
+  # in the active selection of edges
+  expect_equal(
+    get_selection(graph = graph),
+    3:7)
+
+  # Expect that using `deselect_edges()`
+  # on a graph with no active selection
+  # of eges will just return the graph
+  # (and the function won't appear in the
+  # graph log)
+  graph_2 <-
+    create_graph() %>%
+    add_path(n = 8) %>%
+    deselect_edges(edges = 1:4)
+
+  expect_equal(
+    (graph_2 %>% get_graph_log())$function_used[
+      nrow(graph_2 %>% get_graph_log())],
+    "add_path")
+})
+
 test_that("selecting edges via node IDs is possible", {
 
   # Create a graph
