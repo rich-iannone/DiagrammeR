3516382929445ac097eb2fa4ac82010f83dc484f
Richard Iannone <riannone@me.com>
2018-01-06 08:09:53 -0800

    Rename test file

---
+++ tests/testthat/test-htmlwidgets.R
@@ -0,0 +1,36 @@
+context("Creating objects with DiagrammeR, mermaid, and grViz")
+
+test_that("htmlwidgets object can be created", {
+
+  diagrammer_htmlwidget <-
+    DiagrammeR("
+  graph LR
+    A-->B
+    A-->C
+    C-->E
+    B-->D
+    C-->D
+    D-->F
+    E-->F")
+
+  # Expect that the object inherits
+  # from `DiagrammeR` and `htmlwidget`
+  expect_is(
+    diagrammer_htmlwidget, c("DiagrammeR", "htmlwidget"))
+
+  mermaid_htmlwidget <-
+    mermaid("
+  graph LR
+    A-->B
+    A-->C
+    C-->E
+    B-->D
+    C-->D
+    D-->F
+    E-->F")
+
+  # Expect that the object inherits
+  # from `DiagrammeR` and `htmlwidget`
+  expect_is(
+    mermaid_htmlwidget, c("DiagrammeR", "htmlwidget"))
+})
