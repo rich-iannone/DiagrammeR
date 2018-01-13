dec0edc004a2259c2ab83dc6dfac2007fc28a1b0
Richard Iannone <riannone@me.com>
2018-01-12 18:40:50 -0800

    Rename function and file

--- R/add_graph_to_graph_series.R
+++ R/add_graph_to_graph_series.R
@@ -2,45 +2,46 @@
 #' @description Add a graph object to an extant graph
 #' series object for storage of multiple graphs across
 #' a sequential or temporal one-dimensional array.
-#' @param graph a graph object to add to the graph
-#' series object.
 #' @param graph_series a graph series object to which
 #' the graph object will be added.
+#' @param graph a graph object to add to the graph
+#' series object.
 #' @return a graph series object of type
 #' \code{dgr_graph_1D}.
 #' @examples
 #' # Create three graphs
 #' graph_1 <-
 #'   create_graph() %>%
-#'   add_n_nodes(n = 3) %>%
-#'   add_edges_w_string(
-#'     edges = "1->3 1->2 2->3")
+#'   add_path(n = 4)
 #'
 #' graph_2 <-
-#'   graph_1 %>%
-#'   add_node() %>%
-#'   add_edge(from = 4, to = 3)
+#'   create_graph() %>%
+#'   add_cycle(n = 5)
 #'
 #' graph_3 <-
-#'   graph_2 %>%
-#'   add_node() %>%
-#'   add_edge(from = 5, to = 2)
+#'   create_graph() %>%
+#'   add_star(n = 6)
 #'
 #' # Create an empty graph series and add
 #' # the graphs
 #' series <-
 #'   create_series() %>%
-#'   add_to_series(graph_1, .) %>%
-#'   add_to_series(graph_2, .) %>%
-#'   add_to_series(graph_3, .)
+#'   add_graph_to_graph_series(
+#'     graph = graph_1) %>%
+#'   add_graph_to_graph_series(
+#'     graph = graph_2) %>%
+#'   add_graph_to_graph_series(
+#'     graph = graph_3)
 #'
-#' # Count the number of graphs in the graph series
-#' graph_count(series)
+#' # Count the number of graphs
+#' # in the graph series
+#' series %>%
+#'   graph_count()
 #' #> [1] 3
-#' @export add_to_series
+#' @export add_graph_to_graph_series
 
-add_to_series <- function(graph,
-                          graph_series) {
+add_graph_to_graph_series <- function(graph_series,
+                                      graph) {
 
   # Validation: Graph object is valid
   if (graph_object_valid(graph) == FALSE) {
