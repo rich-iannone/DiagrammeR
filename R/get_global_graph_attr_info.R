51c01581f4ae260169a5143b381de76445d08ebe
Richard Iannone <riannone@me.com>
2018-02-03 22:42:25 -0800

    Add function 'get_global_graph_attr_info'

---
+++ R/get_global_graph_attr_info.R
@@ -0,0 +1,52 @@
+#' Get global graph attributes
+#' @description Get the available
+#' global attributes for a graph
+#' object of class \code{dgr_graph}.
+#' @param graph a graph object of
+#' class \code{dgr_graph}.
+#' @return a data frame containing
+#' global attributes for the graph.
+#' @examples
+#' # Create a new graph and set
+#' # some global attributes
+#' graph <-
+#'   create_graph() %>%
+#'   set_global_graph_attrs(
+#'     attr = "overlap",
+#'     value = "true",
+#'     attr_type = "graph")
+#'
+#' # View the graph's set of
+#' # global attributes
+#' graph %>%
+#'   get_global_graph_attr_info()
+#' @importFrom dplyr tibble as_tibble
+#' @export get_global_graph_attr_info
+
+get_global_graph_attr_info <- function(graph) {
+
+  # Validation: Graph object is valid
+  if (graph_object_valid(graph) == FALSE) {
+
+    stop(
+      "The graph object is not valid.",
+      call. = FALSE)
+  }
+
+  if (nrow(graph$global_attrs) == 0) {
+
+    global_graph_attrs_tbl <-
+      dplyr::tibble(
+        attr = NA_character_ ,
+        value = NA_character_,
+        attr_type = NA_character_)[-1, ]
+
+  } else if (nrow(graph$global_attrs) > 0) {
+
+    global_graph_attrs_tbl <-
+      graph$global_attrs %>%
+      dplyr::as_tibble()
+  }
+
+  global_graph_attrs_tbl
+}
