c8679e6294f953925c788c8e55791b86777d7296
Richard Iannone <riannone@me.com>
2018-02-12 15:19:06 -0500

    Add messaging to 2 `select_...()` functions

--- R/select_nodes_in_neighborhood.R
+++ R/select_nodes_in_neighborhood.R
@@ -85,6 +85,11 @@ select_nodes_in_neighborhood <- function(graph,
       call. = FALSE)
   }
 
+  # Obtain the input graph's node and edge
+  # selection properties
+  n_e_select_properties_in <-
+    node_edge_selection_properties(graph = graph)
+
   # Create an empty list object
   nodes <- list()
 
@@ -178,6 +183,11 @@ select_nodes_in_neighborhood <- function(graph,
   # Replace `graph$edge_selection` with an empty df
   graph$edge_selection <- create_empty_esdf()
 
+  # Obtain the output graph's node and edge
+  # selection properties
+  n_e_select_properties_out <-
+    node_edge_selection_properties(graph = graph)
+
   # Update the `graph_log` df with an action
   graph$graph_log <-
     add_action_to_log(
@@ -193,6 +203,44 @@ select_nodes_in_neighborhood <- function(graph,
   if (graph$graph_info$write_backups) {
     save_graph_as_rds(graph = graph)
   }
+
+  # Construct message body
+  if (!n_e_select_properties_in[["node_selection_available"]] &
+      !n_e_select_properties_in[["edge_selection_available"]]) {
+
+    msg_body <-
+      glue::glue(
+        "created a new selection of \\
+        {n_e_select_properties_out[['selection_count_str']]}")
+
+  } else if (n_e_select_properties_in[["node_selection_available"]] |
+             n_e_select_properties_in[["edge_selection_available"]]) {
+
+    if (n_e_select_properties_in[["edge_selection_available"]]) {
+      msg_body <-
+        glue::glue(
+          "modified an existing selection of\\
+           {n_e_select_properties_in[['selection_count_str']]}:
+           * {n_e_select_properties_out[['selection_count_str']]}\\
+           are now in the active selection
+           * used the `{set_op}` set operation")
+    }
+
+    if (n_e_select_properties_in[["node_selection_available"]]) {
+      msg_body <-
+        glue::glue(
+          "created a new selection of\\
+           {n_e_select_properties_out[['selection_count_str']]}:
+           * this replaces\\
+           {n_e_select_properties_in[['selection_count_str']]}\\
+           in the prior selection")
+    }
+  }
+
+  # Issue a message to the user
+  emit_message(
+    fcn_name = "select_nodes_in_neighborhood",
+    message_body = msg_body)
 
   graph
 }
