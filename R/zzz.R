47d0a4e19d4a6e18f5a9b323e4b666ce3abedf2f
Richard Iannone <riannone@me.com>
2019-04-17 16:48:07 -0400

    Add `zzz.R` file with `globalVariables()` call

---
+++ R/zzz.R
@@ -0,0 +1,15 @@
+#nocov start
+
+#' @importFrom utils globalVariables
+globalVariables(
+  c(
+    ".",
+    "type",
+    "index",
+    "image",
+    "node_id",
+    "fa_uri"
+    )
+  )
+
+#nocov end
